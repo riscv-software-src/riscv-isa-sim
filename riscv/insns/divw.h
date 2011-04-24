c0cd05e70b496598033134acd5e038502d3763f0
Andrew Waterman <waterman@s141.Millennium.Berkeley.EDU>
2011-04-23 21:31:50 -0700

    [sim] fixed divw/remw crashing simulator

--- riscv/insns/divw.h
+++ riscv/insns/divw.h
@@ -1,9 +1,7 @@
 require_xpr64;
 if(RS2 == 0)
   RD = UINT64_MAX;
-// INT64_MIN/-1 corner case shouldn't occur in correct code, since
-// INT64_MIN is not a proper 32-bit signed value
-else if(sreg_t(RS1) == INT64_MIN && sreg_t(RS2) == -1)
+else if(int32_t(RS1) == INT32_MIN && int32_t(RS2) == -1)
   RD = RS1;
 else
   RD = sext32(int32_t(RS1) / int32_t(RS2));
