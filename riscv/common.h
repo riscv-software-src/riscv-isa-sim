55cc5d40ec16ea0639046ff1487636ba618d0e09
Andrew Waterman <waterman@cs.berkeley.edu>
2013-02-12 22:59:56 -0800

    migrate to c++11

--- riscv/common.h
+++ riscv/common.h
@@ -1,8 +1,6 @@
 #ifndef _RISCV_COMMON_H
 #define _RISCV_COMMON_H
 
-#define static_assert(x)	switch (x) case 0: case (x):
-
 #define   likely(x) __builtin_expect(x, 1)
 #define unlikely(x) __builtin_expect(x, 0)
 
