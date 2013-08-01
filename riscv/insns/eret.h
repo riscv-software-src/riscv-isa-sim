a39658bf83c03c734fef210131d649540940258a
Quan Nguyen <quannguyen@berkeley.edu>
2013-07-31 21:33:25 -0700

    Fix eret (again)
    * Set SR_S if SR_PS is set, not the other way around
      (that is, set SR_S if SR_PS is not set).

--- riscv/insns/eret.h
+++ riscv/insns/eret.h
@@ -1,5 +1,5 @@
 require_supervisor;
 set_pcr(PCR_SR, ((sr & ~(SR_S | SR_EI)) |
-                ((sr & SR_PS) ? 0 : SR_S)) |
+                ((sr & SR_PS) ? SR_S : 0)) |
                 ((sr & SR_PEI) ? SR_EI : 0));
 set_pc(epc);
