620d8bc0fe8cf8e8d3425db7ea2e6aaba145c86d
Andrew Waterman <waterman@cs.berkeley.edu>
2013-09-15 04:27:07 -0700

    Add helper disassembly program

---
+++ riscv/riscv-dis.cc
@@ -0,0 +1,41 @@
+// See LICENSE for license details.
+
+// This little program finds occurrences of strings like
+//  DASM(ffabc013)
+// in its input, then replaces them with the disassembly
+// enclosed hexadecimal number, interpreted as a RISC-V
+// instruction.
+
+#include "disasm.h"
+#include <iostream>
+#include <string>
+#include <cstdint>
+using namespace std;
+
+int main()
+{
+  string s;
+  disassembler d;
+
+  while (getline(cin, s))
+  {
+    for (size_t start = 0; (start = s.find("DASM(", start)) != string::npos; )
+    {
+      size_t end = s.find(')', start);
+      if (end == string::npos)
+        break;
+
+      size_t numstart = start + strlen("DASM(");
+      uint32_t n = strtoul(&s[numstart], NULL, 16);
+
+      string dis = d.disassemble(*(insn_t*)&n);
+
+      s = s.substr(0, start) + dis + s.substr(end+1);
+      start += dis.length();
+    }
+
+    cout << s << '\n';
+  }
+
+  return 0;
+}
