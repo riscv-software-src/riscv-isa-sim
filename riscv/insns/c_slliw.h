6025b02665744b71d442912a9895b917cff8590f
Andrew Waterman <waterman@cs.berkeley.edu>
2015-05-21 11:55:52 -0700

    Fix c.slliw implementation

--- riscv/insns/c_slliw.h
+++ riscv/insns/c_slliw.h
@@ -1,3 +1,5 @@
 require_extension('C');
 require_rv64;
-WRITE_RD(sext_xlen(RVC_RS1 << insn.rvc_imm()));
+require(insn.rvc_rd() != 0);
+require(insn.rvc_imm() < 32);
+WRITE_RD(sext32(RVC_RS1 << insn.rvc_imm()));
