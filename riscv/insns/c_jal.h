0f622f0e89ff62022b59ac0124fcdf8198da020d
Andrew Waterman <waterman@cs.berkeley.edu>
2015-11-19 19:00:14 -0800

    C.ADDIW is reserved for rd=0

--- riscv/insns/c_jal.h
+++ riscv/insns/c_jal.h
@@ -3,6 +3,7 @@ if (xlen == 32) {
   reg_t tmp = npc;
   set_pc(pc + insn.rvc_j_imm());
   WRITE_REG(X_RA, tmp);
-} else {
+} else { // c.addiw
+  require(insn.rvc_rd() != 0);
   WRITE_RD(sext32(RVC_RS1 + insn.rvc_imm()));
 }
