bf8d2b71bb76b03b9a4db36fe6928c29a41a43e7
Andrew Waterman <andrew@sifive.com>
2016-11-10 13:40:37 -0800

    AMOs should always return store faults, not load faults
    This commit also factors out the common AMO code into mmu_t.

--- riscv/insns/amomax_d.h
+++ riscv/insns/amomax_d.h
@@ -1,5 +1,3 @@
 require_extension('A');
 require_rv64;
-sreg_t v = MMU.load_int64(RS1);
-MMU.store_uint64(RS1, std::max(sreg_t(RS2),v));
-WRITE_RD(v);
+WRITE_RD(MMU.amo_uint64(RS1, [&](int64_t lhs) { return std::max(lhs, int64_t(RS2)); }));
