f2e8eb28cca2a5f49cea9f37774058718f795fdd
Yunsup Lee <yunsup@sifive.com>
2017-03-31 19:14:19 -0700

    update encoding.h to get PMP updates

--- riscv/encoding.h
+++ riscv/encoding.h
@@ -127,12 +127,13 @@
 #define PMP_R     0x01
 #define PMP_W     0x02
 #define PMP_X     0x04
-#define PMP_M     0x08
-#define PMP_NAPOT 0x10
-#define PMP_TOR   0x20
-#define PMP_EN    0x40
-#define PMP_LOCK  0x80
+#define PMP_A     0x18
+#define PMP_L     0x80
 #define PMP_SHIFT 2
+
+#define PMP_TOR   0x08
+#define PMP_NA4   0x10
+#define PMP_NAPOT 0x18
 
 #define IRQ_S_SOFT   1
 #define IRQ_H_SOFT   2
