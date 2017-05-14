e465de145c69c28230b5c73cc58f96fd9bd04419
Andrew Waterman <andrew@sifive.com>
2017-05-13 22:37:22 -0700

    Make C.LI/C.LUI trapping behavior match spec

--- riscv/insns/c_lui.h
+++ riscv/insns/c_lui.h
@@ -3,6 +3,6 @@ if (insn.rvc_rd() == 2) { // c.addi16sp
   require(insn.rvc_addi16sp_imm() != 0);
   WRITE_REG(X_SP, sext_xlen(RVC_SP + insn.rvc_addi16sp_imm()));
 } else {
-  require(insn.rvc_rd() != 0);
+  require(insn.rvc_imm() != 0);
   WRITE_RD(insn.rvc_imm() << 12);
 }
