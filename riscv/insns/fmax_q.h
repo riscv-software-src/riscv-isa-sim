27ffc270f4e08862606e3532a87556e2f16fa87b
Andrew Waterman <andrew@sifive.com>
2017-10-19 12:18:23 -0700

    Fix implementation of FMIN/FMAX NaN case
    If rd=rs1 or rd=rs2, the NaN check examined the wrong value.

--- riscv/insns/fmax_q.h
+++ riscv/insns/fmax_q.h
@@ -2,7 +2,8 @@ require_extension('Q');
 require_fp;
 bool greater = f128_lt_quiet(f128(FRS2), f128(FRS1)) ||
                (f128_eq(f128(FRS2), f128(FRS1)) && (f128(FRS2).v[1] & F64_SIGN));
-WRITE_FRD(greater || isNaNF128(f128(FRS2)) ? FRS1 : FRS2);
 if (isNaNF128(f128(FRS1)) && isNaNF128(f128(FRS2)))
   WRITE_FRD(f128(defaultNaNF128()));
+else
+  WRITE_FRD(greater || isNaNF128(f128(FRS2)) ? FRS1 : FRS2);
 set_fp_exceptions;
