d2e9a109e8f7b851fd153b469cc42a8519d85679
Andrew Waterman <andrew@sifive.com>
2018-04-30 20:20:43 -0700

    Fix commit log for serializing instructions
    Resolves #199

--- riscv/execute.cc
+++ riscv/execute.cc
@@ -76,7 +76,7 @@ static reg_t execute_insn(processor_t* p, reg_t pc, insn_fetch_t fetch)
 {
   commit_log_stash_privilege(p);
   reg_t npc = fetch.func(p, fetch.insn, pc);
-  if (!invalid_pc(npc)) {
+  if (npc != PC_SERIALIZE_BEFORE) {
     commit_log_print_insn(p->get_state(), pc, fetch.insn);
     p->update_histogram(pc);
   }
