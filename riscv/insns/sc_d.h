9b260eff8de8976aed81e85084f6aaae930bf43b
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2018-07-09 10:02:53 -0700

    Fix missing reservation yields, in sfence.vma and after a sc. Fixes #215.

--- riscv/insns/sc_d.h
+++ riscv/insns/sc_d.h
@@ -2,6 +2,7 @@ require_extension('A');
 require_rv64;
 if (RS1 == p->get_state()->load_reservation)
 {
+  p->yield_load_reservation();
   MMU.store_uint64(RS1, RS2);
   WRITE_RD(0);
 }
--- riscv/insns/sfence_vma.h
+++ riscv/insns/sfence_vma.h
@@ -1,2 +1,3 @@
 require_privilege(get_field(STATE.mstatus, MSTATUS_TVM) ? PRV_M : PRV_S);
 MMU.flush_tlb();
+p->yield_load_reservation();
