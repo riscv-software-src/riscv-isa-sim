606314955661d65ac080fc9a8ff198cfed8be1ad
Andrew Waterman <andrew@sifive.com>
2018-09-20 17:31:11 -0700

    For backwards compatibility, reset PMP to permit all accesses

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -128,6 +128,9 @@ void state_t::reset(reg_t max_isa)
   tselect = 0;
   for (unsigned int i = 0; i < num_triggers; i++)
     mcontrol[i].type = 2;
+
+  pmpcfg[0] = PMP_R | PMP_W | PMP_X | PMP_NAPOT;
+  pmpaddr[0] = ~reg_t(0);
 }
 
 void processor_t::set_debug(bool value)
