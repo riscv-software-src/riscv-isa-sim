77170c38839eb8996dad498ba4a189d0a4df38df
Ronald G. Minnich <rminnich@gmail.com>
2018-09-24 13:34:26 +0200

    uart: add uart input support
    This lets you do in, as well as out.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    Signed-off-by: Jonathan Neuschäfer <j.neuschaefer@gmx.net>

--- riscv/uart.cc
+++ riscv/uart.cc
@@ -2,6 +2,7 @@
 #include "devices.h"
 #include "processor.h"
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 enum {
   UART_DATA = 0,                  /* Data RX/TX */
@@ -15,12 +16,16 @@ enum {
 };
 
 enum {
+  UART_LINE_STAT_DR = (1 << 0), /* Data ready */
   UART_LINE_STAT_THRE = (1 << 5), /* Transmitter holding register empty */
   UART_LINE_STAT_TEMT = (1 << 6), /* Transmitter completely empty */
 };
 
 bool uart_t::load(reg_t addr, size_t len, uint8_t* bytes)
 {
+  int amt;
+  char c;
+
   /* For simplicity on our side, only support byte-sized accesses for now. */
   if (len > 1)
     return false;
@@ -29,6 +34,14 @@ bool uart_t::load(reg_t addr, size_t len, uint8_t* bytes)
   {
     case UART_LINE_STAT:
       *bytes = UART_LINE_STAT_TEMT | UART_LINE_STAT_THRE;
+      if ((ioctl(0, FIONREAD, &amt) == 0) && (amt > 0))
+        *bytes |= UART_LINE_STAT_DR;
+      break;
+    case UART_DATA:
+      *bytes = 0;
+      if ((ioctl(0, FIONREAD, &amt) == 0) && (amt > 0))
+        if (read(0, bytes, 1))
+          ; // Not much point in a warning but C++ demands something.
       break;
     default:
       *bytes = 0;
