01252686902fa30665fbecfc1476d169ad1333d1
Andrew Waterman <andrew@sifive.com>
2018-11-06 15:41:34 -0800

    Report misaligned-address exception on failed store-conditionals
    Previously, the exception would only be raised if the store-conditional
    would have succeeded.

--- riscv/insns/sc_d.h
+++ riscv/insns/sc_d.h
@@ -1,11 +1,8 @@
 require_extension('A');
 require_rv64;
-if (MMU.check_load_reservation(RS1))
-{
-  MMU.store_uint64(RS1, RS2);
-  WRITE_RD(0);
-}
-else
-  WRITE_RD(1);
 
+bool have_reservation = MMU.check_load_reservation(RS1);
+MMU.amo_uint64(RS1, [&](uint64_t lhs) { return have_reservation ? RS2 : lhs; });
 MMU.yield_load_reservation();
+
+WRITE_RD(!have_reservation);
