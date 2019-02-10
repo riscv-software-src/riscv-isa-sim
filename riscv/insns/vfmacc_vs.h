5699b06e5446d7af6aec69a428e8673162b94f43
Bruce Hoult <bruce@hoult.org>
2019-02-10 02:38:56 -0800

    fix vfmacc_vs

--- riscv/insns/vfmacc_vs.h
+++ riscv/insns/vfmacc_vs.h
@@ -1,15 +1,15 @@
 reg_t vl = STATE.VU.vl;
 reg_t accVReg = insn.rd();
 reg_t srcVReg = insn.rs2();
-float a = f32(FRS1).v;
+float32_t a = f32(FRS1);
 softfloat_roundingMode = RM;
 for (reg_t i=STATE.VU.vstart; i<vl; ++i){
-  float x = STATE.VU.elt<float>(srcVReg, i);
-  float y = STATE.VU.elt<float>(accVReg, i);
-  float r = f32_mulAdd({a}, {x}, {y}).v;
-  STATE.VU.elt<float>(accVReg, i) = r;
+  float32_t x = STATE.VU.elt<float32_t>(srcVReg, i);
+  float32_t y = STATE.VU.elt<float32_t>(accVReg, i);
+  float32_t r = f32_mulAdd(a, x, y);
+  STATE.VU.elt<float32_t>(accVReg, i) = r;
   #if 0
-  printf("vfmacc_vs(%lu) %f * %f + %f = %f\n", i, a, x, y, r);
+  printf("vfmacc_vs(%lu) %f * %f + %f = %f\n", i, to_f(a), to_f(x), to_f(y), to_f(r));
   #endif
 }
 STATE.VU.vstart = 0;
