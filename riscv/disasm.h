9a46ecee8d77de79f23420884a0798d9b26f2fda
Bruce Hoult <bruce@hoult.org>
2019-02-24 08:55:29 -0800

    typo in vector disassembly

--- riscv/disasm.h
+++ riscv/disasm.h
@@ -10,7 +10,7 @@
 
 extern const char* xpr_name[NXPR];
 extern const char* fpr_name[NFPR];
-extern const char* vr_name[NFPR];
+extern const char* vr_name[NVPR];
 extern const char* csr_name(int which);
 
 class arg_t
