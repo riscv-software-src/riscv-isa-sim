5de7794efbb00cab21d912a80b4c91de2e6c2810
Chih-Min Chao <chihmin.chao@sifive.com>
2019-03-28 08:44:47 -0700

    rvv: imple vmpopc_m.h
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmpopc_m.h
+++ riscv/insns/vmpopc_m.h
@@ -1,5 +1,14 @@
-// vmpopc
-VI_VV_LOOP
-({
-    // NOT IMPLEMENTED YET
-})
+// vmpopc rd, vs2, vm
+require(STATE.VU.vstart == 0);
+reg_t popcount = 0;
+VI_LOOP_BASE
+  bool has_count = STATE.VU.elt<int8_t>(rd_num,  (STATE.VU.vsew / 8) * i) & 0x1;
+  bool do_mask = false;
+  V_CHECK_MASK(do_mask);
+
+  if ((do_mask && has_count) || has_count)
+    popcount++;
+
+VI_LOOP_END
+WRITE_RD(popcount);
+
