d849d6a96d75a50d77d677422b78ee34ebc686b0
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:24:37 -0700

    rvv: add vfmul_v[vf]

--- riscv/insns/vfmul_vf.h
+++ riscv/insns/vfmul_vf.h
@@ -1,5 +1,16 @@
 // vfmul
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
 VFP_VF_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    vd = f32_mul(rs1, vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
--- riscv/insns/vfmul_vv.h
+++ riscv/insns/vfmul_vv.h
@@ -1,5 +1,16 @@
 // vfmul.vv vd, vs1, vs2, vm
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
 VFP_VV_LOOP
 ({
-  vd = f32_mul(vs1, vs2);
- })
+ switch(STATE.VU.vsew){
+ case e32:
+    vd = f32_mul(vs1, vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
+})
+set_fp_exceptions;
