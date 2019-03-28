ffb7800e21be4c3abbf03a2b31a1fb804cb89848
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:25:12 -0700

    rvv: add vfmv_f_s and vfmv_s_f

--- riscv/insns/vfmv_f_s.h
+++ riscv/insns/vfmv_f_s.h
@@ -1,5 +1,9 @@
-// vfmv_f_s
-VFP_VV_LOOP
-({
-    // NOT IMPLEMENTED YET
-})
+// vfmv_f_s: rd = vs2[0] (rs1=0)
+require_fp;
+require(STATE.VU.vsew == e8 || STATE.VU.vsew == e16 || STATE.VU.vsew == e32 || STATE.VU.vsew == e64); 
+
+reg_t rs2_num = insn.rs2(); 
+
+int32_t vs2 = STATE.VU.elt<int32_t>(rs2_num, 0); 
+uint64_t sew_mask = (1 << STATE.VU.vsew) - 1;
+WRITE_RD(vs2 & sew_mask);
--- riscv/insns/vfmv_s_f.h
+++ riscv/insns/vfmv_s_f.h
@@ -1,5 +1,16 @@
-// vfmv_s_f
-VFP_VF_LOOP
-({
-    // NOT IMPLEMENTED YET
-})
+// vfmv_s_f: vd[0] = rs1 (vs2=0)
+require_fp;
+require(STATE.VU.vsew == e8 || STATE.VU.vsew == e16 || STATE.VU.vsew == e32 || STATE.VU.vsew == e64); 
+
+// SEW < FLEN ?
+if (STATE.VU.vsew > FLEN){
+// the least-significant FLEN bits are transferred and the upper SEW-FLEN bits are ignored.
+    
+}else if (STATE.VU.vsew < FLEN){
+// the value is NaN-boxed (1-extended) to FLEN bits.
+}
+
+
+reg_t rd_num = insn.rd(); 
+float32_t&vd = STATE.VU.elt<float32_t>(rd_num, 0); \
+vd = f32(FRS1);
