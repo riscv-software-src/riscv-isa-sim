71a1d7a6010d628e90a760cc24901a39a0dadb87
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:22:04 -0700

    rvv: add vf[n]madd_v[vf]

--- riscv/insns/vfmadd_vv.h
+++ riscv/insns/vfmadd_vv.h
@@ -1,5 +1,17 @@
-// vfmadd
+// vfmadd: vd[i] = +(vd[i] * vs1[i]) + vs2[i]
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
+
 VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    vd = f32_mulAdd(vd, vs1, vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
--- riscv/insns/vfnmadd_vf.h
+++ riscv/insns/vfnmadd_vf.h
@@ -1,5 +1,19 @@
-// vfnmadd
+// vfnmadd: vd[i] = -(vd[i] * f[rs1]) - vs2[i]
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
+float32_t mul_result;
+
 VFP_VF_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    mul_result = f32_mul(f32(vd.v ^ F32_SIGN), rs1);
+    vd = f32_sub(mul_result, vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
