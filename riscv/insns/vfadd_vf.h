2f3b68ee97429848b2ebab9ddeca45d229426fa7
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:19:58 -0700

    rvv: add vfadd_v[vf], vfsub_v[vf]

--- riscv/insns/vfadd_vf.h
+++ riscv/insns/vfadd_vf.h
@@ -1,5 +1,16 @@
 // vfadd
-VFP_VF_LOOP
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
+VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    vd = f32_add(f32(RS1), vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
