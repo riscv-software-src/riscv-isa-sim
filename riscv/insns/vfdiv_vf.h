26ae001654eb452a0e274f056c1349deb908cc1a
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:20:42 -0700

    rvv: add vfdiv_v[vf]

--- riscv/insns/vfdiv_vf.h
+++ riscv/insns/vfdiv_vf.h
@@ -1,5 +1,16 @@
 // vfdiv
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
 VFP_VF_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    vd = f32_div(rs1, vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
