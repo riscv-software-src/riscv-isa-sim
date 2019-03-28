6e3ff8e6660df563fafa085688b0b86330afa11e
Dave.Wen <dave.wen@sifive.com>
2019-03-28 06:38:57 -0700

    rvv: add vfeq_v[vf]

--- riscv/insns/vfeq_vf.h
+++ riscv/insns/vfeq_vf.h
@@ -1,5 +1,17 @@
 // vfeq
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
 VFP_VF_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ bool &vd = STATE.VU.elt<bool>(rd_num, i); \
+ switch(STATE.VU.vsew){
+ case e32:
+    vd = f32_eq(f32(rs1), vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
