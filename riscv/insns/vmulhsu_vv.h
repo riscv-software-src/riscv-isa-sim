cfa5b3650fc13295e7ffd3391fcd4a80760af011
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:13:04 -0700

    rvv: add vmulhu_[vx] and vmulhsu_[vx]

--- riscv/insns/vmulhsu_vv.h
+++ riscv/insns/vmulhsu_vv.h
@@ -1,5 +1,11 @@
-// vmulhsu
+// vmulhsu: Signed multiply, returning high bits of product
+require(STATE.VU.ELEN <= 32);
+reg_t vsew = STATE.VU.vsew;
+uint64_t sew_result_bits = vsew * 2;
+uint64_t upper_mask = 1 - ((1 << vsew) - 1);
+
 VI_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+    int64_t result = vs1 * vs2;
+    vd = (result>>(sew_result_bits - vsew)) & upper_mask;
 })
--- riscv/insns/vmulhsu_vx.h
+++ riscv/insns/vmulhsu_vx.h
@@ -1,5 +1,11 @@
-// vmulhsu
-VI_VX_LOOP
+// vmulhsu: Signed multiply, returning high bits of product
+require(STATE.VU.ELEN <= 32);
+reg_t vsew = STATE.VU.vsew;
+uint64_t sew_result_bits = vsew * 2;
+uint64_t upper_mask = 1 - ((1 << vsew) - 1);
+
+VI_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+    int64_t result = RS1 * vs2;
+    rd = (result>>(sew_result_bits - vsew)) & upper_mask;
 })
