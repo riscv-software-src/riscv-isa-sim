3c85d7979c2e43ae7c1316c2bf103b1c9f736f0c
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:23:23 -0700

    rvv: add vf[n]msub_v[vf]

--- riscv/insns/vfmsub_vf.h
+++ riscv/insns/vfmsub_vf.h
@@ -1,5 +1,19 @@
-// vfmsub
-VFP_VF_LOOP
+// vfmsub: vd[i] = +(vd[i] * f[rs1]) - vs2[i]
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
+float32_t mul_result;
+
+VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    mul_result = f32_mul(vd, rs1);
+    vd = f32_sub(mul_result, vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
--- riscv/insns/vfmsub_vv.h
+++ riscv/insns/vfmsub_vv.h
@@ -1,5 +1,20 @@
-// vfmsub
+// vfmsub: vd[i] = +(vd[i] * vs1[i]) - vs2[i]
+
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
+float32_t mul_result;
+
 VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    mul_result = f32_mul(vd, vs1);
+    vd = f32_sub(mul_result, vs2);
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
