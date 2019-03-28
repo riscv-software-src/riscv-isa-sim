4d1ea70662d680283581bca9fc1de084cf60d636
Dave.Wen <dave.wen@sifive.com>
2019-03-28 03:24:02 -0700

    rvv: add vfmax_v[vf], vfmin_v[vf]

--- riscv/insns/vfmax_vv.h
+++ riscv/insns/vfmax_vv.h
@@ -1,5 +1,24 @@
 // vfmax
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
+bool greater;
+
 VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    greater = f32_lt_quiet(vs2, vs1) || (f32_eq(vs2, vs1) && (vs2.v & F32_SIGN));
+
+    if(isNaNF32UI(vs1.v) && isNaNF32UI(vs2.v)){
+      vd = f32(defaultNaNF32UI);
+    }else{
+      vd = (greater || isNaNF32UI(vs2.v) ? vs1 : vs2);
+    }
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
--- riscv/insns/vfmin_vv.h
+++ riscv/insns/vfmin_vv.h
@@ -1,5 +1,24 @@
-// vfmin
+// vfmin:
+require_fp;
+softfloat_roundingMode = STATE.VU.vxrm;
+bool less;
+
 VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    less = f32_lt_quiet(vs1, vs2) || (f32_eq(vs1, vs2) && (vs1.v & F32_SIGN));
+
+    if(isNaNF32UI(vs1.v) && isNaNF32UI(vs2.v)){
+      vd = f32(defaultNaNF32UI);
+    }else{
+      vd = (less || isNaNF32UI(vs2.v) ? vs1 : vs2);
+    }
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
