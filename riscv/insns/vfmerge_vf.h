bdb71e8138085ba81ff01cce2b02f48c7519be1a
Chih-Min Chao <chihmin.chao@sifive.com>
2019-03-28 18:27:36 -0700

    rvv: add vfmerge.vf
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfmerge_vf.h
+++ riscv/insns/vfmerge_vf.h
@@ -1,5 +1,10 @@
-// vfmerge_vf
+// vfmerge_vf vd, vs2, vs1, vm
 VFP_VF_LOOP
 ({
-    // NOT IMPLEMENTED YET
+  bool do_mask = false;
+
+  V_CHECK_MASK(do_mask);
+
+  bool use_first = (insn.v_vm() == 1) || do_mask;
+  vd = use_first ? vs2 : rs1;
 })
