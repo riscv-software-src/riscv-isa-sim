7f8292335aaf2935dd012be9044d15287e85d318
Chih-Min Chao <chihmin.chao@sifive.com>
2019-03-28 19:30:08 -0700

    rvv: fix vmerge[xi] operand use and apply V_CHECK_MASK macro
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmerge_vi.h
+++ riscv/insns/vmerge_vi.h
@@ -1,26 +1,21 @@
-// vmerge.vi vd, vs2, imm, vm
+// vmerge.vi vd, vs2, simm5, vm
 VI_VI_LOOP
 ({
   bool do_mask = false;
-
-  if (insn.v_vm() == 0) {
-    int midx = (STATE.VU.vmlen * i) / 32;
-    int mpos = (STATE.VU.vmlen * i) % 32;
-    do_mask = (STATE.VU.elt<uint32_t>(0, midx) >> mpos) & 0x1;
-  }
+  V_CHECK_MASK(do_mask);
 
   bool use_first = (insn.v_vm() == 1) || do_mask;
   switch (STATE.VU.vsew) {
   case e8:
-    STATE.VU.elt<uint8_t>(rd_num, i) = use_first ? rs1 : vs2;
+    STATE.VU.elt<uint8_t>(rd_num, i) = use_first ? simm5 : vs2;
     break;
   case e16:
-    STATE.VU.elt<uint16_t>(rd_num, i) = use_first ? rs1 : vs2;
+    STATE.VU.elt<uint16_t>(rd_num, i) = use_first ? simm5 : vs2;
     break;
   case e32:
-    STATE.VU.elt<uint32_t>(rd_num, i) = use_first ? rs1 : vs2;
+    STATE.VU.elt<uint32_t>(rd_num, i) = use_first ? simm5 : vs2;
     break;
   default:
-    STATE.VU.elt<uint64_t>(rd_num, i) = use_first ? rs1 : vs2;
+    STATE.VU.elt<uint64_t>(rd_num, i) = use_first ? simm5 : vs2;
   }
 })
