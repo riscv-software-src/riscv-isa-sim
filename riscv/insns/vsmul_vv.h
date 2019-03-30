2880218488276c80bdc8893f75a627c694346492
Dave.Wen <dave.wen@sifive.com>
2019-03-29 17:33:59 -0700

    rvv: add vsmul_v[vx]

--- riscv/insns/vsmul_vv.h
+++ riscv/insns/vsmul_vv.h
@@ -1,5 +1,31 @@
-// vsmul
+// vsmul: Signed saturating and rounding fractional multiply
+int64_t round = 0;
+VRM vrm = STATE.VU.get_vround_mode();
 VI_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+    int64_t result = vsext(vs1 * vs2, sew * 2);
+    // rounding
+    switch(vrm){
+        case VRM::RUN:
+            result += (1 << (sew - 1) );
+            break;
+        case VRM::RNE:
+            assert(true);
+            break;
+        case VRM::RDN:
+            result = (result >> (sew - 1)) << (sew - 1);
+            break;
+        case VRM::ROD:
+            if ((result & (1 << ((sew -1) - 1 ))) > 0){
+                result = ((result >> (sew - 1)) + 1) << (sew - 1);
+            }
+            break;
+        case VRM::INVALID_RM:
+            assert(true);
+    };    
+    
+    // saturation
+    if (result >= (int64_t)(2^(sew - 1)))
+        result = (2^(sew - 1)) - 1;
+    
 })
--- riscv/insns/vsmul_vx.h
+++ riscv/insns/vsmul_vx.h
@@ -1,5 +1,31 @@
 // vsmul
+int64_t round = 0;
+VRM vrm = STATE.VU.get_vround_mode();
 VI_VX_LOOP
 ({
-    // NOT IMPLEMENTED YET
+    int64_t result = vsext(rs1 * vs2, sew * 2);
+    // rounding
+    switch(vrm){
+        case VRM::RUN:
+            result += (1 << (sew - 1) );
+            break;
+        case VRM::RNE:
+            assert(true);
+            break;
+        case VRM::RDN:
+            result = (result >> (sew - 1)) << (sew - 1);
+            break;
+        case VRM::ROD:
+            if ((result & (1 << ((sew -1) - 1 ))) > 0){
+                result = ((result >> (sew - 1)) + 1) << (sew - 1);
+            }
+            break;
+        case VRM::INVALID_RM:
+            assert(true);
+    };    
+    
+    // saturation
+    if (result >= (int64_t)(2^(sew - 1)))
+        result = (2^(sew - 1)) - 1;
+    
 })
