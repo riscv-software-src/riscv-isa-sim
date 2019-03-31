b59625b34ef809fe5a297b6a971f2dc6e1a7e778
Dave.Wen <dave.wen@sifive.com>
2019-03-31 08:28:41 -0700

    rvv: add vnclip[u]_v[vxi]

--- riscv/insns/vnclip_vi.h
+++ riscv/insns/vnclip_vi.h
@@ -1,5 +1,39 @@
-// vnclip
+// vnclip: vd[i] = clip(round(vs2[i] + rnd) >> simm)
+int64_t round = 0;
+VRM vrm = STATE.VU.get_vround_mode();
+reg_t sew_lo;
 VI_VI_LOOP
 ({
-    // NOT IMPLEMENTED YET
+    if (i % 2 == 0){
+        sew_lo = vs2;
+        continue;
+    }
+    
+    int64_t result = (vs2<<sew) | sew_lo;
+    // rounding
+    switch(vrm){
+        case VRM::RUN:
+            result += (1 << (sew - 1) );
+            break;
+        case VRM::RNE:
+            assert(true);
+            break;
+        case VRM::RDN:
+            result = (result >> (sew - 1)) << (sew - 1);
+            break;
+        case VRM::ROD:
+            if ((result & (1 << ((sew -1) - 1 ))) > 0){
+                result = ((result >> (sew - 1)) + 1) << (sew - 1);
+            }
+            break;
+        case VRM::INVALID_RM:
+            assert(true);
+    };    
+    // unsigned shifting to rs1
+    result = result >> simm5;
+
+    // saturation
+    if (result >= (int64_t)(2^(sew - 1)))
+        result = (2^(sew - 1)) - 1;
+    
 })
--- riscv/insns/vnclipu_vx.h
+++ riscv/insns/vnclipu_vx.h
@@ -1,5 +1,39 @@
-// vnclipu
+// vnclipu: vd[i] = clip(round(vs2[i] + rnd) >> rs1[i])
+int64_t round = 0;
+VRM vrm = STATE.VU.get_vround_mode();
+reg_t sew_lo;
 VI_VX_LOOP
 ({
-    // NOT IMPLEMENTED YET
+    if (i % 2 == 0){
+        sew_lo = vs2;
+        continue;
+    }
+    
+    uint64_t result = (vs2<<sew) | sew_lo;
+    // rounding
+    switch(vrm){
+        case VRM::RUN:
+            result += (1 << (sew - 1) );
+            break;
+        case VRM::RNE:
+            assert(true);
+            break;
+        case VRM::RDN:
+            result = (result >> (sew - 1)) << (sew - 1);
+            break;
+        case VRM::ROD:
+            if ((result & (1 << ((sew -1) - 1 ))) > 0){
+                result = ((result >> (sew - 1)) + 1) << (sew - 1);
+            }
+            break;
+        case VRM::INVALID_RM:
+            assert(true);
+    };    
+    // unsigned shifting to rs1
+    result = result >> rs1;
+
+    // saturation
+    if (result >= (uint64_t)(2^(sew - 1)))
+        result = (2^(sew - 1)) - 1;
+    
 })
