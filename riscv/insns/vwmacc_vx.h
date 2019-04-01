5efcf97b0a17375b6c9cf4f817d5682004466312
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-01 02:19:47 -0700

    rvv: fix operaned using for wide instruciton
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vwmacc_vx.h
+++ riscv/insns/vwmacc_vx.h
@@ -1,6 +1,6 @@
 // vwmacc.vx vd, vs2, rs1
 V_WIDE_CHECK;
-VI_VV_LOOP
+VI_VX_LOOP
 ({
   V_WIDE_OP_AND_ASSIGN(vs2, rs1, vd, *, +, int);
 })
--- riscv/insns/vwmsac_vv.h
+++ riscv/insns/vwmsac_vv.h
@@ -1,6 +1,6 @@
 // vwmsac.vv vd, vs2, vs1
 V_WIDE_CHECK;
-VI_VX_LOOP
+VI_VV_LOOP
 ({
   V_WIDE_OP_AND_ASSIGN(vs2, vs1, vd, *, -, int);
 })
