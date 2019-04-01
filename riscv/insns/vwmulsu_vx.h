24f5ed32946e439fe8a41dee993f03ab0d7a6410
Chih-Min Chao <chihmin.chao@sifive.com>
2019-03-31 22:54:52 -0700

    rvv: add wvmulsu.v[vx]
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vwmulsu_vx.h
+++ riscv/insns/vwmulsu_vx.h
@@ -1,5 +1,16 @@
-// vwmulsu
+// vwmulsu vd, vs2, vs1
+V_WIDE_CHECK;
 VI_VX_LOOP
 ({
-    // NOT IMPLEMENTED YET
+  switch(STATE.VU.vsew) {
+  case e8:
+    STATE.VU.elt<uint16_t>(rd_num, i) = (int16_t)(int8_t)vs2 * (int16_t)(uint8_t)rs1;
+    break;
+  case e16:
+    STATE.VU.elt<uint32_t>(rd_num, i) = (int32_t)(int16_t)vs2 * (int32_t)(uint16_t)rs1;
+    break;
+  default:
+    STATE.VU.elt<uint64_t>(rd_num, i) = (int64_t)(int32_t)vs2 * (int64_t)(uint32_t)rs1;
+    break;
+  }
 })
