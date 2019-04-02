24f1a81e0f1e6a6254820158feb0f759bbd9eab7
Dave.Wen <dave.wen@sifive.com>
2019-04-01 19:16:43 -0700

    rvv: add vfsg[nx]_v[vf]

--- riscv/insns/vfsgnn_vv.h
+++ riscv/insns/vfsgnn_vv.h
@@ -1,5 +1,16 @@
 // vfsgnn
+softfloat_roundingMode = STATE.VU.vxrm;
 VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    //fsgnj32(vs1, vs2, true, false));
+    vd = f32((vs1.v & ~F32_SIGN) | ((((false) ? vs2.v : (true) ? F32_SIGN : 0) ^ vs2.v) & F32_SIGN));
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
--- riscv/insns/vfsgnx_vv.h
+++ riscv/insns/vfsgnx_vv.h
@@ -1,5 +1,16 @@
 // vfsgnx
+softfloat_roundingMode = STATE.VU.vxrm;
 VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+ switch(STATE.VU.vsew){
+ case e32:
+    //fsgnj32(vs1, vs2, false, true));
+    vd = f32((vs1.v & ~F32_SIGN) | ((((true) ? vs2.v : (false) ? F32_SIGN : 0) ^ vs2.v) & F32_SIGN));
+    break;
+ case e16:
+ case e8:
+ default:
+     softfloat_exceptionFlags = 1;
+ };
 })
+set_fp_exceptions;
