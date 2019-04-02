b1bff4882ba4575b9b941641c5bb959b79992dfc
Dave.Wen <dave.wen@sifive.com>
2019-04-01 17:28:27 -0700

    rvv: add vfredosum_vs.h

--- riscv/insns/vfredosum_vs.h
+++ riscv/insns/vfredosum_vs.h
@@ -1,5 +1,20 @@
-// vfredosum
+// vfredosum: vd[0] =  sum( vs2[*] , vs1[0]  )
+softfloat_roundingMode = STATE.VU.vxrm;
+float32_t sum;
+float32_t vs1_0;
 VFP_VV_LOOP
 ({
-    // NOT IMPLEMENTED YET
+    if(i == 0){
+        vs1_0 = vs1;
+    }
+    switch(STATE.VU.vsew){
+    case e32:
+       sum = f32_add(sum, f32_add(vs2, vs1_0));
+       break;
+    case e16:
+    case e8:
+    default:
+        softfloat_exceptionFlags = 1;
+    };
+    vd = sum;
 })
