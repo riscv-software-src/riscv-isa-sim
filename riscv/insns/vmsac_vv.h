9d2b4ec074147ad7672d9bf89b8c6bf438f1265a
Dave <dave.wen@sifive.com>
2019-04-11 16:24:28 -0700

    rvv: fixed the vmsac*

--- riscv/insns/vmsac_vv.h
+++ riscv/insns/vmsac_vv.h
@@ -1,7 +1,7 @@
 // vmsac: vd[i] = -(vs1[i] * vs2[i]) + vd[i]
 VI_VV_LOOP
 ({
-    int64_t result = vsext(vd - ((int32_t)vd * (int32_t)vs1), sew);
+    int64_t result = vsext(vd - ((int32_t)vs1 * (int32_t)vs2), sew);
     result &= ((1 << sew) - 1);
     vd = result;
 })
