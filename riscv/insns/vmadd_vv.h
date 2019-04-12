fa20cd3571490a901274b5b0d278d0a58af5aa3f
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-12 03:09:01 -0700

    rvv: simply vmadd, we have correct type don't need masking
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmadd_vv.h
+++ riscv/insns/vmadd_vv.h
@@ -1,7 +1,7 @@
 // vmadd: vd[i] = (vd[i] * vs1[i]) + vs2[i]
 VI_VV_LOOP
 ({
-    int64_t result = ((int32_t)vd * (int32_t)vs1) + vs2;
-    result &= ((1 << sew) - 1);
-    vd = vsext(result, sew);
+  V_LOOP_ELEMENT_SKIP;
+
+  vd = vd * vs1 + vs2;
 })
--- riscv/insns/vmadd_vx.h
+++ riscv/insns/vmadd_vx.h
@@ -1,7 +1,7 @@
 // vmadd: vd[i] = (vd[i] * x[rs1]) + vs2[i]
 VI_VX_LOOP
 ({
-    int64_t result = ((int32_t)vd * (int32_t)rs1) + vs2;
-    result &= ((1 << sew) - 1);
-    vd = vsext(result, sew);
+  V_LOOP_ELEMENT_SKIP;
+
+  vd = vd * rs1 + vs2;
 })
