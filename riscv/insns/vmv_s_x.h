9192c166011f9ccbc85b72959c36bf6fa978e993
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-15 03:04:17 -0700

    fix vmv.s.x, it set other part as 0
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmv_s_x.h
+++ riscv/insns/vmv_s_x.h
@@ -1,22 +1,45 @@
 // vmv_s_x: vd[0] = rs1
 // this is wordaround implementation. The spec uses rs1
-VI_LOOP_BASE
+//
+require(STATE.VU.vsew == e8 || STATE.VU.vsew == e16 ||
+        STATE.VU.vsew == e32 || STATE.VU.vsew == e64);
+reg_t vl = STATE.VU.vl;
+reg_t sew = STATE.VU.vsew;
+reg_t rd_num = insn.rd();
 
-  uint64_t val = RS1;
+if (vl == 0) {
+  STATE.VU.vstart = 0;
+} else {
   switch(sew) {
   case e8:
-    STATE.VU.elt<uint8_t>(rd_num, i) = RS2;
+    STATE.VU.elt<uint8_t>(rd_num, 0) = RS2;
     break;
   case e16:
-    STATE.VU.elt<uint16_t>(rd_num, i) = RS2;
+    STATE.VU.elt<uint16_t>(rd_num, 0) = RS2;
     break;
   case e32:
-    STATE.VU.elt<uint32_t>(rd_num, i) = RS2;
+    STATE.VU.elt<uint32_t>(rd_num, 0) = RS2;
     break;
   default:
-    STATE.VU.elt<uint64_t>(rd_num, i) = RS2;
+    STATE.VU.elt<uint64_t>(rd_num, 0) = RS2;
     break;
   }
 
-  break;
-VI_LOOP_END 
+  const reg_t max_len = STATE.VU.VLEN / sew;
+  for (reg_t i = 1; i < max_len; ++i){
+    switch(sew) {
+    case e8:
+      STATE.VU.elt<uint8_t>(rd_num, i) = 0;
+      break;
+    case e16:
+      STATE.VU.elt<uint16_t>(rd_num, i) = 0;
+      break;
+    case e32:
+      STATE.VU.elt<uint32_t>(rd_num, i) = 0;
+      break;
+    default:
+      STATE.VU.elt<uint64_t>(rd_num, i) = 0;
+      break;
+    }
+  }
+}
