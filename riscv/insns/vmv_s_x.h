aa7733f6e81f08c42a987e19de805c09c488f51e
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-15 02:04:52 -0700

    wt: fix: temporary use rs2 register
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmv_s_x.h
+++ riscv/insns/vmv_s_x.h
@@ -1,5 +1,22 @@
 // vmv_s_x: vd[0] = rs1
-VI_XV_LOOP({
-	vd = RS1;
-	break;
-})
+// this is wordaround implementation. The spec uses rs1
+VI_LOOP_BASE
+
+  uint64_t val = RS1;
+  switch(sew) {
+  case e8:
+    STATE.VU.elt<uint8_t>(rd_num, i) = RS2;
+    break;
+  case e16:
+    STATE.VU.elt<uint16_t>(rd_num, i) = RS2;
+    break;
+  case e32:
+    STATE.VU.elt<uint32_t>(rd_num, i) = RS2;
+    break;
+  default:
+    STATE.VU.elt<uint64_t>(rd_num, i) = RS2;
+    break;
+  }
+
+  break;
+VI_LOOP_END 
