19e5323edc65b0ec7f42516ef60da8597e004aea
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-19 01:03:21 -0700

    rvv: add fmvsf impl, only support f32
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfmv_s_f.h
+++ riscv/insns/vfmv_s_f.h
@@ -1,16 +1,9 @@
 // vfmv_s_f: vd[0] = rs1 (vs2=0)
 require_fp;
-require(STATE.VU.vsew == e8 || STATE.VU.vsew == e16 || STATE.VU.vsew == e32 || STATE.VU.vsew == e64); 
-
-// SEW < FLEN ?
-if (STATE.VU.vsew > FLEN){
-// the least-significant FLEN bits are transferred and the upper SEW-FLEN bits are ignored.
-    
-}else if (STATE.VU.vsew < FLEN){
-// the value is NaN-boxed (1-extended) to FLEN bits.
-}
-
+require(STATE.VU.vsew == e32);
 
 reg_t rd_num = insn.rd(); 
-float32_t&vd = STATE.VU.elt<float32_t>(rd_num, 0); \
-vd = f32(FRS1);
+if (FLEN == 64)
+  STATE.VU.elt<uint32_t>(rd_num, 0) = f64(FRS1).v;
+else
+  STATE.VU.elt<uint32_t>(rd_num, 0) = f32(FRS1).v;
