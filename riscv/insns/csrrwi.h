5a656548a71f20af2acb24126c6a14a183b542b1
Dave <dave.wen@sifive.com>
2019-04-20 16:31:42 -0700

    add csrr[csw]i

--- riscv/insns/csrrwi.h
+++ riscv/insns/csrrwi.h
@@ -1,5 +1,11 @@
 int csr = validate_csr(insn.csr(), true);
-reg_t old = p->get_csr(csr);
-p->set_csr(csr, insn.rs1());
+reg_t old;
+if (check_vcsr(csr)) {
+  old = STATE.VU.get_vcsr(csr);
+  STATE.VU.set_vcsr(csr, insn.rs1());
+}else{
+  old = p->get_csr(csr);
+  p->set_csr(csr, insn.rs1());
+}
 WRITE_RD(sext_xlen(old));
 serialize();
