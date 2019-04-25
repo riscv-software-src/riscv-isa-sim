2e063048a8464e9d17aaa7af9ec15e75a3c76f85
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-24 20:30:55 -0700

    rvv: fix vmv.s.x and vfmv.s.f
    1. only work when vl > 0
    2. don't change vstart
    3. zero untouched part
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmv_s_x.h
+++ riscv/insns/vmv_s_x.h
@@ -5,9 +5,7 @@ reg_t vl = STATE.VU.vl;
 reg_t sew = STATE.VU.vsew;
 reg_t rd_num = insn.rd();
 
-if (vl == 0) {
-  STATE.VU.vstart = 0;
-} else {
+if (vl > 0) {
   switch(sew) {
   case e8:
     STATE.VU.elt<uint8_t>(rd_num, 0) = RS1;
