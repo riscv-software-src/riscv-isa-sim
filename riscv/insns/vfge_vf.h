6b8476e10aca755f9a5efa5074de6023ec21de10
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-25 23:35:41 -0700

    rvv: fix vfgt/vfge
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfge_vf.h
+++ riscv/insns/vfge_vf.h
@@ -1,5 +1,5 @@
 // vfge.vf vd, vs2, rs1
 VFP_LOOP_CMP
 ({
-  res = !f32_lt(vs2, rs1);
+  res = f32_le(rs1, vs2);
 })
--- riscv/insns/vfgt_vf.h
+++ riscv/insns/vfgt_vf.h
@@ -1,5 +1,5 @@
 // vfgt.vf vd, vs2, rs1
 VFP_LOOP_CMP
 ({
-  res = !f32_le(vs2, rs1);
+  res = f32_lt(rs1, vs2);
 })
