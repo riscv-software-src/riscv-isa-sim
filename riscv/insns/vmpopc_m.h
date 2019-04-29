7db5ead5cc8fea398d348aa98b07b532cf0e1c9f
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-29 08:28:26 -0700

    rvv: rewrite vmpopc
      1. the source is mask register that than element register
      2. don't use the check macro
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmpopc_m.h
+++ riscv/insns/vmpopc_m.h
@@ -1,14 +1,25 @@
 // vmpopc rd, vs2, vm
+require(STATE.VU.vsew >= e8 && STATE.VU.vsew <= e64);
+require(!STATE.VU.vill);
+reg_t vl = STATE.VU.vl;
+reg_t sew = STATE.VU.vsew;
+reg_t rd_num = insn.rd();
+reg_t rs2_num = insn.rs2();
 require(STATE.VU.vstart == 0);
 reg_t popcount = 0;
-VI_LOOP_BASE
-  bool has_count = STATE.VU.elt<int8_t>(rd_num,  (STATE.VU.vsew / 8) * i) & 0x1;
-  bool do_mask = false;
-  V_CHECK_MASK(do_mask);
+for (reg_t i=STATE.VU.vstart; i<vl; ++i){
+  const int mlen = STATE.VU.vmlen;
+  const int midx = (mlen * i) / 32;
+  const int mpos = (mlen * i) % 32;
 
-  if ((do_mask && has_count) || has_count)
-    popcount++;
-
-VI_LOOP_END
+  bool vs2_lsb = ((STATE.VU.elt<uint32_t>(rs2_num, midx ) >> mpos) & 0x1) == 1;
+  if (insn.v_vm() == 1) {
+    popcount += vs2_lsb;
+  } else {
+    bool do_mask = (STATE.VU.elt<uint32_t>(0, midx) >> mpos) & 0x1;
+    popcount += (vs2_lsb && do_mask);
+  }
+}
+STATE.VU.vstart = 0;
 WRITE_RD(popcount);
 
