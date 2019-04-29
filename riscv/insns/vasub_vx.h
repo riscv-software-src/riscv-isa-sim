c2f5e254cb2d2b3bd285a6fd88598973c6ea0751
Dave.Wen <dave.wen@sifive.com>
2019-04-28 19:39:57 -0700

    WIP: vasub_v[vx]

--- riscv/insns/vasub_vx.h
+++ riscv/insns/vasub_vx.h
@@ -1,30 +1,10 @@
 // vasub: Averaging subs of integers 
-int64_t round = 0;
-VRM vrm = STATE.VU.get_vround_mode();
+VRM xrm = STATE.VU.get_vround_mode();
 VI_VX_LOOP
 ({
-    int64_t result = rs1 - vs2; 
-    // rounding
-    switch(vrm){
-        case VRM::RNU:
-            result += (1 << (sew - 1) );
-            break;
-        case VRM::RNE:
-            assert(true);
-            break;
-        case VRM::RDN:
-            result = (result >> (sew - 1)) << (sew - 1);
-            break;
-        case VRM::ROD:
-            if ((result & (1 << ((sew -1) - 1 ))) > 0){
-                result = ((result >> (sew - 1)) + 1) << (sew - 1);
-            }
-            break;
-        case VRM::INVALID_RM:
-            assert(true);
-    };    
-    // unsigned shifting to rs1
-    result = result >> 1;
+    int64_t result = vsext(rs1, sew) - vsext(vs2, sew); 
+	INT_ROUNDING(result, xrm, 1);
+	result = vzext(result >> 1, sew);
 
     vd = result; 
 })
