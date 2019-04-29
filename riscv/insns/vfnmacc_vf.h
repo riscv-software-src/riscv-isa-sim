8a5ed7231cce84d9d58ace0a9410796924cccc14
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-28 23:11:27 -0700

    rvv: fix vfnmacc_vf
      use correct fused mac api
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfnmacc_vf.h
+++ riscv/insns/vfnmacc_vf.h
@@ -3,14 +3,14 @@ float32_t mul_result;
 
 VFP_VF_LOOP
 ({
- switch(STATE.VU.vsew){
+  switch(STATE.VU.vsew){
   case e32:
-    mul_result = f32_mul(rs1, f32(vs2.v ^ F32_SIGN));
-    vd = f32_sub(mul_result, vd);
+    vd = f32_mulAdd(rs1, f32(vs2.v ^ F32_SIGN), f32(vd.v ^ F32_SIGN));
     break;
   case e16:
   case e8:
   default:
     softfloat_exceptionFlags = 1;
+    break;
   };
 })
