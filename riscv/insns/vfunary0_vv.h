91cb32d9acd8108dc1325d2b063738a3f9be02d8
Chih-Min Chao <chihmin.chao@sifive.com>
2019-04-28 19:39:52 -0700

    rvv: remove debug msg
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfunary0_vv.h
+++ riscv/insns/vfunary0_vv.h
@@ -3,13 +3,11 @@ VFP_VV_LOOP
 ({
   switch(STATE.VU.vsew){
   case e32:
-    fprintf(stderr, "here : %lx %lx %lx\n", rd_num, rs1_num, rs2_num);
     if (rs1_num == VFUNARY0::VFCVT_XU_F_V) {
       STATE.VU.elt<uint32_t>(rd_num, i) = f32_to_ui32(vs2, STATE.frm, true);
     } else if (rs1_num == VFUNARY0::VFCVT_X_F_V) {
       STATE.VU.elt<int32_t>(rd_num, i) = f32_to_i32(vs2, STATE.frm, true);
     } else if (rs1_num == VFUNARY0::VFCVT_F_XU_V) {
-      fprintf(stderr, "here : %lx\n", rs2_num);
       auto vs2_u = STATE.VU.elt<uint32_t>(rs2_num, i);
       vd = ui32_to_f32(vs2_u);
     } else if (rs1_num == VFUNARY0::VFCVT_F_X_V) {
