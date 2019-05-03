d4c1688d7f628a95ec2f63ec5b858f74982eb2b1
Chih-Min Chao <chihmin.chao@sifive.com>
2019-05-03 03:07:23 -0700

    rvv: add vfdot.vv
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfdot_vv.h
+++ riscv/insns/vfdot_vv.h
@@ -1,5 +1,13 @@
-// vfdot
+// vfdot.vv vd, vs2, vs1
 VFP_VV_LOOP
 ({
-    throw trap_unimplemented_instruction(0);
+  switch(p->VU.vsew){
+  case e32:
+    vd = f32_add(vd, f32_mul(vs2, vs1));
+    break;
+  case e16:
+  case e8:
+  default:
+    softfloat_exceptionFlags = 1;
+  };
 })
