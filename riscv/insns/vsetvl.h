a45a637e0daef260a9adb9ccf42becef4de0a47e
Dave.Wen <dave.wen@sifive.com>
2019-05-07 07:16:47 -0700

    vsetvl[i]: if vl == 0, don't update the destination

--- riscv/insns/vsetvl.h
+++ riscv/insns/vsetvl.h
@@ -1,1 +1,3 @@
-WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, RS2));
+if (p->VU.set_vl(insn.rs1(), RS1, RS2) != 0 ){
+  WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, RS2));
+}
--- riscv/insns/vsetvli.h
+++ riscv/insns/vsetvli.h
@@ -1,1 +1,3 @@
-WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, insn.v_zimm11()));
+if(p->VU.set_vl(insn.rs1(), RS1, insn.v_zimm11()) != 0){
+  WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, insn.v_zimm11()));
+}
