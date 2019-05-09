c3ae31ea9f991aa1b73352279acc159122e2b9ac
Dave.Wen <dave.wen@sifive.com>
2019-05-09 05:51:12 -0700

    vsetvl: double set vl may cause error. Need to be fixed later

--- riscv/insns/vsetvl.h
+++ riscv/insns/vsetvl.h
@@ -1,3 +1,1 @@
-if (p->VU.set_vl(insn.rs1(), RS1, RS2) != 0 ){
-  WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, RS2));
-}
+WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, RS2));
--- riscv/insns/vsetvli.h
+++ riscv/insns/vsetvli.h
@@ -1,3 +1,1 @@
-if(p->VU.set_vl(insn.rs1(), RS1, insn.v_zimm11()) != 0){
-  WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, insn.v_zimm11()));
-}
+WRITE_RD(p->VU.set_vl(insn.rs1(), RS1, insn.v_zimm11()));
