ca420a64907cf106eb2a90676e4662fddf4d818d
Dave.Wen <dave.wen@sifive.com>
2019-05-16 13:54:42 -0700

    fix the smul

--- riscv/insns/vsmul_vv.h
+++ riscv/insns/vsmul_vv.h
@@ -2,10 +2,9 @@
 VRM xrm = p->VU.get_vround_mode();
 int64_t int_max = (1 << p->VU.vsew) - 1;
 int64_t int_min = - (1 << (p->VU.vsew - 1));
-int64_t val_mask = ((1 << (p->VU.vsew - 1)) - 1);
 int64_t sign_mask = ((1 << (p->VU.vsew - 1)));
 
-VI_VV_LOOP
+VI_VV_ULOOP
 ({
     int64_t vs1_sign;
     int64_t vs2_sign;
@@ -13,27 +12,31 @@ VI_VV_LOOP
 
 	vs1_sign = vs1 & sign_mask;
 	vs2_sign = vs2 & sign_mask;
-	vs1 = vs1 & val_mask;
-	vs2 = vs2 & val_mask;
     uint64_t result = vzext((uint64_t)vs1 * (uint64_t)vs2, sew * 2);
-	result_sign = vs1_sign ^ vs2_sign;
+    printf("sew:   %d, %llX * %llX = %llX, mask: %llX\n",sew, vs1, vs2, result, (0x3 << ((sew * 2) - 2)));
+    uint64_t sign_bits = (result & (0x3 << ((sew * 2) - 2))) >> ((sew * 2) - 2);
+    printf("signbits=%llX\n",sign_bits);
+
+    result_sign = vs1_sign ^ vs2_sign;
 	// rounding
 	INT_ROUNDING(result, xrm, sew);
-	// checking the overflow
-	uint64_t overflow = result & ((uint64_t)-1 << ((sew - 1) * 2));
+    uint64_t after_sign_bits = (result & (0x3 << ((sew * 2) - 2))) >> ((sew * 2) - 2);
+    printf("after signbits=%llX\n",after_sign_bits);
+
     // unsigned shifting to rs1
     result = result >> (sew - 1);
-	result &= val_mask;
-	result |= result_sign;
+    printf("result=%llX\n", result);
 
     // saturation
-	if (overflow){
+	if (sign_bits != after_sign_bits){
 		if (result_sign == 0){ // positive
 			result = int_max;
 		}else{
 			result = int_min;
 		}
         p->VU.vxsat = 1;
-	}
+    }else{
+	  result |= result_sign;
+    }
     vd = result;
 })
--- riscv/insns/vsmul_vx.h
+++ riscv/insns/vsmul_vx.h
@@ -2,37 +2,38 @@
 VRM xrm = p->VU.get_vround_mode();
 int64_t int_max = (1 << p->VU.vsew) - 1;
 int64_t int_min = - (1 << (p->VU.vsew - 1));
-int64_t val_mask = ((1 << (p->VU.vsew - 1)) - 1);
 int64_t sign_mask = ((1 << (p->VU.vsew - 1)));
 
-VI_VX_LOOP
+VI_VX_ULOOP
 ({
-    int64_t rs1_sign;
+    int64_t vs1_sign;
     int64_t vs2_sign;
     int64_t result_sign;
 
-	rs1_sign = rs1 & sign_mask;
+	vs1_sign = rs1 & sign_mask;
 	vs2_sign = vs2 & sign_mask;
-	rs1 = rs1 & val_mask;
-	vs2 = vs2 & val_mask;
     uint64_t result = vzext((uint64_t)rs1 * (uint64_t)vs2, sew * 2);
-	result_sign = rs1_sign ^ vs2_sign;
+    uint64_t sign_bits = (result & (0x3 << ((sew * 2) - 2))) >> ((sew * 2) - 2);
+
+    result_sign = vs1_sign ^ vs2_sign;
 	// rounding
 	INT_ROUNDING(result, xrm, sew);
-	// checking the overflow
-	uint64_t overflow = result & ((uint64_t)-1 << ((sew - 1) * 2));
+    uint64_t after_sign_bits = (result & (0x3 << ((sew * 2) - 2))) >> ((sew * 2) - 2);
+
+    // unsigned shifting to rs1
     result = result >> (sew - 1);
-	result &= val_mask;
-	result |= result_sign;
 
     // saturation
-	if (overflow){
+	if (sign_bits != after_sign_bits){
 		if (result_sign == 0){ // positive
 			result = int_max;
 		}else{
 			result = int_min;
 		}
         p->VU.vxsat = 1;
-	}
+    }else{
+	  result |= result_sign;
+    }
     vd = result;
+
 })
