d7c60fe2db31a4316c45c68f9f76ff8dc6effadf
Dave.Wen <dave.wen@sifive.com>
2019-05-16 18:34:56 -0700

    rvv: remove the debug information

--- riscv/insns/vsmul_vv.h
+++ riscv/insns/vsmul_vv.h
@@ -13,19 +13,15 @@ VI_VV_ULOOP
 	vs1_sign = vs1 & sign_mask;
 	vs2_sign = vs2 & sign_mask;
     uint64_t result = vzext((uint64_t)vs1 * (uint64_t)vs2, sew * 2);
-    printf("sew:   %d, %llX * %llX = %llX, mask: %llX\n",sew, vs1, vs2, result, (0x3 << ((sew * 2) - 2)));
     uint64_t sign_bits = (result & (0x3 << ((sew * 2) - 2))) >> ((sew * 2) - 2);
-    printf("signbits=%llX\n",sign_bits);
 
     result_sign = vs1_sign ^ vs2_sign;
 	// rounding
 	INT_ROUNDING(result, xrm, sew);
     uint64_t after_sign_bits = (result & (0x3 << ((sew * 2) - 2))) >> ((sew * 2) - 2);
-    printf("after signbits=%llX\n",after_sign_bits);
 
     // unsigned shifting to rs1
     result = result >> (sew - 1);
-    printf("result=%llX\n", result);
 
     // saturation
 	if (sign_bits != after_sign_bits){
