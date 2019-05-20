a9d29cbd1e9e6b915bec6720a07df6a124ce3322
Chih-Min Chao <chihmin.chao@sifive.com>
2019-05-20 02:54:40 -0700

    rvv: fix vmiota
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmiota_m.h
+++ riscv/insns/vmiota_m.h
@@ -9,44 +9,49 @@ reg_t rs2_num = insn.rs2();
 require(p->VU.vstart == 0);
 
 int cnt = 0;
-for (reg_t i = 0 ; i < P.VU.vlmax; ++i) {
-  const int mlen = p->VU.vmlen;
-  const int midx = (mlen * i) / 32;
-  const int mpos = (mlen * i) % 32;
-  const uint32_t mmask = ((1ul << mlen) - 1) << mpos;
+if (vl > 0) {
+  for (reg_t i = 0; i < vl; ++i) {
+    const int mlen = p->VU.vmlen;
+    const int midx = (mlen * i) / 64;
+    const int mpos = (mlen * i) % 64;
+    const uint32_t mmask = ((1ul << mlen) - 1) << mpos;
 
-  bool vs2_lsb = ((p->VU.elt<uint32_t>(rs2_num, midx ) >> mpos) & 0x1) == 1;
-  bool do_mask = (p->VU.elt<uint32_t>(0, midx) >> mpos) & 0x1;
-  uint32_t &vd = P.VU.elt<uint32_t>(rd_num, midx);
+    bool vs2_lsb = ((p->VU.elt<uint64_t>(rs2_num, midx ) >> mpos) & 0x1) == 1;
+    bool do_mask = (p->VU.elt<uint64_t>(0, midx) >> mpos) & 0x1;
 
-  bool has_one = false;
-  if (insn.v_vm() == 1 || (insn.v_vm() == 0 && do_mask)) {
-    if (vs2_lsb) {
-      has_one = true;
+    bool has_one = false;
+    if (insn.v_vm() == 1 || (insn.v_vm() == 0 && do_mask)) {
+      if (vs2_lsb) {
+        has_one = true;
+      }
+    }
+
+    bool use_ori = (insn.v_vm() == 0) && !do_mask;
+    switch (sew) {
+    case e8:
+      P.VU.elt<uint8_t>(rd_num, i) = use_ori ?
+        P.VU.elt<uint8_t>(rd_num, i) : cnt;
+      break;
+    case e16:
+      P.VU.elt<uint16_t>(rd_num, i) = use_ori ?
+        P.VU.elt<uint16_t>(rd_num, i) : cnt;
+      break;
+    case e32:
+      P.VU.elt<uint32_t>(rd_num, i) = use_ori ?
+        P.VU.elt<uint32_t>(rd_num, i) : cnt;
+      break;
+    default:
+      P.VU.elt<uint64_t>(rd_num, i) = use_ori ?
+        P.VU.elt<uint64_t>(rd_num, i) : cnt;
+      break;
+    }
+
+    if (has_one) {
+      cnt++;
     }
   }
 
-  bool use_ori = (insn.v_vm() == 0) && !do_mask;
-  switch (sew) {
-  case e8:
-    P.VU.elt<uint8_t>(rd_num, i) = use_ori ?
-      P.VU.elt<uint8_t>(rs2_num, i) : cnt;
-    break;
-  case e16:
-    P.VU.elt<uint16_t>(rd_num, i) = use_ori ?
-      P.VU.elt<uint16_t>(rs2_num, i) : cnt;
-    break;
-  case e32:
-    P.VU.elt<uint32_t>(rd_num, i) = use_ori ?
-      P.VU.elt<uint32_t>(rs2_num, i) : cnt;
-    break;
-  default:
-    P.VU.elt<uint64_t>(rd_num, i) = use_ori ?
-      P.VU.elt<uint64_t>(rs2_num, i) : cnt;
-    break;
-  }
-
-  if (has_one)
-    cnt++;
+  uint8_t *tail = &P.VU.elt<uint8_t>(rd_num, vl * ((sew >> 3) * 1)); \
+  memset(tail, 0, (P.VU.vlmax - vl) * ((sew >> 3) * 1)); \
 }
 VI_CHECK_1905
