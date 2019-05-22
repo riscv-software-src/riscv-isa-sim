cf5e8a0c338cdb37f2f89de2a8165e6da9ca6d64
Chih-Min Chao <chihmin.chao@sifive.com>
2019-05-22 01:49:14 -0700

    div
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vdiv_vx.h
+++ riscv/insns/vdiv_vx.h
@@ -1,6 +1,11 @@
 // vdiv.vx vd, vs2, rs1
 VI_VX_LOOP
 ({
-  vd = vs2 / rs1;
+  if(rs1 == 0)
+    vd = -1;
+  else if(vs2 == -(1 << (sew - 1)) && rs1 == -1)
+    vd = vs2;
+  else
+    vd = vs2 / rs1;
 })
 VI_CHECK_1905
