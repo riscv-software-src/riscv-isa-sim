09ea04766aea584077b7f8782bad3717b6a2e504
Dave.Wen <dave.wen@sifive.com>
2019-05-23 15:43:40 -0700

    rvv: fix vssrl

--- riscv/insns/vssrl_vi.h
+++ riscv/insns/vssrl_vi.h
@@ -3,7 +3,7 @@ VRM xrm = p->VU.get_vround_mode();
 VI_VI_ULOOP
 ({
   uint64_t v2 = vs2;
-  INT_ROUNDING(v2, xrm, sew);
+  INT_ROUNDING(v2, xrm, 1);
   vd = v2 >> (simm5 & 0x1f);
 })
 VI_CHECK_1905
--- riscv/insns/vssrl_vx.h
+++ riscv/insns/vssrl_vx.h
@@ -3,7 +3,7 @@ VRM xrm = p->VU.get_vround_mode();
 VI_VX_ULOOP
 ({
   uint64_t v2 = vs2;
-  INT_ROUNDING(v2, xrm, sew);
+  INT_ROUNDING(v2, xrm, 1);
   vd = v2 >> (rs1 & ((2 * sew) - 1));
 })
 VI_CHECK_1905
