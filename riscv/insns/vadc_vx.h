372d4400df0be81bb1a85472739d02a4e7135bb9
Chih-Min Chao <chihmin.chao@sifive.com>
2019-05-23 07:26:33 -0700

    rvv: fix adc
     1. use unsiged to get carry correctly
     2. zero des mask register
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vadc_vx.h
+++ riscv/insns/vadc_vx.h
@@ -1,15 +1,19 @@
 // vadc.vx vd, vs2, rs1
 require(insn.v_vm() == 1);
+require(insn.rd() != 0);
 VI_VX_LOOP
 ({
-  uint32_t &v0 = P.VU.elt<uint32_t>(0, midx);
-  int carry = (v0 >> mpos) & 0x1;
+  auto &v0 = P.VU.elt<uint64_t>(0, midx);
+  const uint64_t mmask = (UINT64_MAX << (64 - mlen)) >> (64 - mlen - mpos);
+  const uint64_t op_mask = (UINT64_MAX >> (64 - sew));
+  uint64_t carry = (v0 >> mpos) & 0x1;
 
-  uint128_t res = rs1 + vs2 + carry;
+  uint128_t res = (op_mask & rs1) + (op_mask & vs2) + carry;
   vd = res;
 
-  const uint32_t mmask = ((1ul << mlen) - 1) << mpos;
   carry = (res >> sew) & 0x1u;
   v0 = (v0 & ~mmask) | ((carry << mpos) & mmask);
 })
+
+VI_TAIL_ZERO_MASK(0);
 VI_CHECK_1905
