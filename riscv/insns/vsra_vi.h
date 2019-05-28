2517f7dd9f0a0cda60520e3589835349584527c4
Chih-Min Chao <chihmin.chao@sifive.com>
2019-05-27 19:47:43 -0700

    rvv: don't maks shift vi value
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vsra_vi.h
+++ riscv/insns/vsra_vi.h
@@ -1,6 +1,6 @@
 // vsra.vi vd, vs2, zimm5
 VI_VI_LOOP
 ({
-  vd = vs2 >> (simm5 & ((1u << log2(sew)) - 1) & 0x1f);
+  vd = vs2 >> (simm5 & 0x1f);
 })
 VI_CHECK_1905
--- riscv/insns/vsrl_vi.h
+++ riscv/insns/vsrl_vi.h
@@ -1,6 +1,6 @@
 // vsrl.vi vd, vs2, zimm5
 VI_VI_ULOOP
 ({
-  vd = vs2 >> (simm5 & ((1u << log2(sew)) - 1) & 0x1f);
+  vd = vs2 >> simm5;
 })
 VI_CHECK_1905
