5b42c21abbc39e56ff313b0cd27e7d28cfe7a707
Chih-Min Chao <chihmin.chao@sifive.com>
2019-05-28 02:00:14 -0700

    rvv: change sbc impl from carry to borrow
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vsbc_vv.h
+++ riscv/insns/vsbc_vv.h
@@ -5,10 +5,9 @@ VI_VV_LOOP
 ({
   auto &v0 = P.VU.elt<uint64_t>(0, midx);
   const uint64_t mmask = (UINT64_MAX << (64 - mlen)) >> (64 - mlen - mpos);
-  const uint64_t op_mask = (UINT64_MAX >> (64 - sew));
   uint64_t carry = (v0 >> mpos) & 0x1;
 
-  uint128_t res = (op_mask & vs1) + (op_mask & -vs2) + (op_mask & -carry);
+  uint128_t res = vs1 - vs2 - carry;
   vd = res;
 
   carry = (res >> sew) & 0x1u;
