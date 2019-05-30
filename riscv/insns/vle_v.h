2ec17a7d5182d0845afc1394bf2264980789687f
Dave.Wen <dave.wen@sifive.com>
2019-05-29 17:44:26 -0700

    rvv: update ordinary load instructions to new strip function

--- riscv/insns/vle_v.h
+++ riscv/insns/vle_v.h
@@ -7,52 +7,33 @@ require(sew >= e8 && sew <= e64);
 require((nf >= 2 && p->VU.vlmul == 1) || nf == 1);
 reg_t baseAddr = RS1;
 reg_t vd = insn.rd();
-for (reg_t i = p->VU.vstart; i < vl; ++i) {
+reg_t vlmax = p->VU.vlmax;
+for (reg_t i = 0; i < vlmax && vl != 0; ++i) {
+  bool is_valid = true;
   STRIP(i)
-  V_ELEMENT_SKIP(mmu_inx);
-
+  V_ELEMENT_SKIP(i);
   for (reg_t fn = 0; fn < nf; ++fn) {
     switch (sew) {
     case e8:
-      p->VU.elt<uint8_t>(vd + fn, i) = 
-          MMU.load_int8(baseAddr + (mmu_inx * nf + fn) * elt_byte);
+      p->VU.elt<uint8_t>(vd + fn, vreg_inx) = is_valid ? 
+          MMU.load_int8(baseAddr + (i * nf + fn) * elt_byte) : 0;
       break;
     case e16:
-      p->VU.elt<uint16_t>(vd + fn, i) = 
-          MMU.load_int16(baseAddr + (mmu_inx * nf + fn) * elt_byte);
+      p->VU.elt<uint16_t>(vd + fn, vreg_inx) = is_valid ?
+          MMU.load_int16(baseAddr + (i * nf + fn) * elt_byte) : 0;
       break;
     case e32:
-      p->VU.elt<uint32_t>(vd + fn, i) = 
-          MMU.load_int32(baseAddr + (mmu_inx * nf + fn) * elt_byte);
+      p->VU.elt<uint32_t>(vd + fn, vreg_inx) = is_valid ?
+          MMU.load_int32(baseAddr + (i * nf + fn) * elt_byte) : 0;
       break;
     case e64:
-      p->VU.elt<uint64_t>(vd + fn, i) = 
-          MMU.load_int64(baseAddr + (mmu_inx * nf + fn) * elt_byte);
+      p->VU.elt<uint64_t>(vd + fn, vreg_inx) =  is_valid ?
+          MMU.load_int64(baseAddr + (i * nf + fn) * elt_byte) : 0;
       break;
     }
   }
 }
 
-//zero unfilled part
-if (vl != 0 && TAIL_ZEROING){
-  for (reg_t i = vl; i < p->VU.vlmax; ++i) {
-    for (reg_t fn = 0; fn < nf; ++fn) {
-      switch (sew) {
-        case e8:
-          p->VU.elt<uint8_t>(vd + fn, i) = 0;
-          break;
-        case e16:
-          p->VU.elt<uint16_t>(vd + fn, i) = 0;
-          break;
-        case e32:
-          p->VU.elt<uint32_t>(vd + fn, i) = 0;
-          break;
-        case e64:
-          p->VU.elt<uint64_t>(vd + fn, i) = 0;
-          break;
-      }
-    }
-  }
-}
+
 p->VU.vstart = 0;
 VI_CHECK_1905
--- riscv/insns/vlh_v.h
+++ riscv/insns/vlh_v.h
@@ -5,35 +5,22 @@ require((nf >= 2 && p->VU.vlmul == 1) || nf == 1);
 reg_t vl = p->VU.vl;
 reg_t baseAddr = RS1;
 reg_t vd = insn.rd();
-for (reg_t i=p->VU.vstart; i<vl; ++i) {
+reg_t vlmax = p->VU.vlmax;
+for (reg_t i = 0; i < vlmax && vl != 0; ++i) {
+  bool is_valid = true;
+  V_ELEMENT_SKIP(i);
   STRIP(i)
-  V_ELEMENT_SKIP(mmu_inx);
-
   for (reg_t fn=0; fn<nf; ++fn) {
-    int64_t val = MMU.load_int16(baseAddr + (mmu_inx * nf + fn) * 2);
+    int64_t val = MMU.load_int16(baseAddr + (i * nf + fn) * 2);
     if (p->VU.vsew == e16) {
-      p->VU.elt<uint16_t>(vd+fn, i) = val;
+      p->VU.elt<uint16_t>(vd+fn, vreg_inx) = is_valid ? val : 0;
     } else if (p->VU.vsew == e32) {
-      p->VU.elt<uint32_t>(vd+fn, i) = val;
+      p->VU.elt<uint32_t>(vd+fn, vreg_inx) = is_valid ? val : 0;
     } else {
-      p->VU.elt<uint64_t>(vd+fn, i) = val;
+      p->VU.elt<uint64_t>(vd+fn, vreg_inx) = is_valid ? val : 0;
     }
   }
 }
 
-//zero unfilled part
-if (vl != 0 && TAIL_ZEROING){
-  for (reg_t i=vl; i < p->VU.vlmax; ++i) {
-    for (reg_t fn=0; fn<nf; ++fn) {
-      if (p->VU.vsew == e16) {
-        p->VU.elt<uint16_t>(vd+fn, i) = 0;
-      } else if (p->VU.vsew == e32) {
-        p->VU.elt<uint32_t>(vd+fn, i) = 0;
-      } else {
-        p->VU.elt<uint64_t>(vd+fn, i) = 0;
-      }
-    }
-  }
-}
 p->VU.vstart = 0;
 VI_CHECK_1905
