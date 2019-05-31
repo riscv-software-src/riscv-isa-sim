6f820f198716c648a17f502c22ffd534a287fb7f
Dave.Wen <dave.wen@sifive.com>
2019-05-31 03:19:40 -0700

    rvv: fix vmfirst

--- riscv/insns/vmfirst_m.h
+++ riscv/insns/vmfirst_m.h
@@ -7,12 +7,10 @@ reg_t rd_num = insn.rd();
 reg_t rs2_num = insn.rs2();
 require(p->VU.vstart == 0);
 reg_t pos = -1;
-for (reg_t i=p->VU.vstart; i<vl; ++i){
-  const int mlen = p->VU.vmlen;
-  const int midx = (mlen * i) / 32;
-  const int mpos = (mlen * i) % 32;
+for (reg_t i=p->VU.vstart; i < vl; ++i){
+  V_LOOP_ELEMENT_SKIP
 
-  bool vs2_lsb = ((p->VU.elt<uint32_t>(rs2_num, midx ) >> mpos) & 0x1) == 1;
+  bool vs2_lsb = ((p->VU.elt<uint64_t>(rs2_num, midx ) >> mpos) & 0x1) == 1;
   if (vs2_lsb) {
     pos = i;
     break;
