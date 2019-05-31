5f1582c630c906b7c7e6d2897bb6db923ecba0ac
Chih-Min Chao <chihmin.chao@sifive.com>
2019-05-30 20:37:56 -0700

    rvv: fix vsrl?
    shift amount can't exceed  max bit length - 1
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vssrl_vv.h
+++ riscv/insns/vssrl_vv.h
@@ -4,6 +4,11 @@ VI_VV_ULOOP
 ({
   uint64_t v2 = vs2;
   INT_ROUNDING(v2, xrm, 1);
-  vd = v2 >> (vs1 & ((1u << log2(sew * 2)) - 1));
+  int sh = vs1 & ((1u << log2(sew * 2)) - 1);
+
+  if (sh > 63)
+    vd = (v2 >> 63) >> 1;
+  else
+    vd = v2 >> sh;
 })
 VI_CHECK_1905
--- riscv/insns/vssrl_vx.h
+++ riscv/insns/vssrl_vx.h
@@ -4,6 +4,11 @@ VI_VX_ULOOP
 ({
   uint64_t v2 = vs2;
   INT_ROUNDING(v2, xrm, 1);
-  vd = v2 >> (rs1 & ((1u << log2(sew * 2)) - 1));
+  int sh = rs1 & ((1u << log2(sew * 2)) - 1);
+
+  if (sh > 63)
+    vd = (v2 >> 63) >> 1;
+  else
+    vd = v2 >> sh;
 })
 VI_CHECK_1905
