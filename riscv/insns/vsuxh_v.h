19828c9d04fbe9ae7bc78bb373763f4b9179e296
Dave.Wen <dave.wen@sifive.com>
2019-05-31 03:19:12 -0700

    rvv: fix vsux[bhwe]_v instructions

--- riscv/insns/vsuxh_v.h
+++ riscv/insns/vsuxh_v.h
@@ -4,26 +4,29 @@ reg_t vl = p->VU.vl;
 reg_t baseAddr = RS1;
 reg_t stride = insn.rs2();
 reg_t vs3 = insn.rd();
-for (reg_t i = p->VU.vstart; i < vl; ++i) {
-  V_LOOP_ELEMENT_SKIP;
+reg_t vlmax = p->VU.vlmax;
+DUPLICATE_VREG(stride, vlmax);
+for (reg_t i = 0; i < vlmax && vl != 0; ++i) {
+  bool is_valid = true;
+  V_ELEMENT_SKIP(i);
+  STRIP(i)
 
-  reg_t index = 0;
   switch (p->VU.vsew) {
-  case e16:
-    index = p->VU.elt<int16_t>(stride, i);
-    MMU.store_uint16(baseAddr + index,
-                     p->VU.elt<uint16_t>(vs3, i));
-    break;
-  case e32:
-    index = p->VU.elt<int32_t>(stride, i);
-    MMU.store_uint16(baseAddr + index,
-                     p->VU.elt<uint32_t>(vs3, i));
-    break;
-  case e64:
-    index = p->VU.elt<int64_t>(stride, i);
-    MMU.store_uint16(baseAddr + index,
-                     p->VU.elt<uint64_t>(vs3, i));
-    break;
+    case e16:
+      if (is_valid)
+          MMU.store_uint16(baseAddr + index[i],
+              p->VU.elt<uint16_t>(vs3, vreg_inx));
+      break;
+    case e32:
+      if (is_valid)
+          MMU.store_uint16(baseAddr + index[i],
+              p->VU.elt<uint32_t>(vs3, vreg_inx));
+      break;
+    case e64:
+      if (is_valid)
+          MMU.store_uint16(baseAddr + index[i],
+              p->VU.elt<uint64_t>(vs3, vreg_inx));
+      break;
   }
 }
 p->VU.vstart = 0;
