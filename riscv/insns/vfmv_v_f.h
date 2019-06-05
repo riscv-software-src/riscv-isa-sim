5b5bd969008a0161656aba7595a414b90c861b4b
Chih-Min Chao <chihmin.chao@sifive.com>
2019-06-04 22:59:09 -0700

    rvv :add missing file
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

---
+++ riscv/insns/vfmv_v_f.h
@@ -0,0 +1,20 @@
+// vfmerge_vf vd, vs2, vs1, vm
+require_extension('F');
+require_fp;
+require(P.VU.vsew == 32);
+require(!P.VU.vill);
+reg_t vl = P.VU.vl;
+reg_t sew = P.VU.vsew;
+reg_t rd_num = insn.rd();
+reg_t rs1_num = insn.rs1();
+reg_t rs2_num = insn.rs2();
+for (reg_t i=P.VU.vstart; i<vl; ++i) {
+  auto &vd = P.VU.elt<float32_t>(rd_num, i);
+  auto rs1 = f32(READ_FREG(rs1_num));
+
+  vd = rs1;
+}
+
+VI_TAIL_ZERO(1);
+P.VU.vstart = 0;
+set_fp_exceptions;
