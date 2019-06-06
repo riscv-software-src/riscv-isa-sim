50cff99e41857ad9698382ba044153130d342955
Dave.Wen <dave.wen@sifive.com>
2019-06-05 20:52:14 -0700

    rvv: fix the masking operation

--- riscv/insns/vmsgtu_vi.h
+++ riscv/insns/vmsgtu_vi.h
@@ -1,6 +1,6 @@
 // vsgtu.vi  vd, vd2, zimm5
 VI_VI_ULOOP_CMP
 ({
-  res = vs2 > (simm5 & 0xf);
+  res = vs2 > simm5;
 })
 VI_CHECK_1905
--- riscv/insns/vmsleu_vi.h
+++ riscv/insns/vmsleu_vi.h
@@ -1,6 +1,6 @@
 // vsleu.vi vd, vs2, zimm5
 VI_VI_ULOOP_CMP
 ({
-  res = vs2 <= (simm5 & 0xf);
+  res = vs2 <= simm5;
 })
 VI_CHECK_1905
