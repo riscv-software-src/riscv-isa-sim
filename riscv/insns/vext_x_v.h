17496326ef0c20d4f1f5a187a4e2a84c00e6f444
Dave.Wen <dave.wen@sifive.com>
2019-06-16 19:58:01 -0700

    rvv: fix the vext doesn't signed-extend for spike internal 64bit format under RV32 issue

--- riscv/insns/vext_x_v.h
+++ riscv/insns/vext_x_v.h
@@ -15,7 +15,10 @@ if (!(rs1 >= 0 && rs1 < (P.VU.get_vlen()/sew))) {
     WRITE_RD(P.VU.elt<uint16_t>(rs2_num, rs1));
     break;
   case e32:
-    WRITE_RD(P.VU.elt<uint32_t>(rs2_num, rs1));
+    if (P.get_max_xlen() == 32)
+      WRITE_RD(P.VU.elt<int32_t>(rs2_num, rs1));
+    else
+      WRITE_RD(P.VU.elt<uint32_t>(rs2_num, rs1));
     break;
   case e64:
     if (P.get_max_xlen() <= sew)
