42da64067a9fe494d4df10e48c94bfc0c8c203a2
Jerry Shih <bignose1007@gmail.com>
2019-07-01 13:39:04 +0800

    Use memset to init the index buffer in VI_DUPLICATE_VREG macro.
    The clang compiler has the following compiling error:
    error: variable-sized object may not be initialized.

--- riscv/decode.h
+++ riscv/decode.h
@@ -1374,7 +1374,8 @@ VI_LOOP_END
 
 
 #define VI_DUPLICATE_VREG(v, vlmax) \
-reg_t index[vlmax] = {0}; \
+reg_t index[vlmax]; \
+memset(index, 0, sizeof(reg_t) * vlmax); \
 for (reg_t i = 0; i < vlmax && P.VU.vl != 0; ++i) { \
   switch(P.VU.vsew) { \
     case e8: \
