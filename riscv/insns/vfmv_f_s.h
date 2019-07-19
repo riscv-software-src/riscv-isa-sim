111c2b876f0a0b831beb9dc1e37437d57cbea682
Andrew Waterman <andrew@sifive.com>
2019-07-19 00:43:38 -0700

    Check for F extension in vfmv instructions

--- riscv/insns/vfmv_f_s.h
+++ riscv/insns/vfmv_f_s.h
@@ -1,6 +1,7 @@
 // vfmv_f_s: rd = vs2[0] (rs1=0)
 require(insn.v_vm() == 1);
 require_fp;
+require_extension('F');
 require(P.VU.vsew == e8 || P.VU.vsew == e16 || P.VU.vsew == e32 || P.VU.vsew == e64);
 
 reg_t rs2_num = insn.rs2();
