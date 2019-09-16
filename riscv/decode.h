530b2d1d582d7cef7ab415570e72d1c0994307a9
Chih-Min Chao <chihmin.chao@sifive.com>
2019-09-16 00:33:43 -0700

    rvv: fix floating-point exception for comparison
    don't use quiet api
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/decode.h
+++ riscv/decode.h
@@ -1673,6 +1673,7 @@ for (reg_t i = 0; i < vlmax; ++i) { \
 #define VI_VFP_LOOP_CMP(BODY) \
   VI_VFP_LOOP_CMP_BASE \
   BODY; \
+  set_fp_exceptions; \
   DEBUG_RVV_FP_VV; \
   VI_VFP_LOOP_CMP_END \
 
