025a01aa8b99322793ef8e11560fee284db55ed3
Chih-Min Chao <chihmin.chao@sifive.com>
2019-09-25 01:38:54 -0700

    rvv: fix vmv.x.s signed-ext issue
      sew  : 32
      xlen : 64
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmv_x_s.h
+++ riscv/insns/vmv_x_s.h
@@ -15,10 +15,7 @@ if (!(rs1 >= 0 && rs1 < (P.VU.get_vlen()/sew))) {
     WRITE_RD(P.VU.elt<int16_t>(rs2_num, rs1));
     break;
   case e32:
-    if (P.get_max_xlen() == 32)
-      WRITE_RD(P.VU.elt<int32_t>(rs2_num, rs1));
-    else
-      WRITE_RD(P.VU.elt<uint32_t>(rs2_num, rs1));
+    WRITE_RD(P.VU.elt<int32_t>(rs2_num, rs1));
     break;
   case e64:
     if (P.get_max_xlen() <= sew)
