8f2d28b25c8c6f3af85a6e000988f63152554c4e
Chih-Min Chao <chihmin.chao@sifive.com>
2019-09-29 21:24:45 -0700

    rvv: remove wide reduction dst and src overlap checking
    since reduction keep the accumulation result in pipeline and write 1 widen
    element back to dst register.
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/decode.h
+++ riscv/decode.h
@@ -1245,7 +1245,6 @@ VI_LOOP_END
 
 // wide reduction loop - signed
 #define VI_LOOP_WIDE_REDUCTION_BASE(sew1, sew2) \
-  VI_CHECK_DSS(false); \
   reg_t vl = P.VU.vl; \
   reg_t rd_num = insn.rd(); \
   reg_t rs1_num = insn.rs1(); \
