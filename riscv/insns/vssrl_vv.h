3ccf946e6d814203733a7804b9f9b7b99e38255f
Chih-Min Chao <chihmin.chao@sifive.com>
2019-10-07 23:49:23 -0700

    rvv: fix vssr/vssra rounding issue
    use 128bit to store temporary result to handle shift = 63 case in rv64
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vssrl_vv.h
+++ riscv/insns/vssrl_vv.h
@@ -3,7 +3,8 @@ VRM xrm = P.VU.get_vround_mode();
 VI_VV_ULOOP
 ({
   int sh = vs1 & (sew - 1);
+  uint128_t val = vs2;
 
-  INT_ROUNDING(vs2, xrm, sh);
-  vd = vs2 >> sh;
+  INT_ROUNDING(val, xrm, sh);
+  vd = val >> sh;
 })
