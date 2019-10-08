7b30eeada662f34643680e9dbd805e3062bb1e97
Chih-Min Chao <chihmin.chao@sifive.com>
2019-10-07 23:49:23 -0700

    rvv: fix vssr/vssra rounding issue
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vssra_vi.h
+++ riscv/insns/vssra_vi.h
@@ -3,6 +3,8 @@ VRM xrm = P.VU.get_vround_mode();
 VI_VI_LOOP
 ({
   int sh = simm5 & (sew - 1) & 0x1f;
-  INT_ROUNDING(vs2, xrm, sh);
-  vd = vs2 >> sh;
+  int64_t val = vs2;
+
+  INT_ROUNDING(val, xrm, sh);
+  vd = val >> sh;
 })
--- riscv/insns/vssrl_vi.h
+++ riscv/insns/vssrl_vi.h
@@ -3,7 +3,8 @@ VRM xrm = P.VU.get_vround_mode();
 VI_VI_ULOOP
 ({
   int sh = simm5 & (sew - 1) & 0x1f;
+  uint64_t val = vs2;
 
-  INT_ROUNDING(vs2, xrm, sh);
-  vd = vs2 >> sh;
+  INT_ROUNDING(val, xrm, sh);
+  vd = val >> sh;
 })
