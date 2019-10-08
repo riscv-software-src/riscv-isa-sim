2101d52156b8d05b5c553a930aa383f8e8fcbb18
Chih-Min Chao <chihmin.chao@sifive.com>
2019-10-08 01:25:18 -0700

    rvv: fix vsmul sign and variable type
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vsmul_vv.h
+++ riscv/insns/vsmul_vv.h
@@ -1,33 +1,32 @@
 // vsmul: Signed saturating and rounding fractional multiply
 VRM xrm = P.VU.get_vround_mode();
-uint64_t int_max = (uint64_t(1) << (P.VU.vsew - 1)) - 1;
-uint64_t int_min = - (1 << (P.VU.vsew - 1));
-uint64_t sign_mask = uint64_t(1) << (P.VU.vsew - 1);
+int64_t int_max = (uint64_t(1) << (P.VU.vsew - 1)) - 1;
+int64_t int_min = - (1 << (P.VU.vsew - 1));
+int64_t sign_mask = uint64_t(1) << (P.VU.vsew - 1);
 
-VI_VV_ULOOP
+VI_VV_LOOP
 ({
-  uint64_t vs1_sign;
-  uint64_t vs2_sign;
-  uint64_t result_sign;
+  int64_t vs1_sign;
+  int64_t vs2_sign;
+  int64_t result_sign;
 
   vs1_sign = vs1 & sign_mask;
   vs2_sign = vs2 & sign_mask;
   bool overflow = vs1 == vs2 && vs1 == int_min;
 
-  uint128_t result = (uint128_t)vs1 * (uint128_t)vs2;
-  result &= ((uint128_t)1llu << ((sew * 2) - 2)) - 1;
+  int128_t result = (int128_t)vs1 * (int128_t)vs2;
   result_sign = (vs1_sign ^ vs2_sign) & sign_mask;
+
   // rounding
   INT_ROUNDING(result, xrm, sew - 1);
-  // unsigned shifting
+  // remove guard bits
   result = result >> (sew - 1);
 
   // saturation
   if (overflow) {
     result = int_max;
     P.VU.vxsat = 1;
-  } else {
-    result |= result_sign;
   }
+
   vd = result;
 })
