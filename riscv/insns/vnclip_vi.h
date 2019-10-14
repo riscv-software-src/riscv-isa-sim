eef33e34e5f12bb21aa2b4e4cc64ac24572d9546
Chih-Min Chao <chihmin.chao@sifive.com>
2019-10-14 02:09:08 -0700

    rvv: fix the llu type differenct between 32/64 hosts
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vnclip_vi.h
+++ riscv/insns/vnclip_vi.h
@@ -5,9 +5,9 @@ int64_t int_min = -(1 << (P.VU.vsew - 1));
 VI_VVXI_LOOP_NARROW
 ({
   int64_t result = vs2;
-  uint64_t unsigned_shift_amount = (uint64_t)(zimm5 & ((sew * 2) - 1));
+  unsigned unsigned_shift_amount = zimm5 & ((sew * 2) - 1);
   // The immediate form supports shift amounts up to 31 only.
-  unsigned_shift_amount = std::min(unsigned_shift_amount, 31ull);
+  unsigned_shift_amount = std::min(unsigned_shift_amount, 31u);
 
   // rounding
   INT_ROUNDING(result, xrm, unsigned_shift_amount);
--- riscv/insns/vnclipu_vi.h
+++ riscv/insns/vnclipu_vi.h
@@ -4,9 +4,9 @@ uint64_t int_max = ~(-1ll << P.VU.vsew);
 VI_VVXI_LOOP_NARROW
 ({
   uint64_t result = vs2_u;
-  uint64_t unsigned_shift_amount = (uint64_t)(zimm5 & ((sew * 2) - 1));
+  unsigned unsigned_shift_amount = zimm5 & ((sew * 2) - 1);
   // The immediate form supports shift amounts up to 31 only.
-  unsigned_shift_amount = std::min(unsigned_shift_amount, 31ull);
+  unsigned_shift_amount = std::min(unsigned_shift_amount, 31u);
 
   // rounding
   INT_ROUNDING(result, xrm, unsigned_shift_amount);
