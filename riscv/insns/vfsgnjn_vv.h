8d50b2ff666fdf3e448f666d1741d21a90730bab
Chih-Min Chao <chihmin.chao@sifive.com>
2019-11-19 22:54:45 -0800

    rvv: fix floating sign inject operand order
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfsgnjn_vv.h
+++ riscv/insns/vfsgnjn_vv.h
@@ -1,5 +1,5 @@
 // vfsgnn
 VI_VFP_VV_LOOP
 ({
-  vd = fsgnj32(vs1.v, vs2.v, true, false);
+  vd = fsgnj32(vs2.v, vs1.v, true, false);
 })
--- riscv/insns/vfsgnjx_vv.h
+++ riscv/insns/vfsgnjx_vv.h
@@ -1,5 +1,5 @@
 // vfsgnx
 VI_VFP_VV_LOOP
 ({
-  vd = fsgnj32(vs1.v, vs2.v, false, true);
+  vd = fsgnj32(vs2.v, vs1.v, false, true);
 })
