4ac95a8c99d19c4db3be648e88f853ddf4f66d53
Chih-Min Chao <chihmin.chao@sifive.com>
2019-12-02 06:53:21 -0800

    rvv: refinve vfmv to support float64
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfmerge_vfm.h
+++ riscv/insns/vfmerge_vfm.h
@@ -3,16 +3,37 @@ require(insn.rd() != 0);
 VI_CHECK_SSS(false);
 VI_VFP_COMMON;
 reg_t sew = P.VU.vsew;
-for (reg_t i=P.VU.vstart; i<vl; ++i) {
-  auto &vd = P.VU.elt<float32_t>(rd_num, i);
-  auto rs1 = f32(READ_FREG(rs1_num));
-  auto vs2 = P.VU.elt<float32_t>(rs2_num, i);
 
-  int midx = (P.VU.vmlen * i) / 64;
-  int mpos = (P.VU.vmlen * i) % 64;
-  bool use_first = (P.VU.elt<uint64_t>(0, midx) >> mpos) & 0x1;
+switch(P.VU.vsew) {
+  case 32:
+    for (reg_t i=P.VU.vstart; i<vl; ++i) {
+      auto &vd = P.VU.elt<float32_t>(rd_num, i);
+      auto rs1 = f32(READ_FREG(rs1_num));
+      auto vs2 = P.VU.elt<float32_t>(rs2_num, i);
 
-  vd = use_first ? rs1 : vs2;
+      int midx = (P.VU.vmlen * i) / 64;
+      int mpos = (P.VU.vmlen * i) % 64;
+      bool use_first = (P.VU.elt<uint64_t>(0, midx) >> mpos) & 0x1;
+
+      vd = use_first ? rs1 : vs2;
+    }
+    break;
+  case 64:
+    for (reg_t i=P.VU.vstart; i<vl; ++i) {
+      auto &vd = P.VU.elt<float64_t>(rd_num, i);
+      auto rs1 = f64(READ_FREG(rs1_num));
+      auto vs2 = P.VU.elt<float64_t>(rs2_num, i);
+
+      int midx = (P.VU.vmlen * i) / 64;
+      int mpos = (P.VU.vmlen * i) % 64;
+      bool use_first = (P.VU.elt<uint64_t>(0, midx) >> mpos) & 0x1;
+
+      vd = use_first ? rs1 : vs2;
+    }
+    break;
+  default:
+    require(0);
+    break;
 }
 
 P.VU.vstart = 0;
