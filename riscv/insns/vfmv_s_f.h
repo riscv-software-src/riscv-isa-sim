c529835050826628bd3c76d93d06e7a1b41e6c29
Chih-Min Chao <chihmin.chao@sifive.com>
2019-12-02 06:53:21 -0800

    rvv: refinve vfmv to support float64
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfmv_s_f.h
+++ riscv/insns/vfmv_s_f.h
@@ -1,19 +1,26 @@
 // vfmv_s_f: vd[0] = rs1 (vs2=0)
 require_vector;
-require(insn.v_vm() == 1);
 require_fp;
 require_extension('F');
-require(P.VU.vsew == e32);
+require(P.VU.vsew >= e32 && P.VU.vsew <= 64);
 reg_t vl = P.VU.vl;
 
 if (vl > 0) {
   reg_t rd_num = insn.rd();
-  reg_t sew = P.VU.vsew;
 
-  if (FLEN == 64)
-    P.VU.elt<uint32_t>(rd_num, 0) = f64(FRS1).v;
-  else
-    P.VU.elt<uint32_t>(rd_num, 0) = f32(FRS1).v;
-
-  vl = 0;
+  switch(P.VU.vsew) {
+    case 32:
+      if (FLEN == 64)
+        P.VU.elt<uint32_t>(rd_num, 0) = f64(FRS1).v;
+      else
+        P.VU.elt<uint32_t>(rd_num, 0) = f32(FRS1).v;
+      break;
+    case 64:
+      if (FLEN == 64)
+        P.VU.elt<uint64_t>(rd_num, 0) = f64(FRS1).v;
+      else
+        P.VU.elt<uint64_t>(rd_num, 0) = f32(FRS1).v;
+      break;
+  }
 }
+P.VU.vstart = 0;
--- riscv/insns/vfmv_v_f.h
+++ riscv/insns/vfmv_v_f.h
@@ -1,12 +1,23 @@
 // vfmv_vf vd, vs1
 require((insn.rd() & (P.VU.vlmul - 1)) == 0);
 VI_VFP_COMMON
-reg_t sew = P.VU.vsew;
-for (reg_t i=P.VU.vstart; i<vl; ++i) {
-  auto &vd = P.VU.elt<float32_t>(rd_num, i);
-  auto rs1 = f32(READ_FREG(rs1_num));
+switch(P.VU.vsew) {
+  case e32:
+    for (reg_t i=P.VU.vstart; i<vl; ++i) {
+      auto &vd = P.VU.elt<float32_t>(rd_num, i);
+      auto rs1 = f32(READ_FREG(rs1_num));
 
-  vd = rs1;
+      vd = rs1;
+    }
+    break;
+  case e64:
+    for (reg_t i=P.VU.vstart; i<vl; ++i) {
+      auto &vd = P.VU.elt<float64_t>(rd_num, i);
+      auto rs1 = f64(READ_FREG(rs1_num));
+
+      vd = rs1;
+    }
+    break;
 }
 
 P.VU.vstart = 0;
