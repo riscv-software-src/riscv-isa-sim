5048b7b34db49c59340dd80c6188a6a5c2b18dc3
Andrew Waterman <andrew@sifive.com>
2020-01-29 20:13:23 -0800

    Initialize PMPs with set_csr to fix WARLness of initial value

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -254,10 +254,7 @@ void state_t::reset(reg_t max_isa)
   debug_mode = false;
 
   memset(this->pmpcfg, 0, sizeof(this->pmpcfg));
-  pmpcfg[0] = PMP_R | PMP_W | PMP_X | PMP_NAPOT;
-
   memset(this->pmpaddr, 0, sizeof(this->pmpaddr));
-  pmpaddr[0] = ~reg_t(0);
 
   fflags = 0;
   frm = 0;
@@ -1117,6 +1114,12 @@ void processor_t::set_log_commits(bool value)
 void processor_t::reset()
 {
   state.reset(max_isa);
+
+  // For backwards compatibility with software that is unaware of PMP,
+  // initialize PMP to permit unprivileged access to all of memory.
+  set_csr(CSR_PMPADDR0, ~reg_t(0));
+  set_csr(CSR_PMPCFG0, PMP_R | PMP_W | PMP_X | PMP_NAPOT);
+
   state.dcsr.halt = halt_on_reset;
   halt_on_reset = false;
   set_csr(CSR_MSTATUS, state.mstatus);
