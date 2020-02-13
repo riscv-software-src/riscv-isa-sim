169dfe7cebcec8a7200fc9c26bbbbaf1ab1a8081
Max Lin <max.lin@sifive.com>
2020-02-12 18:18:56 -0800

    rvv: vsbc/vmsbc behavior of the sub order

--- riscv/insns/vmsbc_vxm.h
+++ riscv/insns/vmsbc_vxm.h
@@ -6,7 +6,7 @@ VI_XI_LOOP_CARRY
   const uint128_t op_mask = (UINT64_MAX >> (64 - sew));
   uint64_t carry = insn.v_vm() == 0 ? (v0 >> mpos) & 0x1 : 0;
 
-  uint128_t res = (op_mask & rs1) - (op_mask & vs2) - carry;
+  uint128_t res = (op_mask & vs2) - (op_mask & rs1) - carry;
 
   carry = (res >> sew) & 0x1u;
   vd = (vd & ~mmask) | ((carry << mpos) & mmask);
