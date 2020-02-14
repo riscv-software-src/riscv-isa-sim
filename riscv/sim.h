08cf745aaab48d67fafc995663ec96757ff6b816
Anup Patel <anup.patel@wdc.com>
2020-02-14 11:31:38 +0530

    Make spike capable of booting Linux
    Latest Linux does not boot Spike mainly because:
    1. Spike does not set bootargs in DTS
    2. Spike does not provide mechanism to load initrd for Linux
    
    This patch addresses both above issues and we can now
    get latest Linux to prompt on Spike.
    
    Signed-off-by: Anup Patel <anup.patel@wdc.com>
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/sim.h
+++ riscv/sim.h
@@ -22,7 +22,7 @@ class sim_t : public htif_t, public simif_t
 {
 public:
   sim_t(const char* isa, const char* priv, const char* varch, size_t _nprocs,
-        bool halted,
+        bool halted, reg_t initrd_start, reg_t initrd_end,
         reg_t start_pc, std::vector<std::pair<reg_t, mem_t*>> mems,
         std::vector<std::pair<reg_t, abstract_device_t*>> plugin_devices,
         const std::vector<std::string>& args, const std::vector<int> hartids,
@@ -54,6 +54,8 @@ private:
   std::vector<std::pair<reg_t, abstract_device_t*>> plugin_devices;
   mmu_t* debug_mmu;  // debug port into main memory
   std::vector<processor_t*> procs;
+  reg_t initrd_start;
+  reg_t initrd_end;
   reg_t start_pc;
   std::string dts;
   std::unique_ptr<rom_device_t> boot_rom;
