ff0420606101644507673e8a4a8358c51b8654e6
Chih-Min Chao <chihmin.chao@sifive.com>
2020-02-17 20:47:20 -0800

    rvv: use pre-definied v_simm5() macro
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmsleu_vi.h
+++ riscv/insns/vmsleu_vi.h
@@ -1,6 +1,5 @@
 // vsleu.vi vd, vs2, simm5
 VI_VI_ULOOP_CMP
 ({
-  uint64_t imm = (int8_t)(zimm5 << 3) >> 3;
-  res = vs2 <= (imm & (UINT64_MAX >> (64 - P.VU.vsew)));
+  res = vs2 <= (insn.v_simm5() & (UINT64_MAX >> (64 - P.VU.vsew)));
 })
--- riscv/insns/vsaddu_vi.h
+++ riscv/insns/vsaddu_vi.h
@@ -2,8 +2,7 @@
 VI_VI_ULOOP
 ({
   bool sat = false;
-  uint64_t imm = (int8_t)(zimm5 << 3) >> 3;
-  vd = vs2 + (imm & (UINT64_MAX >> (64 - P.VU.vsew)));
+  vd = vs2 + (insn.v_simm5() & (UINT64_MAX >> (64 - P.VU.vsew)));
 
   sat = vd < vs2;
   vd |= -(vd < vs2);
