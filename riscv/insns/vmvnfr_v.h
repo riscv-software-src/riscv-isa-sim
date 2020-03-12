e3cf508b426e072fbe6585148d5adbcdf0e6eb57
Chih-Min Chao <chihmin.chao@sifive.com>
2020-03-11 22:56:02 -0700

    commitlog: fix missing dump for some instructions
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmvnfr_v.h
+++ riscv/insns/vmvnfr_v.h
@@ -6,7 +6,10 @@ const reg_t vs2 = insn.rs2();
 const reg_t len = insn.rs1() + 1;
 require((vd & (len - 1)) == 0);
 require((vs2 & (len - 1)) == 0);
-if (vd != vs2)
-  memcpy(&P.VU.elt<uint8_t>(vd, 0),
-         &P.VU.elt<uint8_t>(vs2, 0), P.VU.vlenb * len);
+if (vd != vs2) {
+  for (reg_t i = 0; i < len; ++i) {
+    memcpy(&P.VU.elt<uint8_t>(vd + i, 0, true),
+           &P.VU.elt<uint8_t>(vs2 + i, 0), P.VU.vlenb);
+  }
+}
 P.VU.vstart = 0;
--- riscv/insns/vrgather_vx.h
+++ riscv/insns/vrgather_vx.h
@@ -10,16 +10,16 @@ reg_t rs1 = RS1;
 VI_LOOP_BASE
   switch (sew) {
   case e8:
-    P.VU.elt<uint8_t>(rd_num, i) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint8_t>(rs2_num, rs1);
+    P.VU.elt<uint8_t>(rd_num, i, true) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint8_t>(rs2_num, rs1);
     break;
   case e16:
-    P.VU.elt<uint16_t>(rd_num, i) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint16_t>(rs2_num, rs1);
+    P.VU.elt<uint16_t>(rd_num, i, true) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint16_t>(rs2_num, rs1);
     break;
   case e32:
-    P.VU.elt<uint32_t>(rd_num, i) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint32_t>(rs2_num, rs1);
+    P.VU.elt<uint32_t>(rd_num, i, true) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint32_t>(rs2_num, rs1);
     break;
   default:
-    P.VU.elt<uint64_t>(rd_num, i) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint64_t>(rs2_num, rs1);
+    P.VU.elt<uint64_t>(rd_num, i, true) = rs1 >= P.VU.vlmax ? 0 : P.VU.elt<uint64_t>(rs2_num, rs1);
     break;
   }
 VI_LOOP_END;
