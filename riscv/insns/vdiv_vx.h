20528ac553b95c446254ce8587f8ecc8a3a0e44d
Chih-Min Chao <chihmin.chao@sifive.com>
2020-03-17 01:43:43 -0700

    rvv: fix vdiv corner case
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vdiv_vx.h
+++ riscv/insns/vdiv_vx.h
@@ -3,7 +3,7 @@ VI_VX_LOOP
 ({
   if(rs1 == 0)
     vd = -1;
-  else if(vs2 == -(1 << (sew - 1)) && rs1 == -1)
+  else if(vs2 == (INT64_MIN >> (64 - sew)) && rs1 == -1)
     vd = vs2;
   else
     vd = vs2 / rs1;
