49ffb3ac6b775b1eb52bbccb1113db6088041b6f
Andrew Waterman <andrew@sifive.com>
2020-03-23 12:41:05 -0700

    Don't acquire load reservation in the event of a fault
    I think this bug wasn't caught because OS code never steps over faulting
    LR instructions in practice.  The exception is either fatal (in which case
    the point is moot) or the LR is re-executed (in which case the point is
    also moot).
    
    Resolves #431

--- riscv/insns/lr_d.h
+++ riscv/insns/lr_d.h
@@ -1,4 +1,5 @@
 require_extension('A');
 require_rv64;
+auto res = MMU.load_int64(RS1);
 MMU.acquire_load_reservation(RS1);
-WRITE_RD(MMU.load_int64(RS1));
+WRITE_RD(res);
--- riscv/insns/lr_w.h
+++ riscv/insns/lr_w.h
@@ -1,3 +1,4 @@
 require_extension('A');
+auto res = MMU.load_int32(RS1);
 MMU.acquire_load_reservation(RS1);
-WRITE_RD(MMU.load_int32(RS1));
+WRITE_RD(res);
