425e39b0094907d237d62d087b146f80b9c62fc5
Chih-Min Chao <chihmin.chao@sifive.com>
2020-03-26 23:26:45 -0700

    rvv: fix vssra.vi e64 corner case
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vssra_vi.h
+++ riscv/insns/vssra_vi.h
@@ -3,7 +3,7 @@ VRM xrm = P.VU.get_vround_mode();
 VI_VI_LOOP
 ({
   int sh = simm5 & (sew - 1) & 0x1f;
-  int64_t val = vs2;
+  int128_t val = vs2;
 
   INT_ROUNDING(val, xrm, sh);
   vd = val >> sh;
