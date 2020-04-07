ddc056fe783ed74f3770ecb4d3d08dc3ecb2495a
Chih-Min Chao <chihmin.chao@sifive.com>
2020-04-07 01:17:01 -0700

    rvv: add widen conversion instructions
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfwcvt_f_x_v.h
+++ riscv/insns/vfwcvt_f_x_v.h
@@ -1,10 +1,10 @@
 // vfwcvt.f.x.v vd, vs2, vm
-VI_CHECK_DSS(false);
-if (P.VU.vsew == e32)
-  require(p->supports_extension('D'));
-
-VI_VFP_LOOP_BASE
+VI_VFP_CVT_SCALE
+({
+  auto vs2 = P.VU.elt<int16_t>(rs2_num, i);
+  P.VU.elt<float32_t>(rd_num, i, true) = i32_to_f32(vs2);
+},
+{
   auto vs2 = P.VU.elt<int32_t>(rs2_num, i);
   P.VU.elt<float64_t>(rd_num, i, true) = i32_to_f64(vs2);
-  set_fp_exceptions;
-VI_VFP_LOOP_WIDE_END
+}, true)
