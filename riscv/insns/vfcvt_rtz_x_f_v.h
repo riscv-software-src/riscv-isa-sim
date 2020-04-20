adb4453324ef14ca1be3ecf1eae8492d1ff6ab8b
Chih-Min Chao <chihmin.chao@sifive.com>
2020-04-19 23:56:20 -0700

    rvv: remove debug message
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfcvt_rtz_x_f_v.h
+++ riscv/insns/vfcvt_rtz_x_f_v.h
@@ -6,7 +6,6 @@ VI_VFP_VF_LOOP
 },
 {
   P.VU.elt<int32_t>(rd_num, i) = f32_to_i32(vs2, softfloat_round_minMag, true);
-  fprintf(stderr, "here 1: %lx\n", i);
 },
 {
   P.VU.elt<int64_t>(rd_num, i) = f64_to_i64(vs2, softfloat_round_minMag, true);
