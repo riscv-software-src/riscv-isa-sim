5f2f08cdc3b0899d6534b73608c31b63d099c86e
Chih-Min Chao <chihmin.chao@sifive.com>
2020-04-23 21:20:32 -0700

    rvv: fix vfncvt.xu.f.w for fp16
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfncvt_xu_f_w.h
+++ riscv/insns/vfncvt_xu_f_w.h
@@ -2,7 +2,7 @@
 VI_VFP_CVT_SCALE
 ({
   auto vs2 = P.VU.elt<float32_t>(rs2_num, i);
-  P.VU.elt<uint16_t>(rd_num, i, true) = f32_to_ui32(vs2, STATE.frm, true);
+  P.VU.elt<uint16_t>(rd_num, i, true) = f32_to_ui16(vs2, STATE.frm, true);
 },
 {
   auto vs2 = P.VU.elt<float64_t>(rs2_num, i);
