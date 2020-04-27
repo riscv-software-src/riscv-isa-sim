020e9aa441adfd3a80959a6bbba181dcd811807e
Chih-Min Chao <chihmin.chao@sifive.com>
2020-04-27 11:11:46 -0700

    rvv: commitlog: fix vmsgtu.vi and vmsleu.vi dst information
    Comparison only writes one vector register
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmsgtu_vi.h
+++ riscv/insns/vmsgtu_vi.h
@@ -1,4 +1,4 @@
-// vsgtu.vi  vd, vd2, simm5
+// vmsgtu.vi  vd, vd2, simm5
 VI_VI_ULOOP_CMP
 ({
   res = vs2 > (insn.v_simm5() & (UINT64_MAX >> (64 - P.VU.vsew)));
--- riscv/insns/vmsleu_vi.h
+++ riscv/insns/vmsleu_vi.h
@@ -1,4 +1,4 @@
-// vsleu.vi vd, vs2, simm5
+// vmsleu.vi vd, vs2, simm5
 VI_VI_ULOOP_CMP
 ({
   res = vs2 <= (insn.v_simm5() & (UINT64_MAX >> (64 - P.VU.vsew)));
