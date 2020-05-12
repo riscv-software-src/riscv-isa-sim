21dd7ed0aba0d58fb81f93b1063f54b42cc9b4f4
Chih-Min Chao <chihmin.chao@sifive.com>
2020-05-11 23:35:38 -0700

    rvv: handle inactive and NaN case for vfredsum
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfredosum_vs.h
+++ riscv/insns/vfredosum_vs.h
@@ -1,4 +1,5 @@
 // vfredosum: vd[0] =  sum( vs2[*] , vs1[0] )
+bool is_propagate = false;
 VI_VFP_VV_LOOP_REDUCTION
 ({
   vd_0 = f16_add(vd_0, vs2);
--- riscv/insns/vfredsum_vs.h
+++ riscv/insns/vfredsum_vs.h
@@ -1,4 +1,5 @@
 // vfredsum: vd[0] =  sum( vs2[*] , vs1[0] )
+bool is_propagate = true;
 VI_VFP_VV_LOOP_REDUCTION
 ({
   vd_0 = f16_add(vd_0, vs2);
