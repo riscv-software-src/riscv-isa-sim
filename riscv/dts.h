e5dfa01f68ab797b15b5ddb012bdbef7e3103196
Scott Beamer <sbeamer@ucsc.edu>
2020-05-18 15:41:32 -0700

    fix clang compile error wthin FDT parsing

--- riscv/dts.h
+++ riscv/dts.h
@@ -13,10 +13,10 @@ std::string make_dts(size_t insns_per_rtc_tick, size_t cpu_hz,
 
 std::string dts_compile(const std::string& dts);
 
-int fdt_parse_clint(void *fdt, unsigned long *clint_addr,
+int fdt_parse_clint(void *fdt, reg_t *clint_addr,
                     const char *compatible);
-int fdt_parse_pmp_num(void *fdt, unsigned long *pmp_num,
+int fdt_parse_pmp_num(void *fdt, reg_t *pmp_num,
                       const char *compatible);
-int fdt_parse_pmp_alignment(void *fdt, unsigned long *pmp_align,
+int fdt_parse_pmp_alignment(void *fdt, reg_t *pmp_align,
                             const char *compatible);
 #endif
