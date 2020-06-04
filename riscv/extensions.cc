4ae7271b61a124b2227594ebec8f95a3e853c4d1
Chih-Min Chao <chihmin.chao@sifive.com>
2020-06-04 00:58:39 -0700

    ext: support default library name and fix isa parser
    for --isa=rv64_zavmo_xmyext
    
    1. make custom extension work with z extension and underline char
    2. search libmyext.so and libcustomext.so
    3. check myext in open library
    4. fix custom extension disassembler initialization bug
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/extensions.cc
+++ riscv/extensions.cc
@@ -21,14 +21,23 @@ std::function<extension_t*()> find_extension(const char* name)
   if (!extensions().count(name)) {
     // try to find extension xyz by loading libxyz.so
     std::string libname = std::string("lib") + name + ".so";
-    if (!dlopen(libname.c_str(), RTLD_LAZY)) {
-      fprintf(stderr, "couldn't find extension '%s' (or library '%s')\n",
-              name, libname.c_str());
-      exit(-1);
+    std::string libdefault = "libcustomext.so";
+    bool is_default = false;
+    auto dlh = dlopen(libname.c_str(), RTLD_LAZY);
+    if (!dlh) {
+      dlh = dlopen(libdefault.c_str(), RTLD_LAZY);
+      if (!dlh) {
+        fprintf(stderr, "couldn't find shared library either '%s' or '%s')\n",
+                libname.c_str(), libdefault.c_str());
+        exit(-1);
+      }
+
+      is_default = true;
     }
+
     if (!extensions().count(name)) {
       fprintf(stderr, "couldn't find extension '%s' in shared library '%s'\n",
-              name, libname.c_str());
+              name, is_default ? libdefault.c_str() : libname.c_str());
       exit(-1);
     }
   }
