d9573e76aacf1e49202534b728a6e55f1368d097
Andrew Waterman <andrew@sifive.com>
2020-06-08 14:12:36 -0700

    Fix priority of misaligned exceptions for store-conditional
    Previously, we unintentionally prioritized access faults and page faults.
    
    Resolves #431

--- riscv/insns/sc_w.h
+++ riscv/insns/sc_w.h
@@ -1,7 +1,10 @@
 require_extension('A');
 
-bool have_reservation = MMU.check_load_reservation(RS1);
-MMU.amo_uint32(RS1, [&](uint32_t lhs) { return have_reservation ? RS2 : lhs; });
+bool have_reservation = MMU.check_load_reservation(RS1, 4);
+
+if (have_reservation)
+  MMU.store_uint32(RS1, RS2);
+
 MMU.yield_load_reservation();
 
 WRITE_RD(!have_reservation);
