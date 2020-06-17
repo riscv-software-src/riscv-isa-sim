8fb303ed37cbee97120009661914217621834dbe
Chih-Min Chao <chihmin.chao@sifive.com>
2020-06-17 10:35:30 -0700

    rvv: make v[sl]1r respect vstart
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vl1r_v.h
+++ riscv/insns/vl1r_v.h
@@ -2,7 +2,7 @@
 require_vector_novtype(true);
 const reg_t baseAddr = RS1;
 const reg_t vd = insn.rd();
-for (reg_t i = 0; i < P.VU.vlenb; ++i) {
+for (reg_t i = P.VU.vstart; i < P.VU.vlenb; ++i) {
   auto val = MMU.load_uint8(baseAddr + i);
   P.VU.elt<uint8_t>(vd, i, true) = val;
 }
