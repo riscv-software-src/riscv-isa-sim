0884e5bbd77a4df950c2ccfff78ad157ffcec631
Chih-Min Chao <chihmin.chao@sifive.com>
2020-06-29 09:58:41 -0700

    rvv: make vmvfnr respect vstart
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmvnfr_v.h
+++ riscv/insns/vmvnfr_v.h
@@ -6,10 +6,9 @@ const reg_t vs2 = insn.rs2();
 const reg_t len = insn.rs1() + 1;
 require((vd & (len - 1)) == 0);
 require((vs2 & (len - 1)) == 0);
-if (vd != vs2) {
-  for (reg_t i = 0; i < len; ++i) {
-    memcpy(&P.VU.elt<uint8_t>(vd + i, 0, true),
-           &P.VU.elt<uint8_t>(vs2 + i, 0), P.VU.vlenb);
-  }
+const reg_t size = len * P.VU.vlenb;
+if (vd != vs2 && P.VU.vstart < size) {
+  memcpy(&P.VU.elt<uint8_t>(vd, P.VU.vstart, true),
+         &P.VU.elt<uint8_t>(vs2, P.VU.vstart), size - P.VU.vstart);
 }
 P.VU.vstart = 0;
