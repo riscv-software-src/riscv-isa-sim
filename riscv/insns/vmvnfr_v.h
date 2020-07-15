cdbaa3121bdd1014dc7a281108854e14fbe88f86
Chih-Min Chao <chihmin.chao@sifive.com>
2020-07-15 00:49:06 -0700

    commitlog: fix vmvnfr.v register information
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vmvnfr_v.h
+++ riscv/insns/vmvnfr_v.h
@@ -4,11 +4,24 @@ const reg_t baseAddr = RS1;
 const reg_t vd = insn.rd();
 const reg_t vs2 = insn.rs2();
 const reg_t len = insn.rs1() + 1;
-require((vd & (len - 1)) == 0);
-require((vs2 & (len - 1)) == 0);
+require_align(vd, len);
+require_align(vs2, len);
 const reg_t size = len * P.VU.vlenb;
+
+//register needs one-by-one copy to keep commitlog correct
 if (vd != vs2 && P.VU.vstart < size) {
-  memcpy(&P.VU.elt<uint8_t>(vd, P.VU.vstart, true),
-         &P.VU.elt<uint8_t>(vs2, P.VU.vstart), size - P.VU.vstart);
+  reg_t i = P.VU.vstart / P.VU.vlenb;
+  reg_t off = P.VU.vstart % P.VU.vlenb;
+  if (off) {
+    memcpy(&P.VU.elt<uint8_t>(vd + i, off, true),
+           &P.VU.elt<uint8_t>(vs2 + i, off), P.VU.vlenb - off);
+    i++;
+  }
+
+  for (; i < len; ++i) {
+    memcpy(&P.VU.elt<uint8_t>(vd + i, 0, true),
+           &P.VU.elt<uint8_t>(vs2 + i, 0), P.VU.vlenb);
+  }
 }
+
 P.VU.vstart = 0;
