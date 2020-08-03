75f9a3d0abdda318620f48b5c55048a69b89aadd
Chih-Min Chao <chihmin.chao@sifive.com>
2020-08-03 00:06:39 -0700

    op: hyperviosr: fix exception code and name
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/ecall.h
+++ riscv/insns/ecall.h
@@ -3,9 +3,9 @@ switch (STATE.prv)
   case PRV_U: throw trap_user_ecall();
   case PRV_S:
     if (STATE.v)
-      throw trap_supervisor_ecall();
+      throw trap_virtual_supervisor_ecall();
     else
-      throw trap_hypervisor_ecall();
+      throw trap_supervisor_ecall();
   case PRV_M: throw trap_machine_ecall();
   default: abort();
 }
