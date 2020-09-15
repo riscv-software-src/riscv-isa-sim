efd664c9605d7cf6b394487dccedb127f9a58915
Han-Kuan Chen <hankuan.chen@sifive.com>
2020-09-16 02:00:46 +0800

    rvv: fix int type is not enough to do shift (#544)
    int can only represent 32 bit in lp64 model
    
    when sew is greater than 32, the behavior is undefined

--- riscv/insns/vrem_vx.h
+++ riscv/insns/vrem_vx.h
@@ -3,7 +3,7 @@ VI_VX_LOOP
 ({
   if (rs1 == 0)
     vd = vs2;
-  else if (vs2 == -(1 << (sew - 1)) && rs1 == -1)
+  else if (vs2 == -(((intmax_t)1) << (sew - 1)) && rs1 == -1)
     vd = 0;
   else
     vd = vs2 % rs1;
