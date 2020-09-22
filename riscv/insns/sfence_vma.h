ac46e18f82fd2c040680633e089860998baa5a5d
Andrew Waterman <andrew@sifive.com>
2020-09-21 17:53:35 -0700

    Raise virtual-instruction traps correctly for WFI/SRET/SFENCE

--- riscv/insns/sfence_vma.h
+++ riscv/insns/sfence_vma.h
@@ -1,3 +1,5 @@
 require_extension('S');
+if (STATE.v && (STATE.prv == PRV_U || get_field(STATE.hstatus, HSTATUS_VTVM)))
+  require_novirt();
 require_privilege(get_field(STATE.mstatus, MSTATUS_TVM) ? PRV_M : PRV_S);
 MMU.flush_tlb();
