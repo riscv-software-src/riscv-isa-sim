9edcf3ece1d75533a1423ddbb22a8e798ea89e49
Chih-Min Chao <chihmin.chao@sifive.com>
2020-11-05 20:02:10 -0800

    dts: extend dts api to get info of each cpu
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/dts.h
+++ riscv/dts.h
@@ -14,10 +14,15 @@ std::string make_dts(size_t insns_per_rtc_tick, size_t cpu_hz,
 
 std::string dts_compile(const std::string& dts);
 
+int fdt_get_offset(void *fdt, const char *field);
+int fdt_get_first_subnode(void *fdt, int node);
+int fdt_get_next_subnode(void *fdt, int node);
+
 int fdt_parse_clint(void *fdt, reg_t *clint_addr,
                     const char *compatible);
 int fdt_parse_pmp_num(void *fdt, reg_t *pmp_num,
                       const char *compatible);
 int fdt_parse_pmp_alignment(void *fdt, reg_t *pmp_align,
                             const char *compatible);
+int fdt_parse_mmu_type(void *fdt, int cpu_offset, char *mmu_type);
 #endif
