b4aa36c4ba326b6ba143b0b0a843e8c72292f69b
Chih-Min Chao <chihmin.chao@sifive.com>
2020-11-15 22:11:34 -0800

    dts: config pmp attribute by each core's setting
    The original implementation only uses the value in first core and apply it
    to other core. The patch makes the configuration hetergeneous for differenct
    cores.
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/dts.h
+++ riscv/dts.h
@@ -20,9 +20,7 @@ int fdt_get_next_subnode(void *fdt, int node);
 
 int fdt_parse_clint(void *fdt, reg_t *clint_addr,
                     const char *compatible);
-int fdt_parse_pmp_num(void *fdt, reg_t *pmp_num,
-                      const char *compatible);
-int fdt_parse_pmp_alignment(void *fdt, reg_t *pmp_align,
-                            const char *compatible);
+int fdt_parse_pmp_num(void *fdt, int cpu_offset, reg_t *pmp_num);
+int fdt_parse_pmp_alignment(void *fdt, int cpu_offset, reg_t *pmp_align);
 int fdt_parse_mmu_type(void *fdt, int cpu_offset, char *mmu_type);
 #endif
