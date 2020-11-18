a6d65893a45c3879d03a59267029a8fa357b40c9
Andrew Waterman <andrew@sifive.com>
2020-11-18 15:50:38 -0800

    Avoid use of __builtin_ctz for portability

--- riscv/insns/bdepw.h
+++ riscv/insns/bdepw.h
@@ -3,7 +3,7 @@ require_extension('B');
 uint64_t c = 0, i = 0, data = zext32(RS1), mask = zext32(RS2);
 while (mask) {
 	uint64_t b = mask & ~((mask | (mask-1)) + 1);
-	c |= (data << (__builtin_ctzl(b) - i)) & b;
+	c |= (data << (ctz(b) - i)) & b;
 	i += __builtin_popcountl(b);
 	mask -= b;
 }
--- riscv/insns/bextw.h
+++ riscv/insns/bextw.h
@@ -3,7 +3,7 @@ require_extension('B');
 uint64_t c = 0, i = 0, data = zext32(RS1), mask = zext32(RS2);
 while (mask) {
 	uint64_t b = mask & ~((mask | (mask-1)) + 1);
-	c |= (data & b) >> (__builtin_ctzl(b) - i);
+	c |= (data & b) >> (ctz(b) - i);
 	i += __builtin_popcountl(b);
 	mask -= b;
 }
