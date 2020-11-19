73fd49feb91b8227463a58afb8b78acc9c07bf8c
Andrew Waterman <andrew@sifive.com>
2020-11-18 16:05:43 -0800

    Avoid use of __builtin_popcount for portability

--- riscv/insns/bextw.h
+++ riscv/insns/bextw.h
@@ -4,7 +4,7 @@ uint64_t c = 0, i = 0, data = zext32(RS1), mask = zext32(RS2);
 while (mask) {
 	uint64_t b = mask & ~((mask | (mask-1)) + 1);
 	c |= (data & b) >> (ctz(b) - i);
-	i += __builtin_popcountl(b);
+	i += popcount(b);
 	mask -= b;
 }
 WRITE_RD(sext32(c));
