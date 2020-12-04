32ba6a0914ff1eca788aa72207a6406d76929181
Dave.Wen <dave.wen@sifive.com>
2020-12-04 00:45:31 -0800

    add support to page on demand
    This patch splites the target-requested memory regions into pages and only
    allocates host memory when it is accessed to reduce larget memory sceniaro
    in 64 bit target system

--- riscv/sim.cc
+++ riscv/sim.cc
@@ -353,7 +353,7 @@ char* sim_t::addr_to_mem(reg_t addr) {
   auto desc = bus.find_device(addr);
   if (auto mem = dynamic_cast<mem_t*>(desc.second))
     if (addr - desc.first < mem->size())
-      return mem->contents() + (addr - desc.first);
+      return mem->contents(addr);
   return NULL;
 }
 
