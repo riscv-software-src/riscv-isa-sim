5d366e46fa1a0cfa3d8a098a9b818606fe575f3f
Andrew Waterman <andrew@sifive.com>
2021-01-08 17:12:32 -0800

    Update Zba/Zbb/Zbc to v0.93; Zbs/Zbe to v0.94-draft

--- riscv/insns/bext.h
+++ riscv/insns/bext.h
@@ -1,9 +1,3 @@
 require_extension('B');
-uint64_t c = 0, i = 0, data = zext_xlen(RS1), mask = zext_xlen(RS2);
-while (mask) {
-	uint64_t b = mask & ~((mask | (mask-1)) + 1);
-	c |= (data & b) >> (ctz(b) - i);
-	i += popcount(b);
-	mask -= b;
-}
-WRITE_RD(sext_xlen(c));
+int shamt = RS2 & (xlen-1);
+WRITE_RD(sext_xlen(1 & (RS1 >> shamt)));
--- riscv/insns/subwu.h
+++
@@ -1,3 +0,0 @@
-require_rv64;
-require_extension('B');
-WRITE_RD(zext32(RS1 - RS2));
