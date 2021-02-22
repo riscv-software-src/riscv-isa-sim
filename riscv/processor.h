779330d79c2d6f860a29585a5dc0d2902e61d561
Scott Johnson <scott.johnson@arilinc.com>
2021-02-22 11:45:41 -0800

    Teach each CSR object its own address
    Since I will need that soon for the logging.

--- riscv/processor.h
+++ riscv/processor.h
@@ -156,6 +156,8 @@ struct type_sew_t<64>
 // Parent, abstract class for all CSRs
 class csr_t {
  public:
+  csr_t(const reg_t addr): address(addr) {}
+
   // read() returns the architectural value of this CSR. No permission
   // checking needed or allowed. Side effects not allowed.
   virtual reg_t read() const noexcept = 0;
@@ -165,6 +167,9 @@ class csr_t {
   virtual void write(const reg_t val) noexcept = 0;
 
   virtual ~csr_t() {}
+
+ private:
+  const reg_t address;
 };
 
 typedef std::shared_ptr<csr_t> csr_t_p;
@@ -172,7 +177,9 @@ typedef std::shared_ptr<csr_t> csr_t_p;
 // Basic CSRs, with XLEN bits fully readable and writable.
 class basic_csr_t: public csr_t {
  public:
-  basic_csr_t(const reg_t init): val(init) {}
+  basic_csr_t(const reg_t addr, const reg_t init):
+    csr_t(addr),
+    val(init) {}
   virtual reg_t read() const noexcept override {
     return val;
   }
