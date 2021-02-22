cc22e7644a91d7b658da8965ea05acf9b5fe6305
Scott Johnson <scott.johnson@arilinc.com>
2021-02-22 13:44:47 -0800

    Add intermediate class that will (soon) log all CSR writes

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -13,9 +13,19 @@ csr_t::csr_t(processor_t* const proc, const reg_t addr):
 csr_t::~csr_t() {
 }
 
+// implement class logged_csr_t
+logged_csr_t::logged_csr_t(processor_t* const proc, const reg_t addr):
+  csr_t(proc, addr) {
+}
+
+void logged_csr_t::write(const reg_t val) noexcept {
+  unlogged_write(val);
+  // Add logging here soon
+}
+
 // implement class basic_csr_t
 basic_csr_t::basic_csr_t(processor_t* const proc, const reg_t addr, const reg_t init):
-  csr_t(proc, addr),
+  logged_csr_t(proc, addr),
   val(init) {
 }
 
@@ -23,6 +33,6 @@ reg_t basic_csr_t::read() const noexcept {
   return val;
 }
 
-void basic_csr_t::write(const reg_t val) noexcept {
+void basic_csr_t::unlogged_write(const reg_t val) noexcept {
   this->val = val;
 }
