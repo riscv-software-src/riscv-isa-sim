4b46f1bedc4bedbf1d57f79363ef81094d9b1de6
Scott Johnson <scott.johnson@arilinc.com>
2021-02-22 13:22:35 -0800

    Extract csr_t implementations to separate file
    This will enable me to use some forward references here soon.

--- riscv/csrs.h
+++ riscv/csrs.h
@@ -11,7 +11,7 @@
 // Parent, abstract class for all CSRs
 class csr_t {
  public:
-  csr_t(const reg_t addr): address(addr) {}
+  csr_t(const reg_t addr);
 
   // read() returns the architectural value of this CSR. No permission
   // checking needed or allowed. Side effects not allowed.
@@ -21,7 +21,7 @@ class csr_t {
   // permission checking needed or allowed.
   virtual void write(const reg_t val) noexcept = 0;
 
-  virtual ~csr_t() {}
+  virtual ~csr_t();
 
  private:
   const reg_t address;
@@ -32,15 +32,9 @@ typedef std::shared_ptr<csr_t> csr_t_p;
 // Basic CSRs, with XLEN bits fully readable and writable.
 class basic_csr_t: public csr_t {
  public:
-  basic_csr_t(const reg_t addr, const reg_t init):
-    csr_t(addr),
-    val(init) {}
-  virtual reg_t read() const noexcept override {
-    return val;
-  }
-  virtual void write(const reg_t val) noexcept override {
-    this->val = val;
-  }
+  basic_csr_t(const reg_t addr, const reg_t init);
+  virtual reg_t read() const noexcept override;
+  virtual void write(const reg_t val) noexcept override;
  private:
   reg_t val;
 };
