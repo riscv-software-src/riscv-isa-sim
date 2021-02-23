1dcead0220c5e5c49583ef67a0c72c53f2dd498b
Chih-Min Chao <chihmin.chao@sifive.com>
2021-02-22 19:59:42 -0800

    rvv: totally remove ediv
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -392,11 +392,9 @@ reg_t processor_t::vectorUnit_t::set_vl(int rd, int rs1, reg_t reqVL, reg_t newT
     vlmax = (VLEN/vsew) * vflmul;
     vta = extract64(newType, 6, 1);
     vma = extract64(newType, 7, 1);
-    vediv = 1 << extract64(newType, 8, 2);
 
     vill = !(vflmul >= 0.125 && vflmul <= 8)
            || vsew > std::min(vflmul, 1.0f) * ELEN
-           || vediv != 1
            || (newType >> 8) != 0;
 
     if (vill) {
