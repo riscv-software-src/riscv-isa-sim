2112cd0025f9d5c1f095756516ef24e6d63f568f
Scott Johnson <scott.johnson@arilinc.com>
2021-02-22 16:04:04 -0800

    Add permission checks to csr_t class
    This is a reimplementation of the check at the end of
    processor_t::get_csr(). That old check will eventually go away once
    all the CSRs are converted to csr_t.

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -3,6 +3,10 @@
 #include "csrs.h"
 // For processor_t:
 #include "processor.h"
+// For get_field():
+#include "decode.h"
+// For trap_virtual_instruction and trap_illegal_instruction:
+#include "trap.h"
 
 
 // implement class csr_t
@@ -10,6 +14,27 @@ csr_t::csr_t(processor_t* const proc, const reg_t addr):
   proc(proc),
   address(addr) {
 }
+
+void csr_t::verify_permissions(insn_t insn, bool write) const {
+  // Check permissions. Raise virtual-instruction exception if V=1,
+  // privileges are insufficient, and the CSR belongs to supervisor or
+  // hypervisor. Raise illegal-instruction exception otherwise.
+  state_t* const state = proc->get_state();
+  unsigned csr_priv = get_field(address, 0x300);
+  bool csr_read_only = get_field(address, 0xC00) == 3;
+  unsigned priv = state->prv == PRV_S && !state->v ? PRV_HS : state->prv;
+
+  if ((csr_priv == PRV_S && !proc->supports_extension('S')) ||
+      (csr_priv == PRV_HS && !proc->supports_extension('H')))
+    throw trap_illegal_instruction(insn.bits());
+
+  if ((write && csr_read_only) || priv < csr_priv) {
+    if (state->v && csr_priv <= PRV_HS)
+      throw trap_virtual_instruction(insn.bits());
+    throw trap_illegal_instruction(insn.bits());
+  }
+}
+
 
 csr_t::~csr_t() {
 }
