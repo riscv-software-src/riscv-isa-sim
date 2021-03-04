c3544f5b11275449e95100855f4d3d466d6b54cb
Scott Johnson <scott.johnson@arilinc.com>
2021-03-04 15:17:18 -0800

    Convert sret to use sstatus
    Part of step 2 of plan described in csrs.h.

--- riscv/insns/sret.h
+++ riscv/insns/sret.h
@@ -7,14 +7,14 @@ if (STATE.v) {
 }
 reg_t next_pc = p->get_state()->sepc->read();
 set_pc_and_serialize(next_pc);
-reg_t s = STATE.mstatus;
+reg_t s = STATE.sstatus->read();
 reg_t prev_prv = get_field(s, MSTATUS_SPP);
 if (prev_prv != PRV_M)
   s = set_field(s, MSTATUS_MPRV, 0);
 s = set_field(s, MSTATUS_SIE, get_field(s, MSTATUS_SPIE));
 s = set_field(s, MSTATUS_SPIE, 1);
 s = set_field(s, MSTATUS_SPP, PRV_U);
-p->set_csr(CSR_MSTATUS, s);
+STATE.sstatus->write(s);
 p->set_privilege(prev_prv);
 if (!STATE.v) {
   reg_t prev_virt = get_field(STATE.hstatus, HSTATUS_SPV);
