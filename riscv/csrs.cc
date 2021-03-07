f4d4a2fa02a71b480638a0ab03a7d6aee4ef5831
Scott Johnson <scott.johnson@arilinc.com>
2021-03-07 15:53:05 -0800

    Move sstatus_proxy read_mask to class level
    So we don't have to recalculate it every time.

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -352,14 +352,14 @@ bool vsstatus_csr_t::unlogged_write(const reg_t val) noexcept {
 // implement class sstatus_proxy_csr_t
 sstatus_proxy_csr_t::sstatus_proxy_csr_t(processor_t* const proc, const reg_t addr):
   logged_csr_t(proc, addr),
-  mstatus(proc->get_state()->mstatus) {
+  mstatus(proc->get_state()->mstatus),
+  read_mask(SSTATUS_SIE | SSTATUS_SPIE | SSTATUS_UBE | SSTATUS_SPP
+            | SSTATUS_FS | (proc->extension_enabled_const('V') ? SSTATUS_VS : 0)
+            | SSTATUS_XS | SSTATUS_SUM | SSTATUS_MXR | SSTATUS_UXL
+            | (proc->get_const_xlen() == 32 ? SSTATUS32_SD : SSTATUS64_SD)) {
 }
 
 reg_t sstatus_proxy_csr_t::read() const noexcept {
-  reg_t read_mask = SSTATUS_SIE | SSTATUS_SPIE | SSTATUS_UBE | SSTATUS_SPP
-                  | SSTATUS_FS | (proc->extension_enabled_const('V') ? SSTATUS_VS : 0)
-                  | SSTATUS_XS | SSTATUS_SUM | SSTATUS_MXR | SSTATUS_UXL
-                  | (proc->get_const_xlen() == 32 ? SSTATUS32_SD : SSTATUS64_SD);
   return mstatus->read() & read_mask;
 }
 
