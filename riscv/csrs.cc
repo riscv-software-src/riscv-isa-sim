5af6547cd3b8dc7a74c61a05f7de7d96a314ee5f
Scott Johnson <scott.johnson@arilinc.com>
2021-03-08 12:42:40 -0800

    Use generic_ip_csr_t for hip

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -609,16 +609,13 @@ hvip_csr_t::hvip_csr_t(processor_t* const proc, const reg_t addr):
 
 // implement class hip_csr_t
 hip_csr_t::hip_csr_t(processor_t* const proc, const reg_t addr):
-  csr_t(proc, addr) {
-}
-
-reg_t hip_csr_t::read() const noexcept {
-  return state->mip->read() & MIP_HS_MASK;
-}
-
-void hip_csr_t::write(const reg_t val) noexcept {
-  const reg_t mask = MIP_VSSIP;
-  state->mip->write_with_mask(mask, val);
+  generic_ip_csr_t(proc,
+                   addr,
+                   MIP_HS_MASK,   // read_mask
+                   MIP_VSSIP,     // write_mask
+                   false,         // mask_mideleg
+                   false,         // mask_hideleg
+                   0) {           // shiftamt
 }
 
 
--- riscv/csrs.h
+++ riscv/csrs.h
@@ -312,12 +312,9 @@ class hvip_csr_t: public generic_ip_csr_t {
   hvip_csr_t(processor_t* const proc, const reg_t addr);
 };
 
-class hip_csr_t: public csr_t {
+class hip_csr_t: public generic_ip_csr_t {
  public:
   hip_csr_t(processor_t* const proc, const reg_t addr);
-
-  virtual reg_t read() const noexcept override;
-  virtual void write(const reg_t val) noexcept override;
 };
 
 class vsip_csr_t: public csr_t {
