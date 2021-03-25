4403faee253339c88d077abf6ef7539febad3e30
Andrew Waterman <andrew@sifive.com>
2021-03-25 16:36:16 -0700

    Fix xperm.[bhn] on RV32
    Need to sign-extend result.

--- riscv/insns/xperm_b.h
+++ riscv/insns/xperm_b.h
@@ -1,2 +1,2 @@
 require_extension('B');
-WRITE_RD(xperm(RS1, RS2, 3, xlen));
+WRITE_RD(sext_xlen(xperm(RS1, RS2, 3, xlen)));
--- riscv/insns/xperm_n.h
+++ riscv/insns/xperm_n.h
@@ -1,2 +1,2 @@
 require_extension('B');
-WRITE_RD(xperm(RS1, RS2, 2, xlen));
+WRITE_RD(sext_xlen(xperm(RS1, RS2, 2, xlen)));
