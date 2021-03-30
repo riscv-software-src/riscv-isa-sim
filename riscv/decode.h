e8b0cf83d965b10947d568f532bfd3bc4b480077
ChunPing Chung <cpchung@pllab.cs.nthu.edu.tw>
2021-03-30 14:15:55 +0800

    Merge pull request #685 from marcfedorow/p-ext
    rvp: sign-extension for p-ext insns

--- riscv/decode.h
+++ riscv/decode.h
@@ -2766,7 +2766,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
 
 #define P_LOOP_END() \
   } \
-  WRITE_RD(rd_tmp);
+  WRITE_RD(sext_xlen(rd_tmp));
 
 #define P_PAIR_LOOP_END() \
   } \
@@ -2775,7 +2775,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
     WRITE_REG(insn.rd() + 1, rd_tmp >> 32); \
   } \
   else { \
-    WRITE_RD(rd_tmp); \
+    WRITE_RD(sext_xlen(rd_tmp)); \
   }
 
 #define P_REDUCTION_LOOP_END(BIT, IS_SAT) \
@@ -2786,7 +2786,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
     type_usew_t<BIT>::type pd = pd_res; \
     WRITE_PD(); \
   } \
-  WRITE_RD(rd_tmp);
+  WRITE_RD(sext_xlen(rd_tmp));
 
 #define P_REDUCTION_ULOOP_END(BIT, IS_SAT) \
     } \
@@ -2796,7 +2796,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
     type_usew_t<BIT>::type pd = pd_res; \
     WRITE_PD(); \
   } \
-  WRITE_RD(rd_tmp);
+  WRITE_RD(sext_xlen(rd_tmp));
 
 #define P_SUNPKD8(X, Y) \
   require_extension('P'); \
@@ -2812,7 +2812,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
   } else { \
     memcpy(&rd_tmp, pd, 4); \
   } \
-  WRITE_RD(rd_tmp);
+  WRITE_RD(sext_xlen(rd_tmp));
 
 #define P_ZUNPKD8(X, Y) \
   require_extension('P'); \
@@ -2828,7 +2828,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
   } else { \
     memcpy(&rd_tmp, pd, 4); \
   } \
-  WRITE_RD(rd_tmp);
+  WRITE_RD(sext_xlen(rd_tmp));
 
 #define P_PK(BIT, X, Y) \
   require_extension('P'); \
@@ -2840,7 +2840,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
     rd_tmp = set_field(rd_tmp, make_mask64((i * 2 + 1) * BIT, BIT), \
       P_UFIELD(RS1, i * 2 + X, BIT)); \
   } \
-  WRITE_RD(rd_tmp);
+  WRITE_RD(sext_xlen(rd_tmp));
 
 #define P_64_PROFILE_BASE() \
   require_extension('P'); \
@@ -2897,7 +2897,7 @@ for (reg_t i = 0; i < P.VU.vlmax && P.VU.vl != 0; ++i) { \
   if (xlen == 32) { \
     WRITE_RD_PAIR(rd); \
   } else { \
-    WRITE_RD(rd); \
+    WRITE_RD(sext_xlen(rd)); \
   }
 
 #define DEBUG_START             0x0
