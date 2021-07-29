2021fdb41f533882605e9cefc30567ed2fb0aead
Andrew Waterman <andrew@sifive.com>
2021-07-28 17:54:49 -0700

    Add NORETURN macro

--- riscv/common.h
+++ riscv/common.h
@@ -6,11 +6,13 @@
 #ifdef __GNUC__
 # define   likely(x) __builtin_expect(x, 1)
 # define unlikely(x) __builtin_expect(x, 0)
+# define NOINLINE __attribute__ ((noinline))
+# define NORETURN __attribute__ ((noreturn))
 #else
 # define   likely(x) (x)
 # define unlikely(x) (x)
+# define NOINLINE
+# define NORETURN
 #endif
-
-#define NOINLINE __attribute__ ((noinline))
 
 #endif
