riscv-software-src/riscv-isa-sim

commit 69008075a1f3f9d7ed6fec3489c50e5d7701d366

Simplify calculation of next_locked_and_tor()

riscv/csrs.cc
@@ -124,8 +124,9 @@ bool pmpaddr_csr_t::unlogged_write(const reg_t val) noexcept {
 bool pmpaddr_csr_t::next_locked_and_tor() const noexcept {
   state_t* const state = proc->get_state();
   size_t i = address - CSR_PMPADDR0;
-  bool next_locked = i+1 < state->max_pmp && (state->pmpcfg[i+1] & PMP_L);
-  bool next_tor = i+1 < state->max_pmp && (state->pmpcfg[i+1] & PMP_A) == PMP_TOR;
+  if (i >= state->max_pmp) return false;  // this is the last entry
+  bool next_locked = state->pmpcfg[i+1] & PMP_L;
+  bool next_tor = (state->pmpcfg[i+1] & PMP_A) == PMP_TOR;
   return next_locked && next_tor;
 }
 

commit 46d511eb966239b3ff3ae4f706bbab6a697e8c66

Fix segfault when executing 'csrw pmpaddr15' when n_pmp==16
Off-by-one error introduced in 69008075a1f3f9d7ed6fec3489c50e5d7701d366

riscv/csrs.cc
@@ -116,7 +116,7 @@ bool pmpaddr_csr_t::unlogged_write(const reg_t val) noexcept {
 }
 
 bool pmpaddr_csr_t::next_locked_and_tor() const noexcept {
-  if (pmpidx >= state->max_pmp) return false;  // this is the last entry
+  if (pmpidx+1 >= state->max_pmp) return false;  // this is the last entry
   bool next_locked = state->pmpaddr[pmpidx+1]->cfg & PMP_L;
   bool next_tor = (state->pmpaddr[pmpidx+1]->cfg & PMP_A) == PMP_TOR;
   return next_locked && next_tor;