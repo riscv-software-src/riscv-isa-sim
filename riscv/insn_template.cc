734745a1d5b4ae93bc7b4f6898071ac3c3f78059
Andrew Waterman <andrew@sifive.com>
2021-09-15 15:35:01 -0700

    Fix signed/unsigned warnings
    The easiest solution was to make xlen a constant, rather than signed
    or unsigned, since it's used in both contexts.

--- riscv/insn_template.cc
+++ riscv/insn_template.cc
@@ -4,18 +4,20 @@
 
 reg_t rv32_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
-  int xlen = 32;
+  #define xlen 32
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
   #include "insns/NAME.h"
   trace_opcode(p, OPCODE, insn);
+  #undef xlen
   return npc;
 }
 
 reg_t rv64_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
-  int xlen = 64;
+  #define xlen 64
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
   #include "insns/NAME.h"
   trace_opcode(p, OPCODE, insn);
+  #undef xlen
   return npc;
 }
