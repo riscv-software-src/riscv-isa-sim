35cfde167a31f19a93557d7ecb41e18a989f85af
liweiwei90 <34847211+liweiwei90@users.noreply.github.com>
2021-10-19 08:23:45 +0800

    Delete SHAMT check in pack (#838)

--- riscv/insns/pack.h
+++ riscv/insns/pack.h
@@ -3,7 +3,6 @@ require(((xlen == 32) && (insn.rs2() == 0) && p->extension_enabled(EXT_ZBB))
   || p->extension_enabled(EXT_ZPN)
   || p->extension_enabled(EXT_ZBKB)
   || p->extension_enabled(EXT_XBITMANIP));
-require(SHAMT < (xlen/2));
 reg_t lo = zext_xlen(RS1 << (xlen/2)) >> (xlen/2);
 reg_t hi = zext_xlen(RS2 << (xlen/2));
 WRITE_RD(sext_xlen(lo | hi));
