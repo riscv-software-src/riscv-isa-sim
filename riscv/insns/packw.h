d22b280198e74b871e04fc0ddb622fb825fdae49
marcfedorow <mark.fedorov@cloudbear.ru>
2021-10-21 00:06:22 +0300

    Fixup of #830 (#839)

--- riscv/insns/packw.h
+++ riscv/insns/packw.h
@@ -2,7 +2,6 @@
 require(((insn.rs2() == 0) && p->extension_enabled(EXT_ZBB))
   || p->extension_enabled(EXT_ZBKB)
   || p->extension_enabled(EXT_XBITMANIP));
-require(SHAMT < (xlen/2));
 require_rv64;
 reg_t lo = zext32(RS1 << 16) >> 16;
 reg_t hi = zext32(RS2 << 16);
