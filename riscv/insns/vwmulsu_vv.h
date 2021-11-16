d5e1a398b0f4ca41f0398fca2156880eacc6893a
eopXD <eop.chen@sifive.com>
2021-11-16 02:34:35 -0800

    Simplify vwmulsu_vv and vwmulsu_vx
    Leverage macro VI_WIDE_OP_AND_ASSIGN_MIX

--- riscv/insns/vwmulsu_vv.h
+++ riscv/insns/vwmulsu_vv.h
@@ -2,15 +2,5 @@
 VI_CHECK_DSS(true);
 VI_VV_LOOP_WIDEN
 ({
-  switch(P.VU.vsew) {
-  case e8:
-    P.VU.elt<uint16_t>(rd_num, i, true) = (int16_t)(int8_t)vs2 * (int16_t)(uint8_t)vs1;
-    break;
-  case e16:
-    P.VU.elt<uint32_t>(rd_num, i, true) = (int32_t)(int16_t)vs2 * (int32_t)(uint16_t)vs1;
-    break;
-  default:
-    P.VU.elt<uint64_t>(rd_num, i, true) = (int64_t)(int32_t)vs2 * (int64_t)(uint32_t)vs1;
-    break;
-  }
+  VI_WIDE_OP_AND_ASSIGN_MIX(vs2, vs1, 0, *, +, uint, int, uint)
 })
