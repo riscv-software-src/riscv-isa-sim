e4dbd47e497246b601fb087f257905a71b83a80a
Tsukasa OI <research_trasio@irq.a4lg.com>
2021-11-27 13:38:26 +0900

    Accept dummy extension: "Zifencei"
    In Spike, `fence.i` instruction is not masked by any extensions.
    I assume that this is required.
    
    This commit adds dummy extension (which does nothing itself)
    "Zifencei" to `processor_t::parse_isa_string` to prevent
    "unsupported extension" error message.

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -254,6 +254,8 @@ void processor_t::parse_isa_string(const char* str)
     } else if (ext_str == "zicsr") {
       // Spike necessarily has Zicsr, because
       // Zicsr is implied by the privileged architecture
+    } else if (ext_str == "zifencei") {
+      // Zifencei is ignored and implied
     } else if (ext_str == "zmmul") {
       extension_table[EXT_ZMMUL] = true;
     } else if (ext_str == "zba") {
