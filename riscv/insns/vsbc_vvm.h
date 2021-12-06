f6a9202f3844e42a6f3f3d5d262fceeceb70e975
eopXD <yueh.ting.chen@gmail.com>
2021-12-07 01:23:49 +0800

    Simplify vadc and vsbc

--- riscv/insns/vsbc_vvm.h
+++ riscv/insns/vsbc_vvm.h
@@ -1,10 +1,5 @@
 // vsbc.vvm vd, vs2, rs1, v0
 VI_VV_LOOP_WITH_CARRY
 ({
-  auto &v0 = P.VU.elt<uint64_t>(0, midx);
-  const uint128_t op_mask = (UINT64_MAX >> (64 - sew));
-  uint64_t carry = (v0 >> mpos) & 0x1;
-
-  uint128_t res = (op_mask & vs2) - (op_mask & vs1) - carry;
-  vd = res;
+  vd = (uint128_t)((op_mask & vs2) - (op_mask & vs1) - carry);
 })
