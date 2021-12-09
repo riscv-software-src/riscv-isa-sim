7953ae66035f7e8385cc5b861ec88f063818a605
marcfedorow <mark.fedorov@cloudbear.ru>
2021-12-10 00:53:38 +0300

    P-ext v.0.9.11. update (#883)

--- riscv/insns/ksubh.h
+++ riscv/insns/ksubh.h
@@ -1,5 +1,5 @@
 require_vector_vs;
 require_extension(EXT_ZPN);
-sreg_t res = (sreg_t)P_SW(RS1, 0) - (sreg_t)P_SW(RS2, 0);
+sreg_t res = (sreg_t)P_SH(RS1, 0) - (sreg_t)P_SH(RS2, 0);
 P_SAT(res, 16);
 WRITE_RD(sext_xlen((int16_t)res));
