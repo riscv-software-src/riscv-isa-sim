d266b96018d5edb6f8f072e14c66a6595ed68491
Andrew Waterman <andrew@sifive.com>
2022-03-15 21:15:45 -0700

    Speed up sstatus_csr_t::dirty(); remove redundant log entries
    dirty() almost never changes any state.  Reading the old dirtiness state
    and comparing is much faster than writing, so gate the write.
    
    This commit does introduce a benign functional change: it eliminates
    logging messages for e.g. FS changing from Dirty to Dirty.

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -454,6 +454,12 @@ sstatus_csr_t::sstatus_csr_t(processor_t* const proc, sstatus_proxy_csr_t_p orig
 }
 
 void sstatus_csr_t::dirty(const reg_t dirties) {
+  // As an optimization, return early if already dirty.
+  if ((orig_sstatus->read() & dirties) == dirties) {
+    if (likely(!state->v || (virt_sstatus->read() & dirties) == dirties))
+      return;
+  }
+
   // Catch problems like #823 where P-extension instructions were not
   // checking for mstatus.VS!=Off:
   if (!enabled(dirties)) abort();
