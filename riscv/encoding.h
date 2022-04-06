b7d9a64c60acf941080ab249d1b764ffa6a007bd
joxie <joxie@nvidia.com>
2022-04-06 15:28:47 +0800

    delete duplicate defines

--- riscv/encoding.h
+++ riscv/encoding.h
@@ -2941,7 +2941,6 @@
 #define CSR_MIP 0x344
 #define CSR_MTINST 0x34a
 #define CSR_MTVAL2 0x34b
-#define CSR_MSECCFG 0x747
 #define CSR_PMPCFG0 0x3a0
 #define CSR_PMPCFG1 0x3a1
 #define CSR_PMPCFG2 0x3a2
