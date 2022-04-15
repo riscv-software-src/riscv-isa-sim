008dc40b1ee8739e1e1ff5c96c0efe4407cb7859
Denis <denis.hoornaert@tum.de>
2022-04-15 14:14:18 +0200

    riscv: cache sim: (1) introduction of the cache_sim_addr_t, (2) transition to vector of vectors for data structure, and (3) use of algorithm library for speedup

--- riscv/eviction_policies.cc
+++ riscv/eviction_policies.cc
@@ -1,91 +1,82 @@
 #include "eviction_policies.h"
-#include <stdio.h>
+#include <iostream>
+#include <algorithm>
 
-uint32_t lfsr_t::next(size_t idx)
+uint32_t lfsr_t::next(const size_t& idx)
 {
     reg = (reg>>1)^(-(reg&1) & 0xd0000001);
     return reg%ways;
 }
 
-void lfsr_t::update(uint64_t addr, uint32_t way, const size_t idx_shift) {}
+void lfsr_t::update(const cache_sim_addr_t& addr, const uint32_t& way) {}
 
-void lfsr_t::insert(size_t idx, size_t way) {}
+void lfsr_t::insert(const size_t& idx, const size_t& way) {}
 
-uint32_t lru_t::lowest_recency_way(size_t idx)
+uint32_t lru_t::lowest_recency_way(const size_t& idx)
 {
-    size_t argmin = 0;
-    uint32_t min_value = 0xffffffff; // Abitrarily big
-    for (size_t i = 0; i < ways; i++) {
-      if (min_value > this->metadata[idx*ways+i]) {
-        min_value = this->metadata[idx*ways+i];
-        argmin = i;
-      }
-    }
-    return argmin;
+    auto begin = this->metadata[idx].begin();
+    auto end = this->metadata[idx].end();
+    return std::distance(begin, std::min_element(begin, end));
 }
 
 // For set denoted by 'idx', give highest recency to 'way' and
 // decrement all elements accessed more recently than 'recency'.
-void lru_t::promote(size_t idx, size_t way, uint32_t recency)
+void lru_t::promote(const size_t& idx, const size_t& way, const uint32_t& recency)
 {
-  this->metadata[idx*ways+way] = ways;
-  for (size_t i = 0; i < ways; i++)
-    if (this->metadata[idx*ways+i] > recency)
-      this->metadata[idx*ways+i]--;
+  this->metadata[idx][way] = ways;
+  for (auto it = this->metadata[idx].begin(); it != this->metadata[idx].end(); it++)
+    if (*it > recency)
+      (*it)--;
 }
 
 // Instruments the 'promote' method to set cachline located in
 // [idx][way] as most recently accessed and decrement all other
 // cache-lines access recency (all recencies bigger than 0 but
 // for the one already at 0).
-void lru_t::set_as_mru(size_t idx, size_t way)
+void lru_t::set_as_mru(const size_t& idx, const size_t& way)
 {
-  this->promote(idx, way, 0);
+  this->metadata[idx][way] = ways;
+  for (auto it = this->metadata[idx].begin(); it != this->metadata[idx].end(); it++)
+    if (*it > 0)
+      (*it)--;
 }
 
-uint32_t lru_t::next(size_t idx)
+uint32_t lru_t::next(const size_t& idx)
 {
   return this->lowest_recency_way(idx);
 }
 
-void lru_t::update(uint64_t addr, uint32_t way, const size_t idx_shift)
+void lru_t::update(const cache_sim_addr_t& addr, const uint32_t& way)
 {
-  size_t idx = (addr >> idx_shift) & (sets-1);
-  uint32_t recency = this->metadata[idx*ways+way];
-  this->promote(idx, way, recency);
+  uint32_t recency = this->metadata[addr.idx][way];
+  this->promote(addr.idx, way, recency);
 };
 
-void lru_t::insert(size_t idx, size_t way)
+void lru_t::insert(const size_t& idx, const size_t& way)
 {
   this->set_as_mru(idx, way);
 }
 
-void fifo_t::update(uint64_t addr, uint32_t way, const size_t idx_shift) {}
+void fifo_t::update(const cache_sim_addr_t& addr, const uint32_t& way)
+{}
 
-
-void fifo_t::insert(size_t idx, size_t way)
+void lip_t::update(const cache_sim_addr_t& addr, const uint32_t& way)
 {
-  this->set_as_mru(idx, way);
+  if (way == lowest_recency_way(addr.idx))
+    this->set_as_mru(addr.idx, way);
 }
 
-void lip_t::update(uint64_t addr, uint32_t way, const size_t idx_shift)
-{
-  size_t idx = (addr >> idx_shift) & (sets-1);
-  if (way == lowest_recency_way(idx))
-    this->set_as_mru(idx, way);
-}
-
-void lip_t::insert(size_t idx, size_t way)
+void lip_t::insert(const size_t& idx, const size_t& way)
 {
   // Inserted at LRU position
-  this->metadata[idx*ways+way] = 0;
+  this->metadata[idx][way] = 0;
 }
 
-void bip_t::insert(size_t idx, size_t way)
+void bip_t::insert(const size_t& idx, const size_t& way)
 {
   if (!(lfsr->next(idx)%threshold)) {
     // Inserted at LRU position
-    this->metadata[idx*ways+way] = 0;
+    this->metadata[idx][way] = 0;
   }
   else {
     this->set_as_mru(idx, way);
--- riscv/eviction_policies.h
+++ riscv/eviction_policies.h
@@ -3,8 +3,10 @@
 #ifndef _RISCV_EVICTION_POLICIES_H
 #define _RISCV_EVICTION_POLICIES_H
 
+#include <vector>
 #include <stdlib.h>
 #include <cstdint>
+#include "cachesim_addr.h"
 
 class eviction_policy_t
 {
@@ -12,11 +14,11 @@ protected:
   size_t sets;
   size_t ways;
 public:
-  eviction_policy_t(size_t sets, size_t ways) : sets(sets), ways(ways) {}
+  eviction_policy_t(const size_t& sets, const size_t& ways) : sets(sets), ways(ways) {}
   eviction_policy_t(const eviction_policy_t& policy) : sets(policy.sets), ways(policy.ways) {}
-  virtual uint32_t next(size_t idx) = 0;
-  virtual void update(uint64_t addr, uint32_t way, const size_t idx_shift) = 0;
-  virtual void insert(size_t idx, size_t way) = 0;
+  virtual uint32_t next(const size_t& idx) = 0;
+  virtual void update(const cache_sim_addr_t& addr, const uint32_t& way) = 0;
+  virtual void insert(const size_t& idx, const size_t& way) = 0;
   virtual ~eviction_policy_t() {};
 };
 
@@ -25,48 +27,47 @@ class lfsr_t : public eviction_policy_t
 private:
   uint32_t reg;
 public:
-  lfsr_t(size_t sets, size_t ways) : eviction_policy_t(sets, ways), reg(0xACE1u) {}
+  lfsr_t(const size_t& sets, const size_t& ways) : eviction_policy_t(sets, ways), reg(0xACE1u) {}
   lfsr_t(const lfsr_t& lfsr) : eviction_policy_t(lfsr), reg(lfsr.reg) {}
-  uint32_t next(size_t idx) override;
-  void update(uint64_t addr, uint32_t way, const size_t idx_shift) override;
-  void insert(size_t idx, size_t way) override;
+  uint32_t next(const size_t& idx) override;
+  void update(const cache_sim_addr_t& addr, const uint32_t& way) override;
+  void insert(const size_t& idx, const size_t& way) override;
   ~lfsr_t() {};
 };
 
 class lru_t : public eviction_policy_t
 {
 protected:
-  uint32_t* metadata;
-  uint32_t lowest_recency_way(size_t idx);
-  void promote(size_t idx, size_t way, uint32_t recency);
-  void set_as_mru(size_t idx, size_t way);
+  std::vector<std::vector<uint32_t>> metadata;
+  uint32_t lowest_recency_way(const size_t& idx);
+  void promote(const size_t& idx, const size_t& way, const uint32_t& recency);
+  void set_as_mru(const size_t& idx, const size_t& way);
 public:
-  lru_t(size_t sets, size_t ways) : eviction_policy_t(sets, ways) {
-    metadata = new uint32_t[sets*ways]();
+  lru_t(const size_t& sets, const size_t& ways) : eviction_policy_t(sets, ways) {
+    metadata.resize(sets);
+    for (size_t i = 0; i < metadata.size(); i++)
+        metadata[i].resize(ways);
   }
-  uint32_t next(size_t idx) override;
-  void update(uint64_t addr, uint32_t way, const size_t idx_shift) override;
-  void insert(size_t idx, size_t way) override;
-  ~lru_t() {
-    delete [] metadata;
-  };
+  uint32_t next(const size_t& idx) override;
+  void update(const cache_sim_addr_t& addr, const uint32_t& way) override;
+  void insert(const size_t& idx, const size_t& way) override;
+  ~lru_t() {};
 };
 
 class fifo_t : public lru_t
 {
 public:
-  fifo_t(size_t sets, size_t ways) : lru_t(sets, ways) {}
-  void update(uint64_t addr, uint32_t way, const size_t idx_shift) override;
-  void insert(size_t idx, size_t way) override;
+  fifo_t(const size_t& sets, const size_t& ways) : lru_t(sets, ways) {}
+  void update(const cache_sim_addr_t& addr, const uint32_t& way) override;
   ~fifo_t() {};
 };
 
 class lip_t : public lru_t
 {
 public:
-  lip_t(size_t sets, size_t ways) : lru_t(sets, ways) {}
-  void update(uint64_t addr, uint32_t way, const size_t idx_shift) override;
-  void insert(size_t idx, size_t way) override;
+  lip_t(const size_t& sets, const size_t& ways) : lru_t(sets, ways) {}
+  void update(const cache_sim_addr_t& addr, const uint32_t& way) override;
+  void insert(const size_t& idx, const size_t& way) override;
   ~lip_t() {};
 };
 
@@ -76,12 +77,12 @@ private:
   uint32_t threshold;
   eviction_policy_t* lfsr;
 public:
-  bip_t(size_t sets, size_t ways, uint32_t threshold) : lip_t(sets, ways), threshold(threshold)
+  bip_t(const size_t& sets, const size_t& ways, const uint32_t& threshold) : lip_t(sets, ways), threshold(threshold)
   {
     lfsr = new lfsr_t(sets, ways);
   }
-  bip_t(size_t sets, size_t ways) : bip_t(sets, ways, 8) {}
-  void insert(size_t idx, size_t way) override;
+  bip_t(const size_t& sets, const size_t& ways) : bip_t(sets, ways, 8) {}
+  void insert(const size_t& idx, const size_t& way) override;
   ~bip_t()
   {
     delete lfsr;
