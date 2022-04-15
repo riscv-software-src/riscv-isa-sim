44edda18fdcfdd2cdc8ebd2604e8e5a3ebae900c
Denis <denis.hoornaert@tum.de>
2022-04-15 14:06:24 +0200

    riscv: cache sim: addition of addr object designed to limit the amount of index and tag extraction (at the cost of memory footprint though...)

---
+++ riscv/cachesim_addr.h
@@ -0,0 +1,25 @@
+#ifndef _RISCV_CACHE_SIM_ADDR_H
+#define _RISCV_CACHE_SIM_ADDR_H
+
+#include <cstdint>
+
+class cache_sim_addr_t
+{
+public:
+  bool valid;
+  bool dirty;
+  uint64_t tag;
+  uint64_t idx;
+
+  cache_sim_addr_t();
+  cache_sim_addr_t(const uint64_t& addr, const uint32_t& sets, const uint32_t& linesz);
+  cache_sim_addr_t(const cache_sim_addr_t& addr);
+  void set_valid();
+  bool is_valid();
+  void set_dirty();
+  bool is_dirty();
+  uint64_t to_uint64(const uint32_t& sets, const uint32_t& linesz);
+  virtual ~cache_sim_addr_t();
+};
+
+#endif
