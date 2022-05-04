a8245e92fb80023cead1a9c6efb00ff98deb5a09
Shaked Flur <fshaked@gmail.com>
2022-05-04 19:43:38 +0100

    Fix the padding of register names in the log (#987)
    This fix print x5 as "x5 ", instead of "x 5".

--- riscv/execute.cc
+++ riscv/execute.cc
@@ -133,7 +133,7 @@ static void commit_log_print_insn(processor_t *p, reg_t pc, insn_t insn)
       if (prefix == 'c')
         fprintf(log_file, " c%d_%s ", rd, csr_name(rd));
       else
-        fprintf(log_file, " %c%2d ", prefix, rd);
+        fprintf(log_file, " %c%-2d ", prefix, rd);
       if (is_vreg)
         commit_log_print_value(log_file, size, &p->VU.elt<uint8_t>(rd, 0));
       else
