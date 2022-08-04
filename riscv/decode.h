caee7f3fa508b0bf84eb3f8d60d6c9e43b0ccf61
Weiwei Li <liweiwei@iscas.ac.cn>
2022-08-04 10:18:19 +0800

    Add stateen related check for float point instructions

--- riscv/decode.h
+++ riscv/decode.h
@@ -292,7 +292,8 @@ do { \
 #define require_extension(s) require(p->extension_enabled(s))
 #define require_either_extension(A,B) require(p->extension_enabled(A) || p->extension_enabled(B));
 #define require_impl(s) require(p->supports_impl(s))
-#define require_fp          require(STATE.sstatus->enabled(SSTATUS_FS))
+#define require_fs          require(STATE.sstatus->enabled(SSTATUS_FS))
+#define require_fp          STATE.fflags->verify_permissions(insn, false)
 #define require_accelerator require(STATE.sstatus->enabled(SSTATUS_XS))
 #define require_vector_vs   require(STATE.sstatus->enabled(SSTATUS_VS))
 #define require_vector(alu) \
