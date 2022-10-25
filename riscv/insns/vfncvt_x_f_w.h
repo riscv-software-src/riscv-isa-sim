1f1a7da6babfc68c2e2f87f5b252819bcc50195d
Andrew Waterman <andrew@sifive.com>
2022-10-25 16:27:57 -0700

    Change remaining vector FP16 instructions to require Zvfh
    ...as opposed to Zfh.

--- riscv/insns/vfncvt_x_f_w.h
+++ riscv/insns/vfncvt_x_f_w.h
@@ -3,7 +3,7 @@ VI_VFP_NCVT_FP_TO_INT(
   { vd = f16_to_i8(vs2, softfloat_roundingMode, true); },  // BODY16
   { vd = f32_to_i16(vs2, softfloat_roundingMode, true); }, // BODY32
   { vd = f64_to_i32(vs2, softfloat_roundingMode, true); }, // BODY64
-  { require_extension(EXT_ZFH); },                         // CHECK16
+  { require_extension(EXT_ZVFH); },                        // CHECK16
   { require(p->extension_enabled('F')); },                 // CHECK32
   { require(p->extension_enabled('D')); },                 // CHECK64
   int                                                      // sign
--- riscv/insns/vfwcvt_f_x_v.h
+++ riscv/insns/vfwcvt_f_x_v.h
@@ -1,10 +1,10 @@
 // vfwcvt.f.x.v vd, vs2, vm
 VI_VFP_WCVT_INT_TO_FP(
-  { vd = i32_to_f16(vs2); },                   // BODY8
-  { vd = i32_to_f32(vs2); },                   // BODY16
-  { vd = i32_to_f64(vs2); },                   // BODY32
-  { require(p->extension_enabled(EXT_ZFH)); }, // CHECK8
-  { require_extension('F'); },                 // CHECK16
-  { require_extension('D'); },                 // CHECK32
-  int                                          // sign
+  { vd = i32_to_f16(vs2); },                    // BODY8
+  { vd = i32_to_f32(vs2); },                    // BODY16
+  { vd = i32_to_f64(vs2); },                    // BODY32
+  { require(p->extension_enabled(EXT_ZVFH)); }, // CHECK8
+  { require_extension('F'); },                  // CHECK16
+  { require_extension('D'); },                  // CHECK32
+  int                                           // sign
 )
