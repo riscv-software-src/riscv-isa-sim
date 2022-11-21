0e6c3960be2507287f1d4051c7eaa6ea561574a4
YenHaoChen <39526191+YenHaoChen@users.noreply.github.com>
2022-11-22 05:52:40 +0800

    When resuming from debug mode, clear mstatus.MPRV if the new privilege mode is less than M-mode (#1149)

--- riscv/insns/dret.h
+++ riscv/insns/dret.h
@@ -1,6 +1,8 @@
 require(STATE.debug_mode);
 set_pc_and_serialize(STATE.dpc->read());
 p->set_privilege(STATE.dcsr->prv);
+if (STATE.prv < PRV_M)
+  STATE.mstatus->write(STATE.mstatus->read() & ~MSTATUS_MPRV);
 
 /* We're not in Debug Mode anymore. */
 STATE.debug_mode = false;
