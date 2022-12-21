9152a02ac10131ad6880f7d724b354fa64fc8ce4
Andrew Waterman <andrew@sifive.com>
2022-12-20 22:26:09 -0800

    Reduce compile time increase introduced by #1189
    Generate one object file per instruction rather than two, which
    reduces the overhead of invoking the compiler so many times.
    
    This also reduces the size of the (unstripped) binary substantially.

--- riscv/insn_template.cc
+++ riscv/insn_template.cc
@@ -1,9 +1,11 @@
 // See LICENSE for license details.
 
-#include "insn_template_TYPE.h"
+#include "insn_template.h"
 #include "insn_macros.h"
 
-reg_t TYPE_rv32i_NAME(processor_t* p, insn_t insn, reg_t pc)
+#define DECODE_MACRO_USAGE_LOGGED 0
+
+reg_t fast_rv32i_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 32
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
@@ -13,7 +15,30 @@ reg_t TYPE_rv32i_NAME(processor_t* p, insn_t insn, reg_t pc)
   return npc;
 }
 
-reg_t TYPE_rv64i_NAME(processor_t* p, insn_t insn, reg_t pc)
+reg_t fast_rv64i_NAME(processor_t* p, insn_t insn, reg_t pc)
+{
+  #define xlen 64
+  reg_t npc = sext_xlen(pc + insn_length(OPCODE));
+  #include "insns/NAME.h"
+  trace_opcode(p, OPCODE, insn);
+  #undef xlen
+  return npc;
+}
+
+#undef DECODE_MACRO_USAGE_LOGGED
+#define DECODE_MACRO_USAGE_LOGGED 1
+
+reg_t logged_rv32i_NAME(processor_t* p, insn_t insn, reg_t pc)
+{
+  #define xlen 32
+  reg_t npc = sext_xlen(pc + insn_length(OPCODE));
+  #include "insns/NAME.h"
+  trace_opcode(p, OPCODE, insn);
+  #undef xlen
+  return npc;
+}
+
+reg_t logged_rv64i_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 64
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
@@ -26,7 +51,10 @@ reg_t TYPE_rv64i_NAME(processor_t* p, insn_t insn, reg_t pc)
 #undef CHECK_REG
 #define CHECK_REG(reg) require((reg) < 16)
 
-reg_t TYPE_rv32e_NAME(processor_t* p, insn_t insn, reg_t pc)
+#undef DECODE_MACRO_USAGE_LOGGED
+#define DECODE_MACRO_USAGE_LOGGED 0
+
+reg_t fast_rv32e_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 32
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
@@ -36,7 +64,30 @@ reg_t TYPE_rv32e_NAME(processor_t* p, insn_t insn, reg_t pc)
   return npc;
 }
 
-reg_t TYPE_rv64e_NAME(processor_t* p, insn_t insn, reg_t pc)
+reg_t fast_rv64e_NAME(processor_t* p, insn_t insn, reg_t pc)
+{
+  #define xlen 64
+  reg_t npc = sext_xlen(pc + insn_length(OPCODE));
+  #include "insns/NAME.h"
+  trace_opcode(p, OPCODE, insn);
+  #undef xlen
+  return npc;
+}
+
+#undef DECODE_MACRO_USAGE_LOGGED
+#define DECODE_MACRO_USAGE_LOGGED 1
+
+reg_t logged_rv32e_NAME(processor_t* p, insn_t insn, reg_t pc)
+{
+  #define xlen 32
+  reg_t npc = sext_xlen(pc + insn_length(OPCODE));
+  #include "insns/NAME.h"
+  trace_opcode(p, OPCODE, insn);
+  #undef xlen
+  return npc;
+}
+
+reg_t logged_rv64e_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 64
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
--- riscv/insn_template_logged.h
+++
@@ -1,4 +0,0 @@
-// See LICENSE for license details
-
-#define DECODE_MACRO_USAGE_LOGGED
-#include "insn_template.h"
