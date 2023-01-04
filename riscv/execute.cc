riscv-software-src/riscv-isa-sim

commit 20e7f5363b6a383b4baa01f3c8a9b29fc47549a4

Prevent processor_t from retiring instructions after a WFI

riscv/execute.cc
@@ -242,6 +242,10 @@ void processor_t::step(size_t n)
     try
     {
       take_pending_interrupt();
+      if (unlikely(in_wfi)) {
+        // No unmasked pending interrupt, so remain in wfi
+        throw wait_for_interrupt_t();
+      }
 
       if (unlikely(slow_path()))
       {
@@ -321,6 +325,7 @@ void processor_t::step(size_t n)
       // allows us to switch to other threads only once per idle loop in case
       // there is activity.
       n = ++instret;
+      in_wfi = true;
     }
 
     state.minstret->bump(instret);

riscv/processor.cc
@@ -35,6 +35,7 @@ processor_t::processor_t(const isa_parser_t *isa, const char* varch,
   : debug(false), halt_request(HR_NONE), isa(isa), sim(sim), id(id), xlen(0),
   histogram_enabled(false), log_commits_enabled(false),
   log_file(log_file), sout_(sout_.rdbuf()), halt_on_reset(halt_on_reset),
+  in_wfi(false),
   impl_table(256, false), last_pc(1), executions(1), TM(4)
 {
   VU.p = this;
@@ -626,6 +627,9 @@ void processor_t::take_interrupt(reg_t pending_interrupts)
   if (!pending_interrupts) {
     return;
   }
+
+  // Exit WFI if there are any pending interrupts
+  in_wfi = false;
 
   // M-ints have higher priority over HS-ints and VS-ints
   const reg_t mie = get_field(state.mstatus->read(), MSTATUS_MIE);

riscv/processor.h
@@ -292,6 +292,7 @@ private:
   FILE *log_file;
   std::ostream sout_; // needed for socket command interface -s, also used for -d and -l, but not for --log
   bool halt_on_reset;
+  bool in_wfi;
   std::vector<bool> impl_table;
 
   std::vector<insn_desc_t> instructions;

commit 7334b0180e010dc7b9b89128db53045fe67bf3f4

Fix debug-mode regression introduced by 20e7f53
 - Debug mode should break the processor out of wfi
 - wfi in debug mode should execute as a nop

riscv/execute.cc
@@ -203,7 +203,7 @@ static inline reg_t execute_insn_logged(processor_t* p, reg_t pc, insn_fetch_t f
 bool processor_t::slow_path()
 {
   return debug || state.single_step != state.STEP_NONE || state.debug_mode ||
-         log_commits_enabled || histogram_enabled;
+         log_commits_enabled || histogram_enabled || in_wfi;
 }
 
 // fetch/decode/execute loop
@@ -242,10 +242,6 @@ void processor_t::step(size_t n)
     try
     {
       take_pending_interrupt();
-      if (unlikely(in_wfi)) {
-        // No unmasked pending interrupt, so remain in wfi
-        throw wait_for_interrupt_t();
-      }
 
       if (unlikely(slow_path()))
       {
@@ -265,6 +261,12 @@ void processor_t::step(size_t n)
             state.single_step = state.STEP_STEPPED;
           }
 
+          // debug mode wfis must nop
+          if (unlikely(in_wfi && !state.debug_mode)) {
+            throw wait_for_interrupt_t();
+          }
+
+          in_wfi = false;
           insn_fetch_t fetch = mmu->load_insn(pc);
           if (debug && !state.serialized)
             disasm(fetch.insn);

riscv/processor.cc
@@ -537,6 +537,7 @@ void processor_t::reset()
   state.dcsr->halt = halt_on_reset;
   halt_on_reset = false;
   VU.reset();
+  in_wfi = false;
 
   if (n_pmp > 0) {
     // For backwards compatibility with software that is unaware of PMP,
@@ -744,6 +745,7 @@ void processor_t::enter_debug_mode(uint8_t cause)
   set_privilege(PRV_M);
   state.dpc->write(state.pc);
   state.pc = DEBUG_ROM_ENTRY;
+  in_wfi = false;
 }
 
 void processor_t::debug_output_log(std::stringstream *s)