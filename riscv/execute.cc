3d63b06faee13536710fc7db08979797182684fb
Andrew Waterman <andrew@sifive.com>
2023-01-03 22:21:26 -0800

    Only update histogram when histogramming
    This is worth a 1.4x speedup on the slow path (when not histogramming).

--- riscv/execute.cc
+++ riscv/execute.cc
@@ -152,7 +152,8 @@ static void commit_log_print_insn(processor_t *p, reg_t pc, insn_t insn)
 
 inline void processor_t::update_histogram(reg_t UNUSED pc)
 {
-  pc_histogram[pc]++;
+  if (histogram_enabled)
+    pc_histogram[pc]++;
 }
 
 // These two functions are expected to be inlined by the compiler separately in
