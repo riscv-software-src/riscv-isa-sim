0f7946243ee9163bf79eb967ac1d241992c66a50
Jerry Zhao <jerryz123@berkeley.edu>
2023-02-05 13:30:51 -0800

    Set cfg-provided processor_t.pmp_num before parsing the dtb
    This makes proc_t respect cfg->pmpregions even if no dtb parsing
    is performed

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -58,7 +58,7 @@ processor_t::processor_t(const isa_parser_t *isa, const cfg_t *cfg,
     register_extension(e.second);
 
   set_pmp_granularity(1 << PMP_SHIFT);
-  set_pmp_num(state.max_pmp);
+  set_pmp_num(cfg->pmpregions);
 
   if (isa->get_max_xlen() == 32)
     set_mmu_capability(IMPL_MMU_SV32);
