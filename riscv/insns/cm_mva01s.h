b3ef6f76635436c7e9a7edc2a779060ca6c8be1f
Weiwei Li <liweiwei@iscas.ac.cn>
2023-02-16 10:51:12 +0800

    Remove redundant RVE related check in cm.mva01s and cm.mvsa01
    r1s/r2s > 16 when r1sc/r1sc >2. This will also trigger illegal
    instruction exception by CHECK_REG if RVE is enabled

--- riscv/insns/cm_mva01s.h
+++ riscv/insns/cm_mva01s.h
@@ -1,6 +1,3 @@
 require_extension(EXT_ZCMP);
-if (p->extension_enabled('E')) {
-  require((insn.rvc_r1sc() < 2) && (insn.rvc_r2sc() < 2));
-}
 WRITE_REG(X_A0, READ_REG(RVC_R1S));
 WRITE_REG(X_A1, READ_REG(RVC_R2S));
--- riscv/insns/cm_mvsa01.h
+++ riscv/insns/cm_mvsa01.h
@@ -1,7 +1,4 @@
 require_extension(EXT_ZCMP);
-if (p->extension_enabled('E')) {
-  require((insn.rvc_r1sc() < 2) && (insn.rvc_r2sc() < 2));
-}
 require(insn.rvc_r1sc() != insn.rvc_r2sc());
 WRITE_REG(RVC_R1S, READ_REG(X_A0));
 WRITE_REG(RVC_R2S, READ_REG(X_A1));
