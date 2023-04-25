ebe3f8c6ea70e45325f44f8d5d6e1101f94becb1
Zhong Chengyong <zhongcy93@gmail.com>
2023-04-25 16:49:54 +0800

    csrrw/csrrwi should NOT read CSR when rd is zero According to the RISC-V spec, csrrw/csrrwi shouldn't read CSR when rd is zero, otherwise may cause side effects on user-customized registers

--- riscv/insns/csrrw.h
+++ riscv/insns/csrrw.h
@@ -1,5 +1,9 @@
 int csr = validate_csr(insn.csr(), true);
-reg_t old = p->get_csr(csr, insn, true);
+bool read = insn.rd() != 0;
+reg_t old = 0;
+if (read) {
+  old = p->get_csr(csr, insn, true);
+}
 p->put_csr(csr, RS1);
 WRITE_RD(sext_xlen(old));
 serialize();
