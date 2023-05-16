d9e30bb6970c3e387d719b3fd0808937889ca3a6
Atul Khare <atulkhare@rivosinc.com>
2023-05-16 15:11:44 -0700

    triggers: Fix etrigger match on exceptions
    The etrigger match on exceptions doesn't work properly in cases like
    the following:
    
    1) M-mode delegates ECALLs to S-mode
    2) A CPU hardware point mechanism is used to place a breakpoint on the
       Umode instruction that executes the ECALL from Umode to Smode. In
       effect, this creates a breakpoint etrigger based on Umode.
    
    In the above, the expectation is that #2 will first cause an exit to
    the Smode handler (stvec), and the hardware breakpoint exception will
    be triggered following an entry into the handler.
    
    However, since etrigger currently checks the current privilege mode, we
    will never get a match on conditions like #2.
    
    The patch attempts to address the issue by using the stashed version of
    the previous privilege mode for the etrigger match.
    
    cc: YenHaoChen <howard25336284@gmail.com>
    Signed-off-by: Atul Khare <atulkhare@rivosinc.com>

--- riscv/triggers.h
+++ riscv/triggers.h
@@ -90,7 +90,7 @@ public:
 
 protected:
   static action_t legalize_action(reg_t val, reg_t action_mask, reg_t dmode_mask) noexcept;
-  bool common_match(processor_t * const proc) const noexcept;
+  bool common_match(processor_t * const proc, bool use_prev_prv = false) const noexcept;
   bool allow_action(const state_t * const state) const;
   reg_t tdata2;
 
