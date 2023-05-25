c231e0c9f6dcf3e9fe0637724d8672560d62c58e
Gianluca Guida <gianluca@rivosinc.com>
2023-05-25 14:43:57 +0100

    Fix check for extension
    Calling 'extension_enabled' this early during the constructor of
    'processor_t' causes SIGSEGV.

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -43,7 +43,7 @@ processor_t::processor_t(const isa_parser_t *isa, const cfg_t *cfg,
   TM.proc = this;
 
 #ifndef HAVE_INT128
-  if (extension_enabled('V')) {
+  if (isa->extension_enabled('V')) {
     fprintf(stderr, "V extension is not supported on platforms without __int128 type\n");
     abort();
   }
