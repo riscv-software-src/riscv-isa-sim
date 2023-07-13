bea82f174839b099e0efd8c8db3b3b9e8519028a
Yinan Xu <xuyinan@ict.ac.cn>
2023-07-13 16:14:10 +0800

    csr,tvec: mode is non-writable for CPU_NUTSHELL

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -384,7 +384,11 @@ reg_t tvec_csr_t::read() const noexcept {
 }
 
 bool tvec_csr_t::unlogged_write(const reg_t val) noexcept {
+#if defined(CPU_NUTSHELL)
+  this->val = val & ~(reg_t)3;
+#else
   this->val = val & ~(reg_t)2;
+#endif
   return true;
 }
 
