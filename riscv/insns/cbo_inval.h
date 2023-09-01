53ddeadcd3803e8a0598b98b29d3754f43e5fe26
viktoryou <143797577+viktoryou@users.noreply.github.com>
2023-09-01 19:36:35 +0800

    fix condition of executing cbo.inval as a flush operation
    Signed-off-by: viktoryou <143797577+viktoryou@users.noreply.github.com>

--- riscv/insns/cbo_inval.h
+++ riscv/insns/cbo_inval.h
@@ -3,7 +3,7 @@ DECLARE_XENVCFG_VARS(CBIE);
 require_envcfg(CBIE);
 if (((STATE.prv != PRV_M) && (mCBIE == 1)) ||
     ((!STATE.v && (STATE.prv == PRV_U)) && (sCBIE = 1)) ||
-    (STATE.v && ((hCBIE == 1) || ((STATE.prv == PRV_U) && (sCBIE== 0)))))
+    (STATE.v && ((hCBIE == 1) || ((STATE.prv == PRV_U) && (sCBIE== 1)))))
   MMU.clean_inval(RS1, true, true);
 else
   MMU.clean_inval(RS1, false, true);
