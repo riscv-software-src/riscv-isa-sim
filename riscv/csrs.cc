80a3c8160f25fd53d412362edbbc2998be859b7d
Yinan Xu <xuyinan@ict.ac.cn>
2023-09-17 15:15:14 +0800

    Update writable mask for `misa` of XiangShan

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -656,7 +656,7 @@ misa_csr_t::misa_csr_t(processor_t* const proc, const reg_t addr, const reg_t ma
                         | (1L << ('C' - 'A'))
                         | (1L << ('V' - 'A'))
                         )
-#elif defined(CPU_NUTSHELL)
+#elif defined(CPU_NUTSHELL) || defined (CPU_XIANGSHAN)
   write_mask(0 // not allowed
 #else
   write_mask(max_isa & (0  // allow MAFDQCHV bits in MISA to be modified
