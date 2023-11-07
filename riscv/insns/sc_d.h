fc5f1c056dce1b30b441de5f25ecd55fbfea6bcd
=youzi27 <=809891392@qq.com>
2023-11-07 19:46:00 +0800

    sc_return_on_trap

--- riscv/insns/sc_d.h
+++ riscv/insns/sc_d.h
@@ -1,6 +1,8 @@
 require_extension('A');
 require_rv64;
 
+WRITE_RD(!false);
+
 bool have_reservation = MMU.store_conditional<uint64_t>(RS1, RS2);
 
 WRITE_RD(!have_reservation);
--- riscv/insns/sc_w.h
+++ riscv/insns/sc_w.h
@@ -1,5 +1,7 @@
 require_extension('A');
 
+WRITE_RD(!false);
+
 bool have_reservation = MMU.store_conditional<uint32_t>(RS1, RS2);
 
 WRITE_RD(!have_reservation);
