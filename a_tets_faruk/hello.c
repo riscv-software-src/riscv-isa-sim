77983e82e85de6cc7ae7b07ed567b17530b755f7
farukyld <123uzunince123@gmail.com>
2023-11-17 17:16:55 +0300

    make first tests

---
+++ a_tets_faruk/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    a++;
+    printf("Value of a: %d\n", a);
+    return a;
+}
---
+++ a_tets_faruk/hello.s
@@ -0,0 +1,36 @@
+	.file	"hello.c"
+	.option nopic
+	.attribute arch, "rv64i2p0_m2p0_a2p0_f2p0_d2p0_c2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.section	.rodata
+	.align	3
+.LC0:
+	.string	"Value of a: %d\n"
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-32
+	sd	ra,24(sp)
+	sd	s0,16(sp)
+	addi	s0,sp,32
+	sw	zero,-20(s0)
+	lw	a5,-20(s0)
+	addiw	a5,a5,1
+	sw	a5,-20(s0)
+	lw	a5,-20(s0)
+	mv	a1,a5
+	lui	a5,%hi(.LC0)
+	addi	a0,a5,%lo(.LC0)
+	call	printf
+	lw	a5,-20(s0)
+	mv	a0,a5
+	ld	ra,24(sp)
+	ld	s0,16(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (g2ee5e430018) 12.2.0"
