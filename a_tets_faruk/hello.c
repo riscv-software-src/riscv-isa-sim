015942658710928ee8b438f82ba252d56cae0365
farukyld <123uzunince123@gmail.com>
2023-11-20 14:02:50 +0300

    try scanf format ending with \n

--- a_tets_faruk/hello.c
+++ a_tets_faruk/hello.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+int array[10];
+
 int main() {
     int a = 0;
-    a++;
-    printf("Value of a: %d\n", a);
+
+    FILE* read_write = fopen("floating_file","r+");
+    fprintf(read_write,"5\n");
+    fflush(read_write);
+    rewind(read_write);
+    scanf("%d\n",&a);
+    printf("a after first scan: %d\n",a);
+    fflush(stdin);
+    fflush(stdout);
+    fscanf(read_write,"%d\n",&a);
+    printf("a after second scan: %d\n",a);
+
     return a;
 }
