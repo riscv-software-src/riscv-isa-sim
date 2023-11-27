17df91bdc20468610191fb2549c2e1c104f27da3
farukyld <123uzunince123@gmail.com>
2023-11-27 16:44:30 +0300

    add new test fils

---
+++ a_tets_faruk/memset_optimization/large_memory_set.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 100000
+
+int arr[SIZE];
+int main() {
+
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = 0;
+    }
+    printf("%d",arr[8]);
+    // Rest of the program...
+}
