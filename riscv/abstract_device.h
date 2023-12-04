b98e922cb42d4e7ebe7e76ce413acc8594294068
joey0320 <joonho0320@gmail.com>
2023-12-03 16:22:56 -0800

    Fix Spike --device option to pass on args to downstream plugins

--- riscv/abstract_device.h
+++ riscv/abstract_device.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <map>
 #include <stdexcept>
+#include <vector>
 
 class sim_t;
 
@@ -26,10 +27,15 @@ public:
   virtual abstract_device_t* parse_from_fdt(const void* fdt, const sim_t* sim, reg_t* base) const = 0;
   virtual std::string generate_dts(const sim_t* sim) const = 0;
   virtual ~device_factory_t() {}
+  void set_sargs(std::vector<std::string> sargs) { this->sargs = sargs; }
+  std::vector<std::string> get_sargs() { return sargs; }
+
+protected:
+  std::vector<std::string> sargs;
 };
 
 // Type for holding all registered MMIO plugins by name.
-using mmio_device_map_t = std::map<std::string, const device_factory_t*>;
+using mmio_device_map_t = std::map<std::string, device_factory_t*>;
 
 mmio_device_map_t& mmio_device_map();
 
@@ -40,8 +46,8 @@ mmio_device_map_t& mmio_device_map();
     std::string str(#name); \
     if (!mmio_device_map().emplace(str, this).second) throw std::runtime_error("Plugin \"" + str + "\" already registered"); \
   }; \
-  name##_t* parse_from_fdt(const void* fdt, const sim_t* sim, reg_t* base) const override { return parse(fdt, sim, base); } \
+  name##_t* parse_from_fdt(const void* fdt, const sim_t* sim, reg_t* base) const override { return parse(fdt, sim, base, sargs); } \
   std::string generate_dts(const sim_t* sim) const override { return generate(sim); } \
-  }; const device_factory_t *name##_factory = new name##_factory_t();
+  }; device_factory_t *name##_factory = new name##_factory_t();
 
 #endif
