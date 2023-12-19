e2184b65827d00753a618a1d274fe112e569dafb
farukyld <123uzunince123@gmail.com>
2023-12-19 16:37:31 +0300

    update a_test_faruk/minimal

--- a_tets_faruk/minimal/src/hello.c
+++ a_tets_faruk/minimal/src/hello.c
@@ -1,11 +1,33 @@
 
 
 
-char tohost[1000];
-char fromhost[1000];
-// #include <stdio.h>
+// long int tohost;
+// long int fromhost;
+
+#define SIZE 10
+
+long int modular_random() {
+    static long int state = -1;  // Static state variable
+
+    // Constants for the linear congruential generator
+    long int a = 1664525;       // Multiplier
+    long int c = 1013904223;    // Increment
+    long int m = 4294967296;    // Modulus (2^32)
+
+    // Update the state using the LCG formula
+    state = (a * state + c) % m;
+
+    return state;
+}
+
+
 
 void main() {
-    int a = 0;
-    // scanf("%d",&a);
+    int a[SIZE];
+    for (int ii =0; ii < SIZE; ii++){
+        a[ii] = modular_random();
+    }
+    __asm(
+        "nop\n"
+        "nop\n");
 }
