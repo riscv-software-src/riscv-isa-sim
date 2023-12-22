c5b6ebb2aa453cef8d8eb1b8c2c644a65d11d62b
farukyld <123uzunince123@gmail.com>
2023-12-22 12:24:52 +0300

    adjust gitignore for dpi header

---
+++ a_tets_faruk/verilator_verilog/obj_dir/Vtb__Dpi.h
@@ -0,0 +1,26 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VTB__DPI_H_
+#define VERILATED_VTB__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at src/verilog/tb.sv:4:31
+    extern int cpp_test();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
