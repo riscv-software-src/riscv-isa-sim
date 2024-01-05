1f7ff75df4a0bfb8d25a14cf1f730b5665fb36bb
farukyld <123uzunince123@gmail.com>
2024-01-05 11:54:02 +0300

    rename file

--- a_tets_faruk/spike_link/obj_dir_tb_spike_link/tb_spike_link__Dpi.h
+++ a_tets_faruk/spike_link/obj_dir_tb_spike_link/tb_spike_link__Dpi.h
@@ -5,8 +5,8 @@
 // Manually include this file where DPI .c import functions are declared to ensure
 // the C functions match the expectations of the DPI imports.
 
-#ifndef VERILATED_TB_1__DPI_H_
-#define VERILATED_TB_1__DPI_H_  // guard
+#ifndef VERILATED_TB_SPIKE_LINK__DPI_H_
+#define VERILATED_TB_SPIKE_LINK__DPI_H_  // guard
 
 #include "svdpi.h"
 
@@ -16,15 +16,15 @@ extern "C" {
 
 
     // DPI IMPORTS
-    // DPI import at src/rtl/tb/tb_1.sv:39:32
+    // DPI import at src/rtl/tb/tb_spike_link.sv:38:32
     extern void clear_last_commit();
-    // DPI import at src/rtl/tb/tb_1.sv:32:32
+    // DPI import at src/rtl/tb/tb_spike_link.sv:32:32
     extern void get_last_commit(const svOpenArrayHandle key_array, const svOpenArrayHandle value_array, int* num_elements_inserted);
-    // DPI import at src/rtl/tb/tb_1.sv:26:31
-    extern int init(int argc, const svOpenArrayHandle argv);
-    // DPI import at src/rtl/tb/tb_1.sv:29:32
+    // DPI import at src/rtl/tb/tb_spike_link.sv:26:31
+    extern int init();
+    // DPI import at src/rtl/tb/tb_spike_link.sv:30:32
     extern void step();
-    // DPI import at src/rtl/tb/tb_1.sv:19:32
+    // DPI import at src/rtl/tb/tb_spike_link.sv:19:32
     extern void write_unordered_map_to_sv_open_arrays(const svOpenArrayHandle key_array, const svOpenArrayHandle value_array, int* num_elements_inserted);
 
 #ifdef __cplusplus
