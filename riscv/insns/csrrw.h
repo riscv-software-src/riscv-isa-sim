3023a583273a95b65b5d1cfc0aaa88d08081c127
smt694 <smt694@andestech.com>
2024-03-19 10:59:01 +0800

    don't read CSR in csrrw(i) with rd=0
    fix write permission

--- riscv/insns/csrrw.h
+++ riscv/insns/csrrw.h
@@ -1,5 +1,8 @@
 int csr = validate_csr(insn.csr(), true);
-reg_t old = p->get_csr(csr, insn, true);
+reg_t old = (insn.rd() != 0) ? p->get_csr(csr, insn, true) : 0; // don't if rd = 0
+auto &carmap = p->get_state()->csrmap;
+// permission on write
+csrmap.count(csr) ? p->get_state()->csrmap[csr]->verify_permissions(insn, true) : throw trap_illegal_instruction(insn.bits());
 p->put_csr(csr, RS1);
 WRITE_RD(sext_xlen(old));
 serialize();
