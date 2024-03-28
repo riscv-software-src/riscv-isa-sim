50a4aff9b8f8afb46bcb304929df22237a9b18ca
YenHaoChen <howard25336284@gmail.com>
2024-03-28 16:14:00 +0800

    AIA: Raise virtual instruction exception on writing stimecmp (stimecmph) from VS-mode when hvictl.VTI=1

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -1734,6 +1734,9 @@ void stimecmp_csr_t::verify_permissions(insn_t insn, bool write) const {
   }
 
   basic_csr_t::verify_permissions(insn, write);
+
+  if ((state->csrmap[CSR_HVICTL]->read() & HVICTL_VTI) && state->v && write)
+    throw trap_virtual_instruction(insn.bits());
 }
 
 void virtualized_with_special_permission_csr_t::verify_permissions(insn_t insn, bool write) const {
