a466721991ec2bb3df7305202c96e13e8867e2c9
YenHaoChen <howard25336284@gmail.com>
2024-03-29 13:51:56 +0800

    AIA: Let mvien.SEIP be writable
    When mvien.SEIP=0, mip.SEIP includes the software-writable bit, i.e.,
    mvip.SEIP.
    
    When mvien.SEIP=1, mip.SEIP is read-only and does not include the value
    of mvip.SEIP.

--- riscv/csr_init.cc
+++ riscv/csr_init.cc
@@ -417,7 +417,7 @@ void state_t::csr_init(processor_t* const proc, reg_t max_isa)
   const reg_t srmcfg_mask = SRMCFG_MCID | SRMCFG_RCID;
   add_const_ext_csr(EXT_SSQOSID, CSR_SRMCFG, std::make_shared<srmcfg_csr_t>(proc, CSR_SRMCFG, srmcfg_mask, 0));
 
-  mvien = std::make_shared<masked_csr_t>(proc, CSR_MVIEN, MIP_SSIP, 0);
+  mvien = std::make_shared<masked_csr_t>(proc, CSR_MVIEN, MIP_SEIP | MIP_SSIP, 0);
   mvip = std::make_shared<mvip_csr_t>(proc, CSR_MVIP, 0);
   if (proc->extension_enabled_const(EXT_SMAIA)) {
     add_csr(CSR_MTOPI, std::make_shared<mtopi_csr_t>(proc, CSR_MTOPI));
