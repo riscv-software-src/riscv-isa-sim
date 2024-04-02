a4bd580215c9b53ed92590a6a62a0a9855148759
YenHaoChen <howard25336284@gmail.com>
2024-04-02 18:05:36 +0800

    AIA: Let sie[n] be writable when mideleg[n]=0 and mvien[n]=1
    The AIA spec specifies the sie[n] behavior conditionally:
    (1) When mideleg[n]=0 and mvien[n]=0, sie[n] is read-only 0.
    (2) When mideleg[n]=0 and mvien[n]=1, sie[n] is writable.
    (3) When mideleg[n]=1, sie[n] is an alias of mie[n].
    
    Points (1) and (3) describe the same behavior without AIA. This commit
    provides the behavior of point (2).

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -355,7 +355,7 @@ void state_t::reset(processor_t* const proc, reg_t max_isa)
   csrmap[CSR_HIP] = std::make_shared<mip_proxy_csr_t>(proc, CSR_HIP, hip_hie_accr);
   csrmap[CSR_HVIP] = hvip = std::make_shared<hvip_csr_t>(proc, CSR_HVIP, 0);
 
-  auto nonvirtual_sie = std::make_shared<mie_proxy_csr_t>(proc, CSR_SIE, sip_sie_accr);
+  auto nonvirtual_sie = std::make_shared<sie_csr_t>(proc, CSR_SIE, sip_sie_accr);
   auto vsie = std::make_shared<mie_proxy_csr_t>(proc, CSR_VSIE, vsip_vsie_accr);
   csrmap[CSR_VSIE] = vsie;
   csrmap[CSR_SIE] = std::make_shared<virtualized_csr_t>(proc, nonvirtual_sie, vsie);
