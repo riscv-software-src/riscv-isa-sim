b76760f6a2f9ac21f55873f4a53f8c71859f049d
rbuchner <ryan.buchner@arilinc.com>
2024-07-02 12:36:38 -0700

    Extract logic for emulating f8 function using f16 from f8_mul into f8_emulation module
    Will now be able to resuse this logic for other functions.

--- softfloat/f8_emulation.c
+++ softfloat/f8_emulation.c
@@ -40,7 +40,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "specialize.h"
 #include "softfloat.h"
 
-float8_t f8_mul( float8_t a, float8_t b )
-{
-  return f8_emulation_2_operands(a, b, f16_mul);
+float8_t f8_emulation_2_operands(float8_t a8, float8_t b8, float16_t (*operation)(float16_t, float16_t)) {
+  uint_fast8_t roundingMode = softfloat_roundingMode;
+  softfloat_roundingMode = softfloat_round_odd;
+  float16_t a16 = f8_to_f16(a8);
+  float16_t b16 = f8_to_f16(b8);
+  float16_t z16 = operation(a16, b16);
+  softfloat_roundingMode = roundingMode;
+  float8_t z = f16_to_f8(z16);
+  return z;
 }
--- softfloat/softfloat.h
+++ softfloat/softfloat.h
@@ -146,6 +146,8 @@ void i64_to_f128M( int64_t, float128_t * );
 float8_t f8_mul( float8_t, float8_t );
 float16_t f8_to_f16( float8_t );
 
+float8_t f8_emulation_2_operands(float8_t a8, float8_t b8, float16_t (*operation)(float16_t, float16_t));
+
 /*----------------------------------------------------------------------------
 | 16-bit (half-precision) floating-point operations.
 *----------------------------------------------------------------------------*/
