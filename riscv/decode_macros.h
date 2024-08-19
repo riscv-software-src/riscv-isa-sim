58f162881c1203aa20a193f89438754081ccf711
alex2kameboss <palexandru2000@gmail.com>
2024-08-19 10:18:51 +0300

    fix convolution

--- riscv/decode_macros.h
+++ riscv/decode_macros.h
@@ -84,16 +84,13 @@
 #define MA_CNV(SRD, SR1, SR2, DTYPE) ({ \
     DTYPE* a = (DTYPE*)SR1.data; \
     DTYPE* b = (DTYPE*)SR2.data; \
-    SRD.setSize(SR1.getWidth() - SR2.getWidth() + 1, \
-    SR1.getHeight() - SR2.getHeight() + 1, \
-    SRD.getDtype()); \
     DTYPE* c = (DTYPE*)SRD.data; \
     for ( int i = 0; i < SRD.getHeight(); ++i ) \
         for ( int j = 0; j < SRD.getWidth(); ++j ) { \
             c[i * SRD.width + j] = 0; \
             for ( int ii = 0; ii < SR2.getHeight(); ++ii ) \
                 for ( int jj = 0; jj < SR2.getWidth(); ++jj ) \
-                    c[i * SR1.width + j] += a[(i + ii) * SR1.width + (j + jj)] * b[ii * SR2.width + jj]; \
+                    c[i * SRD.width + j] += a[(i + ii) * SR1.width + (j + jj)] * b[ii * SR2.width + jj]; \
         } \
     })
 
