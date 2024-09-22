8686692a3d6bc007dd3621fea616a4d041f87812
FluxByt <FluxByt>
2024-09-22 01:47:29 -0700

    Fix for CSRRWI writing to CSR when uimm = 0.

--- riscv/insns/csrrwi.h
+++ riscv/insns/csrrwi.h
@@ -1,5 +1,8 @@
+bool write = insn.rs1() != 0;
 int csr = validate_csr(insn.csr(), true);
 reg_t old = p->get_csr(csr, insn, true);
-p->put_csr(csr, insn.rs1());
+if (write) {
+  p->put_csr(csr, insn.rs1());
+}
 WRITE_RD(sext_xlen(old));
 serialize();
