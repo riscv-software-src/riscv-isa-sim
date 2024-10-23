b21bbdd8c7ff41ff94b3c5443a6d404cbbdfcfa3
ahmedshakill <shakil.000024@gmail.com>
2024-10-23 16:15:21 +0600

    rename to core

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -605,7 +605,7 @@ void processor_t::disasm(insn_t insn)
 
     unsigned max_xlen = isa.get_max_xlen();
 
-    s << "mycore " << std::dec << std::setfill(' ') << std::setw(3) << id
+    s << "core " << std::dec << std::setfill(' ') << std::setw(3) << id
       << std::hex << ": 0x" << std::setfill('0') << std::setw(max_xlen / 4)
       << zext(state.pc, max_xlen) << " (0x" << std::setw(8) << bits << ") "
       << disassembler->disassemble(insn) << std::endl;
