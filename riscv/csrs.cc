riscv-software-src/riscv-isa-sim

commit ec292be4fdac9685c9d6f2f209b73743c2e573f7

Update encoding.h for pointer masking
Rename DCSR_STOPCYCLE to DCSR_STOPCOUNT
Rename DCSR_HALT to DCSR_NMIP

riscv/csrs.cc
@@ -1312,7 +1312,7 @@ reg_t dcsr_csr_t::read() const noexcept {
   result = set_field(result, DCSR_EBREAKU, ebreaku);
   result = set_field(result, CSR_DCSR_EBREAKVS, ebreakvs);
   result = set_field(result, CSR_DCSR_EBREAKVU, ebreakvu);
-  result = set_field(result, DCSR_STOPCYCLE, 0);
+  result = set_field(result, DCSR_STOPCOUNT, 0);
   result = set_field(result, DCSR_STOPTIME, 0);
   result = set_field(result, DCSR_CAUSE, cause);
   result = set_field(result, DCSR_STEP, step);
@@ -1331,7 +1331,7 @@ bool dcsr_csr_t::unlogged_write(const reg_t val) noexcept {
   ebreaku = proc->extension_enabled('U') ? get_field(val, DCSR_EBREAKU) : false;
   ebreakvs = proc->extension_enabled('H') ? get_field(val, CSR_DCSR_EBREAKVS) : false;
   ebreakvu = proc->extension_enabled('H') ? get_field(val, CSR_DCSR_EBREAKVU) : false;
-  halt = get_field(val, DCSR_HALT);
+  halt = get_field(val, DCSR_NMIP);
   v = proc->extension_enabled('H') ? get_field(val, CSR_DCSR_V) : false;
   pelp = proc->extension_enabled(EXT_ZICFILP) ?
          static_cast<elp_t>(get_field(val, DCSR_PELP)) : elp_t::NO_LP_EXPECTED;

commit 0fea35ddfba2d4be1912da16fc273ddc2000cd86

Set dcsr.xdebugver to 4, as it ought to be
See discusson on #1878.  This restores the behavior prior to
ec292be4fdac9685c9d6f2f209b73743c2e573f7, which inadvertently changed
the value to 1.

Resolves #1878

riscv/csrs.cc
@@ -1359,7 +1359,7 @@ void dcsr_csr_t::verify_permissions(insn_t insn, bool write) const {
 
 reg_t dcsr_csr_t::read() const noexcept {
   reg_t result = 0;
-  result = set_field(result, DCSR_XDEBUGVER, 1);
+  result = set_field(result, DCSR_XDEBUGVER, 4);
   result = set_field(result, DCSR_EBREAKM, ebreakm);
   result = set_field(result, DCSR_EBREAKS, ebreaks);
   result = set_field(result, DCSR_EBREAKU, ebreaku);