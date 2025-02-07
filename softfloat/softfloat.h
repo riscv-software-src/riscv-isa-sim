aedb49c40a7eaf1b5f275917278dcc89d3d85a20
rbuchner <ryan.buchner@arilinc.com>
2025-02-06 16:39:15 -0800

    Implement f8_add/f8_sub in softfloat

--- softfloat/softfloat.h
+++ softfloat/softfloat.h
@@ -149,6 +149,8 @@ float8_t f8_rsqrte7( float8_t );
 float8_t f8_recip7( float8_t );
 float8_t f8_sqrt( float8_t );
 float16_t f8_to_f16( float8_t );
+float8_t f8_sub( float8_t, float8_t );
+float8_t f8_add( float8_t, float8_t );
 
 float8_t f8_emulation_2_operands(float8_t a8, float8_t b8, float16_t (*operation)(float16_t, float16_t));
 float8_t f8_emulation_1_operand(float8_t a8, float16_t (*operation)(float16_t));
