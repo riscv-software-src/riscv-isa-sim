a80db6cb804cb4eb9f70a6c6fec7b39518b9b1d5
Muhammad Moiz Hussain <muhammad.moiz.hussain@semidynamics.com>
2025-04-14 12:05:50 +0200

    Fix double checking for EXT_SMCSRIND in isa_parser.cc
    Signed-off-by: muhammad.moiz.hussain@semidynamics.com

--- disasm/isa_parser.cc
+++ disasm/isa_parser.cc
@@ -324,8 +324,6 @@ isa_parser_t::isa_parser_t(const char* str, const char *priv)
         extension_table[EXT_SSTC] = true;
     } else if (ext_str == "smcsrind") {
       extension_table[EXT_SMCSRIND] = true;
-    } else if (ext_str == "sscsrind") {
-      extension_table[EXT_SSCSRIND] = true;
     } else if (ext_str == "smcntrpmf") {
       extension_table[EXT_SMCNTRPMF] = true;
     } else if (ext_str == "zimop") {
