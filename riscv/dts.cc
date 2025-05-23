02067d39a8e7b6ba6f9187808003392da3c9302c
Ian Huang <ihuang@rivosinc.com>
2025-05-23 07:00:52 -0600

    AIA: add IMSIC dts parser

--- riscv/dts.cc
+++ riscv/dts.cc
@@ -441,3 +441,32 @@ int fdt_parse_hartid(const void *fdt, int cpu_offset, uint32_t *hartid)
 
   return 0;
 }
+
+int fdt_parse_imsics(const void *fdt, reg_t *imsic_m_addr, reg_t *imsic_s_addr, const char *compatible)
+{
+  int nodeoffset = -1, len, rc;
+  const fdt32_t *p;
+  reg_t addr = 0;
+  int val;
+
+  for (int i = 0; i < 2; i++) {
+    nodeoffset = fdt_node_offset_by_compatible(fdt, nodeoffset, compatible);
+    if (nodeoffset < 0)
+      return nodeoffset;
+
+    rc = fdt_get_node_addr_size(fdt, nodeoffset, &addr, NULL, "reg");
+    if (rc < 0)
+      return -ENODEV;
+
+    p = (fdt32_t *)fdt_getprop(fdt, nodeoffset, "interrupts-extended", &len);
+    if (!p)
+      return -ENODEV;
+
+    val = fdt32_to_cpu(p[1]);
+    if (val == IRQ_M_EXT && imsic_m_addr)
+      *imsic_m_addr = addr;
+    if (val == IRQ_S_EXT && imsic_s_addr)
+      *imsic_s_addr = addr;
+  }
+  return 0;
+}
