b6f4d246bc36a539751ee5840395d423075ba7f2
Ian Huang <ihuang@rivosinc.com>
2025-06-25 08:07:16 -0700

    AIA: vstopi should only be used for AIA-extended interrupts
    VSEIP/VSTIP/VSSIP should come in through the non-SSAIA path.
    vstopi will be used to throw VTI and IID > 13 interrupts.

--- riscv/processor.h
+++ riscv/processor.h
@@ -415,7 +415,6 @@ private:
   unsigned ziccid_flush_count = 0;
   static const unsigned ZICCID_FLUSH_PERIOD = 10;
 
-  bool is_handled_in_vs();
   void take_pending_interrupt() { take_interrupt(state.mip->read() & state.mie->read()); }
   void take_interrupt(reg_t mask); // take first enabled interrupt in mask
   void take_trap(trap_t& t, reg_t epc); // take an exception
