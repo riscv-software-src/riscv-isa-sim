37a14f854fde6250b0d0557029f4f608d269a965
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2025-08-05 22:14:30 -0700

    second batch of V bf16 fixes

--- riscv/insns/vmflt_vf.h
+++ riscv/insns/vmflt_vf.h
@@ -1,7 +1,8 @@
 // vmflt.vf vd, vs2, rs1
 VI_VFP_VF_LOOP_CMP
 ({
-  res = f16_lt(vs2, rs1);
+  rs1 = P.VU.altfmt() ? READ_FREG_BF(rs1_num) : rs1;
+  res = P.VU.altfmt() ? bf16_lt(vs2, rs1) : f16_lt(vs2, rs1);
 },
 {
   res = f32_lt(vs2, rs1);
