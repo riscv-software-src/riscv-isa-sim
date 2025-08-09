07ffa0781fa6b684bdf1294065fd7ee6b89304fd
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2025-08-08 23:36:49 -0700

    one more place to register zvfofp4min, zvfofp8min

--- disasm/isa_parser.cc
+++ disasm/isa_parser.cc
@@ -134,6 +134,10 @@ isa_parser_t::isa_parser_t(const char* str, const char *priv)
         if (!has_any_vector() || !extension_table[EXT_ZFBFMIN] || !get_zvf())
           bad_isa_string(str, "'zvfbfa' extension requires at least 'Zve32f', and 'Zfbfmin'");
         extension_table[EXT_ZVFBFA] = true;
+    } else if (ext_str == "zvfofp4min") {
+      extension_table[EXT_ZVFOFP4MIN] = true;
+    } else if (ext_str == "zvfofp8min") {
+      extension_table[EXT_ZVFOFP8MIN] = true;
     } else if (ext_str == "zicsr") {
       // Spike necessarily has Zicsr, because
       // Zicsr is implied by the privileged architecture
