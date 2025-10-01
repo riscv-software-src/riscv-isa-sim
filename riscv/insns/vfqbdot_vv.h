ce4d2de6940ed5297363a23de6996091295372c8
Andrew Waterman <andrew@sifive.com>
2025-09-30 19:13:10 -0700

    Implement Zvfbqdot8f

---
+++ riscv/insns/vfqbdot_vv.h
@@ -0,0 +1,17 @@
+VI_VFP_BASE;
+ZVBDOT_INIT(4);
+
+#define COMMA ,
+
+switch (P.VU.vsew) {
+  case 8: {
+    require_extension(EXT_ZVFQBDOT8F);
+    if (P.VU.altfmt) {
+      ZVBDOT_LOOP(uint8_t, uint8_t, float32_t, zvfqbdot8f_dot_acc<ofp8_e5m2 COMMA ofp8_e4m3>);
+    } else {
+      ZVBDOT_LOOP(uint8_t, uint8_t, float32_t, zvfqbdot8f_dot_acc<ofp8_e4m3 COMMA ofp8_e4m3>);
+    }
+    break;
+  }
+  default: require(false);
+}
