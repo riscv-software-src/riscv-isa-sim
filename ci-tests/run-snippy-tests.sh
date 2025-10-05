dac31c1799274efa25db576cb1bc96fb4d9aa24a
Alexander Romanov <alexander.romanov@syntacore.com>
2025-10-05 14:35:50 +0300

    test: add basic random tests for V extension

--- ci-tests/run-snippy-tests.sh
+++ ci-tests/run-snippy-tests.sh
@@ -8,7 +8,7 @@ SPIKE_PATH="$2"
 error=0
 for testfile in "$TESTDIR"/*; do
   bitness=$(basename "$testfile" .elf | grep -o "[0-9]\+")
-  if ! timeout --foreground 5s "$SPIKE_PATH" -l --log-commits --isa rv"$bitness"ifdc_zicsr "$testfile"
+  if ! timeout --foreground 5s "$SPIKE_PATH" -l --log-commits --isa rv"$bitness"ifdcv_zicsr "$testfile"
   then
     echo "TIMEOUT: $testfile"
     error=1
---
+++ ci-tests/snippy-tests/boot-code-vf.s
@@ -0,0 +1,45 @@
+.option norvc
+
+.global _entry
+.global fromhost
+.global tohost
+
+.text
+_entry:
+  la t0, exception_handler
+  csrw mtvec, t0
+  csrr t1, mstatus
+# Setting bit number 9 (mstatus.VS)
+  li t2, 1
+  slli t2, t2, 9
+  or t1, t1, t2
+# Setting bit number 13 (mstatus.FS)
+  li t3, 1
+  slli t3, t3, 13
+  or t1, t1, t3
+  csrw mstatus, t1
+  la t0, SNIPPY_ENTRY
+  jalr t0
+
+exception_handler:
+  csrr x10, mcause
+# In case of breakpoint (Interrupt = 0, Exception code = 3) we finalize.
+# Otherwise it's not the expected behavior and we go into an infinite loop.
+  li x11, 3
+  beq x10, x11, exit
+  j infinite_loop
+
+exit:
+  li ra, 1
+  la sp, tohost
+  sd ra, 0(sp)
+
+infinite_loop:
+  j infinite_loop
+
+.balign 64
+tohost:
+.8byte 0x0
+.balign 64
+fromhost:
+.8byte 0x0
