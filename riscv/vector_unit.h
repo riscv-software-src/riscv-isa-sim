d018cabce3d71d6050ff93333281c3df69540b2c
Alexander Romanov <alexander.romanov@syntacore.com>
2025-10-14 15:14:50 +0300

    fix: move vectorUnit_t::elt defintion to the header
    Prior to this commit vectorUnit_t::elt was a bottleneck for vector
    instructions performance. This function was not being inlined because
    the definition was inside a .cc file. After moving the definition to the
    header file I measured the 20-30% increase in performance on random
    vector test from CI.

--- riscv/vector_unit.h
+++ riscv/vector_unit.h
@@ -103,10 +103,71 @@ public:
   bool vstart_alu = false;
 
   // vector element for various SEW
-  template<class T> T& elt(reg_t vReg, reg_t n, bool is_write = false);
+  template<typename T> T& elt(reg_t vReg, reg_t n, bool is_write = false) {
+    assert(vsew != 0);
+    assert((VLEN >> 3)/sizeof(T) > 0);
+    reg_t elts_per_reg = (VLEN >> 3) / (sizeof(T));
+    vReg += n / elts_per_reg;
+    n = n % elts_per_reg;
+#ifdef WORDS_BIGENDIAN
+    // "V" spec 0.7.1 requires lower indices to map to lower significant
+    // bits when changing SEW, thus we need to index from the end on BE.
+    n ^= elts_per_reg - 1;
+#endif
+    if (is_write)
+      log_elt_write_if_needed(vReg);
+
+    T *regStart = (T*)((char*)reg_file + vReg * (VLEN >> 3));
+    return regStart[n];
+  }
+
   // vector element group access, where EG is a std::array<T, N>.
+  // The logic differences between 'elt()' and 'elt_group()' come from
+  // the fact that, while 'elt()' requires that the element is fully
+  // contained in a single vector register, the element group may span
+  // multiple registers in a single register group (LMUL>1).
+  //
+  // Notes:
+  // - We do NOT check that a single element - i.e., the T in the element
+  //   group type std::array<T, N> - fits within a single register, or that
+  //   T is smaller or equal to VSEW. Implementations of the instructions
+  //   sometimes use a different T than what the specification suggests.
+  //   Instructon implementations should 'require()' what the specification
+  //   dictates.
+  // - We do NOT check that 'vReg' is a valid register group, or that
+  //   'n+1' element groups fit in the register group 'vReg'. It is
+  //   the responsibility of the caller to validate those preconditions.
+
   template<typename EG> EG&
-  elt_group(reg_t vReg, reg_t n, bool is_write = false);
+  elt_group(reg_t vReg, reg_t n, bool is_write = false) {
+#ifdef WORDS_BIGENDIAN
+    fputs("vectorUnit_t::elt_group is not compatible with WORDS_BIGENDIAN setup.\n",
+            stderr);
+    abort();
+#endif
+    using T = typename EG::value_type;
+    constexpr std::size_t N = std::tuple_size<EG>::value;
+    assert(N > 0);
+
+    assert(vsew != 0);
+    constexpr reg_t elt_group_size = N * sizeof(T);
+    const reg_t reg_group_size = (VLEN >> 3) * vflmul;
+    assert(((n + 1) * elt_group_size) <= reg_group_size);
+
+    const reg_t start_byte = n * elt_group_size;
+    const reg_t bytes_per_reg = VLEN >> 3;
+
+    // Inclusive first/last register indices.
+    const reg_t reg_first = vReg + start_byte / bytes_per_reg;
+    const reg_t reg_last = vReg + (start_byte + elt_group_size - 1) / bytes_per_reg;
+
+    // Element groups per register groups
+    for (reg_t vidx = reg_first; vidx <= reg_last; ++vidx)
+      if (is_write)
+        log_elt_write_if_needed(vidx);
+
+    return *(EG*)((char*)reg_file + vReg * (VLEN >> 3) + start_byte);
+  }
 
   bool mask_elt(reg_t vReg, reg_t n)
   {
@@ -118,6 +179,10 @@ public:
     auto& e = elt<uint8_t>(vReg, n / 8, true);
     e = (e & ~(1U << (n % 8))) | (value << (n % 8));
   }
+
+private:
+
+  void log_elt_write_if_needed(reg_t vReg) const;
 
 public:
 
