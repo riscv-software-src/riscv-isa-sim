f74f53af4ac0985f0be6010fcf113ef7419f3195
Chih-Min Chao <chihmin.chao@sifive.com>
2025-10-21 01:43:49 -0700

    zve: correct the requirement for convension
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/vfncvt_f_x_w.h
+++ riscv/insns/vfncvt_f_x_w.h
@@ -2,9 +2,9 @@
 VI_NON_ALTFMT_INSN
 
 VI_VFP_NCVT_INT_TO_FP(
-  { vd = i32_to_f16(vs2); },        // BODY32
-  { vd = i64_to_f32(vs2); },        // BODY64
-  { require_extension(EXT_ZVFH); }, // CHECK32
-  { require_extension('F'); },      // CHECK64
-  int                               // sign
+  { vd = i32_to_f16(vs2); },             // BODY32
+  { vd = i64_to_f32(vs2); },             // BODY64
+  { require_extension(EXT_ZVFH); },      // CHECK32
+  { require(p->get_isa().get_zvf()); },  // CHECK64
+  int                                    // sign
 )
--- riscv/insns/vfncvt_f_xu_w.h
+++ riscv/insns/vfncvt_f_xu_w.h
@@ -2,9 +2,9 @@
 VI_NON_ALTFMT_INSN
 
 VI_VFP_NCVT_INT_TO_FP(
-  { vd = ui32_to_f16(vs2); },       // BODY32
-  { vd = ui64_to_f32(vs2); },       // BODY64
-  { require_extension(EXT_ZVFH); }, // CHECK32
-  { require_extension('F'); },      // CHECK64
-  uint                              // sign
+  { vd = ui32_to_f16(vs2); },            // BODY32
+  { vd = ui64_to_f32(vs2); },            // BODY64
+  { require_extension(EXT_ZVFH); },      // CHECK32
+  { require(p->get_isa().get_zvf()); },  // CHECK64
+  uint                                   // sign
 )
