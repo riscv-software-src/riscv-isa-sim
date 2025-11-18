d545f1336aa839d8237c745ebb9f27a25c466c58
Andrew Waterman <andrew@sifive.com>
2025-11-18 02:08:44 -0800

    Remove unnecessary illegal-instruction checks
    All of these are cases where other instructions overlay the reserved
    encodings (e.g. C.EBREAK is C.JALR with rs1=x0), so the checks are
    redundant.

--- riscv/insns/c_add.h
+++ riscv/insns/c_add.h
@@ -1,3 +1,2 @@
 require_extension(EXT_ZCA);
-require(insn.rvc_rs2() != 0);
 WRITE_RD(sext_xlen(RVC_RS1 + RVC_RS2));
--- riscv/insns/c_mv.h
+++ riscv/insns/c_mv.h
@@ -1,3 +1,2 @@
 require_extension(EXT_ZCA);
-require(insn.rvc_rs2() != 0);
 WRITE_RD(RVC_RS2);
