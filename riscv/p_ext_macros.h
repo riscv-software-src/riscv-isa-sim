3a634eecec433246ab3510332ea8b811cbdf7097
Dklt1117 <remoklt996@gmail.com>
2025-12-23 02:00:34 +0800

    rvp: add packed accumulation instructions

--- riscv/p_ext_macros.h
+++ riscv/p_ext_macros.h
@@ -25,6 +25,9 @@
 #define P_RS1_UPARAMS(BIT) \
   auto p_rs1 = P_UFIELD(rs1, i, BIT);
 
+#define P_RS1_INNER_UPARAMS(BIT_INNER) \
+  auto p_rs1 = P_UFIELD(rs1, j, BIT_INNER);
+
 #define P_RS2_PARAMS(BIT) \
   auto p_rs2 = P_FIELD(rs2, i, BIT);  
 
@@ -36,6 +39,9 @@
  
 #define P_RS2_CROSS_UPARAMS(BIT) \
   auto p_rs2 = P_UFIELD(rs2, (i ^ 1), BIT);
+
+#define P_RS2_INNER_UPARAMS(BIT_INNER) \
+  auto p_rs2 = P_UFIELD(rs2, j, BIT_INNER);
 
 // Loop base
 #define P_RD_LOOP_BASE(BIT) \
@@ -61,6 +67,25 @@
   reg_t rs2 = RS2; \
   sreg_t len = xlen / (BIT); \
   for (sreg_t i = len - 1; i >= 0; --i) {
+
+#define P_RS1_LOOP_BASE(BIT) \
+  require_extension('P'); \
+  require((BIT) == e8 || (BIT) == e16 || (BIT) == e32); \
+  reg_t rs1 = RS1; \
+  sreg_t len = xlen / (BIT); \
+  for (sreg_t i = len - 1; i >= 0; --i) {
+
+#define P_REDUCTION_ULOOP_BASE(BIT, BIT_INNER, USE_RD) \
+  require_extension('P'); \
+  require(BIT == e16 || BIT == e32 || BIT == e64); \
+  reg_t rd_tmp = USE_RD ? zext_xlen(RD) : 0; \
+  reg_t rs1 = zext_xlen(RS1); \
+  reg_t rs2 = zext_xlen(RS2); \
+  sreg_t len = 64 / BIT; \
+  sreg_t len_inner = BIT / BIT_INNER; \
+  for (sreg_t i = len - 1; i >= 0; --i) { \
+    sreg_t p_res = P_UFIELD(rd_tmp, i, BIT); \
+    for (sreg_t j = i * len_inner; j < (i + 1) * len_inner; ++j) {
 
 // Loop body
 #define P_RD_LOOP_BODY(BIT, BODY) { \
@@ -113,6 +138,16 @@
   } \
   WRITE_RD(sext_xlen(rd_tmp));
 
+#define P_REDUCTION_ULOOP_END(BIT, IS_SAT) \
+    } \
+    if (IS_SAT) { \
+      P_USAT(BIT, p_res); \
+    } \
+    type_usew_t<BIT>::type p_rd = p_res; \
+    WRITE_P_RD(); \
+  } \
+  WRITE_RD(sext_xlen(rd_tmp));
+
 // Loop
 #define P_RD_LOOP(BIT_RD, BODY) \
   P_RD_LOOP_BASE(BIT_RD) \
@@ -158,11 +193,25 @@
   } \
   P_RD_LOOP_END()
 
+#define P_REDUCTION_ULOOP(BIT, BIT_INNER, USE_RD, IS_SAT, BODY) \
+  P_REDUCTION_ULOOP_BASE(BIT, BIT_INNER, USE_RD) \
+  P_RS1_INNER_UPARAMS(BIT_INNER) \
+  P_RS2_INNER_UPARAMS(BIT_INNER) \
+  BODY \
+  P_REDUCTION_ULOOP_END(BIT, IS_SAT)
+
 // Misc
 #define P_SAT(BIT, R) ( \
   ((BIT) == 64) ? (R) : \
   ((R) > ((1LL << ((BIT) - 1)) - 1)) ? ((1LL << ((BIT) - 1)) - 1) : \
   ((R) < -(1LL << ((BIT) - 1))) ? -(1LL << ((BIT) - 1)) : \
+  (R) \
+)
+
+#define P_USAT(BIT, R) ( \
+  ((R) < 0) ? 0 : \
+  ((BIT) == 64) ? (R) : \
+  ((R) > ((1LL << ((BIT) - 1)) - 1)) ? ((1LL << ((BIT) - 1)) - 1) : \
   (R) \
 )
 
