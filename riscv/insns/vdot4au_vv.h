a72edf6caa0c56f6dab89298ff3e853e73d92ecb
Nicolas Brunie <nibrunie@gmail.com>
2026-01-18 09:17:23 -0800

    [Zvdot4a] Renaming Zvqdotq to Zvdot4a and vdotq to vdot4a
    Folowing Architecture Review Committee's (ARC) recommendation
    https://riscv.atlassian.net/browse/RVS-1971?focusedCommentId=21112
    shared during the review of Zvqdotq, the extension and its instructions
    are renamed.
    
    Changelogs:
    * Renaming extension Zvqdotq to Zvdot4a8i
    * Renaming instructions vqdot* to vdot4a*
    * Importing new encodings from https://github.com/riscv/riscv-opcodes/pull/402

---
+++ riscv/insns/vdot4au_vv.h
@@ -0,0 +1,10 @@
+// vdot4au.vv vd, vs2, vs1, vm
+#include "vdot4a_common.h"
+
+require_extension(EXT_ZVDOT4A8I);
+require(P.VU.vsew == e32);
+
+VI_VV_LOOP({
+  VDOT4A(vs1, vs2, uint8_t, uint8_t);
+  vd = (vd + result) & 0xffffffff;
+})
