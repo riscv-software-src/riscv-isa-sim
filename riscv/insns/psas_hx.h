9205a7674c9ab96e5265a4850bdc388b2cc0c07b
Jason <jason01180118@gmail.com>
2026-01-28 07:11:29 -0800

    rvp: add packed cross instructions

---
+++ riscv/insns/psas_hx.h
@@ -0,0 +1,7 @@
+P_CROSS_ULOOP(16, {
+  bool sat = false;
+  p_rd = (sat_add<int16_t, uint16_t>(p_rs1, p_rs2, sat));
+}, {
+  bool sat = false;
+  p_rd = (sat_sub<int16_t, uint16_t>(p_rs1, p_rs2, sat));
+})
