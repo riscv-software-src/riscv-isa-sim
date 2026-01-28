b5c5dc0472a0991cedafd3482b805a8ac0a8c8b8
Jason <jason01180118@gmail.com>
2026-01-28 07:11:33 -0800

    rvp: add packed narrowing shift and clip register-pair

---
+++ riscv/insns/pnclipu_hs.h
@@ -0,0 +1,4 @@
+require_rv32;
+P_NARROW_RD_RS1_ULOOP(16, 32, {
+    p_rd = P_USAT(16, (sreg_t)(p_rs1 >> (P_UFIELD(RS2, 0, 16) & 0X1F)));
+})
---
+++ riscv/insns/pnsrar_bs.h
@@ -0,0 +1,12 @@
+require_rv32;
+P_NARROW_RD_RS1_LOOP(8, 16, {
+    uint32_t shamt = P_FIELD(RS2, 0, 8) & 0xF;
+    if (shamt != 0) {
+        sreg_t val = (sreg_t)p_rs1;
+        sreg_t shifted = val >> shamt;
+        sreg_t roundbit = (val >> (shamt - 1)) & 1;
+        p_rd = (shifted + roundbit) & 0xFF;
+    } else {
+        p_rd = p_rs1 & 0xFF;
+    }
+})
