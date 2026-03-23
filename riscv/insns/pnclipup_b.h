a22585fcf09f3493252191e850ae0b09a403579a
Chih-Min Chao <chihmin.chao@sifive.com>
2026-03-23 02:22:37 -0700

    rvp: new instructions from 0.20
    The complete set of instructions added for RV32 are:
      PSSHL.HS SSHL PSSHL.DHS PSSHL.DWS
      PSSHLR.HS SSHLR PSSHLR.DHS PSSHLR.DWS
    And for RV64:
      PSSHL.HS PSSHL.WS SHL PNCLIPP.B PNCLIPP.H PNCLIPP.W
      PSSHLR.HS PSSHLR.WS SHLR PNCLIPUP.B PNCLIPUP.H PNCLIPUP.W
    
    reference:
      https://www.jhauser.us/RISCV/ext-P/RVP-instrEncodings-020.pdf
    https://www.jhauser.us/RISCV/ext-P/RVP-baseInstrs-Sail-020.txt
    
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

---
+++ riscv/insns/pnclipup_b.h
@@ -0,0 +1,13 @@
+require_extension('P');
+require_rv64;
+reg_t rd_tmp = 0;
+reg_t s_low = RS1;
+reg_t s_high = RS2;
+for (int i = 0; i < 8; i++) {
+  reg_t s_h = (i < 4) ? P_UFIELD(s_low, i, 16) : P_UFIELD(s_high, i - 4, 16);
+  reg_t sat_val = P_USAT(8, (sreg_t)s_h);
+  if (sat_val != s_h) P.VU.vxsat->write(1);
+  rd_tmp = set_field(rd_tmp, make_mask64(i * 8, 8), (uint8_t)sat_val);
+}
+WRITE_RD(rd_tmp);
+
---
+++ riscv/insns/pnclipup_w.h
@@ -0,0 +1,13 @@
+require_extension('P');
+require_rv64;
+uint64_t s1 = RS1;
+uint64_t s2 = RS2;
+
+reg_t sat_w0 = P_USAT(32, (sreg_t)s1);
+if (sat_w0 != s1) P.VU.vxsat->write(1);
+
+reg_t sat_w1 = P_USAT(32, (sreg_t)s2);
+if (sat_w1 != s2) P.VU.vxsat->write(1);
+
+WRITE_RD(((uint64_t)(uint32_t)sat_w1 << 32) | (uint32_t)sat_w0);
+
