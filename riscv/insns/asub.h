7d33bb52441af578d3bcaf83aac94e5e5094b81e
Chih-Min Chao <chihmin.chao@sifive.com>
2026-05-19 23:56:18 -0700

    rvp: do sext32 before writing to rd for rv32
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/asub.h
+++ riscv/insns/asub.h
@@ -1,3 +1,3 @@
 require_extension('P');
 require_rv32;
-WRITE_RD((RS1 - RS2)>>1);
+WRITE_RD(sext32((RS1 - RS2)>>1));
--- riscv/insns/nsrli.h
+++ riscv/insns/nsrli.h
@@ -1,3 +1,3 @@
 require_extension('P');
 require_rv32;
-WRITE_RD(P_RS1_PAIR >> insn.shamtd());
+WRITE_RD(sext32(P_RS1_PAIR >> insn.shamtd()));
