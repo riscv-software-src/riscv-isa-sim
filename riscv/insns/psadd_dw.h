12b6d86f3a7221d06fc5a6ee4b3c93f95f98afef
Chih-Min Chao <chihmin.chao@sifive.com>
2026-05-27 07:59:33 -0700

    rvp: add missing vxsat updating
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>

--- riscv/insns/psadd_dw.h
+++ riscv/insns/psadd_dw.h
@@ -2,4 +2,6 @@ require_rv32;
 P_RD_RS1_RS2_DW_LOOP(32,32,32, {
   bool sat = false;
   p_rd = (sat_add<int32_t, uint32_t>(p_rs1, p_rs2, sat));
+  if (sat)
+    P.set_vxsat();
 })
--- riscv/insns/saddu.h
+++ riscv/insns/saddu.h
@@ -4,4 +4,5 @@ bool sat = false;
 reg_t tmp = sat_addu<uint32_t>(RS1, RS2, sat);
 if (sat)
   P.set_vxsat();
+ 
 WRITE_RD(sext32(tmp));
