1e4347edcb1f87ea1f82e866cc0d12ad4ef7c529
Andrew Waterman <andrew@sifive.com>
2026-09-17 11:11:43 -0700

    Fix how cbo.inval determines whether to flush
    The comparison with CBIE should be against 1, not against any nonzero value.
    
    Signed-off-by: Andrew Waterman <andrew@sifive.com>

--- riscv/insns/cbo_inval.h
+++ riscv/insns/cbo_inval.h
@@ -3,9 +3,9 @@ require_envcfg(CBIE);
 
 DECLARE_XENVCFG_VARS(CBIE);
 
-if ((STATE.prv != PRV_M && mCBIE) ||
-    (!STATE.v && STATE.prv == PRV_U && p->extension_enabled('S') && sCBIE) ||
-    (STATE.v && (hCBIE || (STATE.prv == PRV_U && sCBIE))))
+if ((STATE.prv != PRV_M && mCBIE == 1) ||
+    (!STATE.v && STATE.prv == PRV_U && p->extension_enabled('S') && sCBIE == 1) ||
+    (STATE.v && (hCBIE == 1 || (STATE.prv == PRV_U && sCBIE == 1))))
   MMU.clean_inval(RS1, true, true);
 else
   MMU.clean_inval(RS1, false, true);
