riscv-software-src/riscv-isa-sim

commit 1494468abe7a26efa3ada422fef7f203b41a753a

Report triggers on CBOs using effective address, not block address

riscv/mmu.h
@@ -228,7 +228,7 @@ public:
     reg_t transformed_addr = access_info.transformed_vaddr;
 
     auto base = transformed_addr & ~(blocksz - 1);
-    check_triggers(triggers::OPERATION_STORE, base, false, blocksz);
+    check_triggers(triggers::OPERATION_STORE, transformed_addr, false, blocksz);
     for (size_t offset = 0; offset < blocksz; offset += 1)
       store<uint8_t>(base + offset, 0);
   }
@@ -237,8 +237,7 @@ public:
     auto access_info = generate_access_info(addr, LOAD, {.clean_inval = true});
     reg_t transformed_addr = access_info.transformed_vaddr;
 
-    auto base = transformed_addr & ~(blocksz - 1);
-    check_triggers(triggers::OPERATION_STORE, base, false, blocksz);
+    check_triggers(triggers::OPERATION_STORE, transformed_addr, false, blocksz);
     convert_load_traps_to_store_traps({
       const reg_t paddr = translate(access_info, 1);
       if (sim->reservable(paddr)) {

commit 58327ba3cfa1a919ef38ad4e6dc07e7ee1fbcb17

CBO operations should use aligned base for trigger comparisons
this fixes regression introduced in 1494468abe7a

riscv/mmu.cc
@@ -193,7 +193,8 @@ void mmu_t::check_triggers(triggers::operation_t operation,
   check_triggers(operation, addr, virt, access_len, std::nullopt);
 }
 
-void mmu_t::check_triggers(triggers::operation_t operation, reg_t address, bool virt, std::size_t size, std::optional<reg_t> data)
+void mmu_t::check_triggers(triggers::operation_t operation, reg_t address, bool virt,
+  std::size_t size, std::optional<reg_t> data, std::optional<reg_t> tval)
 {
   if (matched_trigger || !proc)
     return;
@@ -202,16 +203,19 @@ void mmu_t::check_triggers(triggers::operation_t operation, reg_t address, bool 
   if (!match.has_value())
     return;
 
+  // CBOs compare the aligned block but report the effective address on a trap.
+  const reg_t trap_address = tval.value_or(address);
+
   switch (match->timing) {
     case triggers::TIMING_BEFORE:
-      throw triggers::matched_t(operation, address, match->action, virt);
+      throw triggers::matched_t(operation, trap_address, match->action, virt);
 
     case triggers::TIMING_AFTER:
       // We want to take this exception on the next instruction.  We check
       // whether to do so outside of the main simulation loop, which we can
       // force by flushing the I$.
       flush_icache();
-      matched_trigger = triggers::matched_t(operation, address, match->action, virt);
+      matched_trigger = triggers::matched_t(operation, trap_address, match->action, virt);
   }
 }
 

riscv/mmu.h
@@ -238,7 +238,8 @@ public:
     auto access_info = generate_access_info(addr, STORE, {});
     reg_t transformed_addr = access_info.transformed_vaddr;
 
-    check_triggers(triggers::OPERATION_STORE, transformed_addr, false, blocksz);
+    check_triggers(triggers::OPERATION_STORE, transformed_addr & ~(blocksz - 1),
+      /*virt*/ false, blocksz, /*data*/ std::nullopt, transformed_addr);
 
     reg_t paddr = translate(access_info, 1) - (transformed_addr & (blocksz - 1));
     if (auto host_addr = sim->addr_to_mem(paddr)) {
@@ -254,7 +255,8 @@ public:
     auto access_info = generate_access_info(addr, LOAD, {.clean_inval = true});
     reg_t transformed_addr = access_info.transformed_vaddr;
 
-    check_triggers(triggers::OPERATION_STORE, transformed_addr, false, blocksz);
+    check_triggers(triggers::OPERATION_STORE, transformed_addr & ~(blocksz - 1),
+      /*virt*/ false, blocksz, /*data*/ std::nullopt, transformed_addr);
     convert_load_traps_to_store_traps({
       const reg_t paddr = translate(access_info, 1) - (transformed_addr & (blocksz - 1));
       if (sim->reservable(paddr)) {
@@ -460,7 +462,8 @@ private:
   void check_triggers(triggers::operation_t operation,
     reg_t addr, bool virt, std::size_t access_len);
   void check_triggers(triggers::operation_t operation, reg_t address,
-    bool virt, std::size_t size, std::optional<reg_t> data);
+    bool virt, std::size_t size, std::optional<reg_t> data,
+    std::optional<reg_t> tval = std::nullopt);
 
   bool svukte_qualified(mem_access_info_t access_info);
   bool svukte_fault(reg_t addr, mem_access_info_t access_info);